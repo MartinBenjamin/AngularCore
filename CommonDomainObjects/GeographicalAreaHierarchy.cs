a91ccda7cfa2aee761d449b628470530b24c2e82
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2019-09-16 22:35:58 +0100

    Add GeographicalAreaHierarchy.

---
+++ CommonDomainObjects/GeographicalAreaHierarchy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommonDomainObjects
+{
+    public class GeographicalAreaHierarchy: Hierarchy<Guid, GeographicalAreaHierarchy, GeographicalAreaHierarchyMember, GeographicalArea>
+    {
+        protected GeographicalAreaHierarchy() : base()
+        {
+        }
+
+        public GeographicalAreaHierarchy(
+            IDictionary<GeographicalArea, IList<GeographicalArea>> hierachy
+            ) : base(
+                Guid.NewGuid(),
+                hierachy)
+        {
+        }
+
+        protected override GeographicalAreaHierarchyMember NewHierarchyMember(
+            GeographicalArea                geographicalArea,
+            GeographicalAreaHierarchyMember parentHierarchyMember
+            )
+        {
+            return new GeographicalAreaHierarchyMember(
+                this,
+                geographicalArea,
+                parentHierarchyMember);
+        }
+    }
+
+    public class GeographicalAreaHierarchyMember: HierarchyMember<Guid, GeographicalAreaHierarchy, GeographicalAreaHierarchyMember, GeographicalArea>
+    {
+        protected GeographicalAreaHierarchyMember() : base()
+        {
+        }
+
+        internal GeographicalAreaHierarchyMember(
+            GeographicalAreaHierarchy       hierarchy,
+            GeographicalArea                geographicalArea,
+            GeographicalAreaHierarchyMember parent
+            ) : base(
+                Guid.NewGuid(),
+                hierarchy,
+                geographicalArea,
+                parent)
+        {
+        }
+    }
+}
