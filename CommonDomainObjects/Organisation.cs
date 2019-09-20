1f422ac3d87d8f5ecb2db2041a52e5966b460f30
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2019-09-20 23:26:35 +0100

    Refactor.

--- CommonDomainObjects/Organisation.cs
+++ CommonDomainObjects/Organisation.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace CommonDomainObjects
 {
     public abstract class Organisation: AutonomousAgent
     {
-        public virtual string Acronym { get; protected set; }
+        private IList<OrganisationalSubUnit> _subUnits;
+
+        public virtual string     Acronym  { get; protected set; }
+        public virtual Range<int> Interval { get; protected set; }
+
+        public virtual IReadOnlyList<OrganisationalSubUnit> SubAreas
+        {
+            get
+            {
+                return new ReadOnlyCollection<OrganisationalSubUnit>(_subUnits);
+            }
+        }
 
         protected Organisation() : base()
         {
@@ -19,6 +32,36 @@ namespace CommonDomainObjects
                 name)
         {
             Acronym = acronym;
+        }
+
+        public virtual bool Contains(
+            Organisation organisation
+            )
+        {
+            return Interval.Contains(organisation.Interval);
+        }
+
+        protected internal virtual int AssignInterval(
+            int next
+            )
+        {
+            var start = next++;
+
+            foreach(var child in _subUnits)
+                next = child.AssignInterval(next);
+
+            Interval = new Range<int>(
+                start,
+                next++);
+
+            return next;
+        }
+
+        protected internal virtual void Add(
+            OrganisationalSubUnit subUnit
+            )
+        {
+            _subUnits.Add(subUnit);
         }
     }
 }
--- CommonDomainObjects/OrganisationalSubUnit.cs
+++ CommonDomainObjects/OrganisationalSubUnit.cs
@@ -4,28 +4,35 @@ namespace CommonDomainObjects
 {
     public class OrganisationalSubUnit: Organisation
     {
+        public virtual Organisation Organisation { get; protected set; }
+
         protected OrganisationalSubUnit() : base()
         {
         }
 
         public OrganisationalSubUnit(
-            Guid   id,
-            string name,
-            string acronym
+            Guid         id,
+            string       name,
+            string       acronym,
+            Organisation organisation
             ) : base(
                 id,
                 name,
                 acronym)
         {
+            Organisation = organisation;
+            Organisation?.Add(this);
         }
 
         public OrganisationalSubUnit(
-            string name,
-            string acronym
+            string       name,
+            string       acronym,
+            Organisation organisation
             ) : this(
                 Guid.NewGuid(),
                 name,
-                acronym)
+                acronym,
+                organisation)
         {
         }
     }
