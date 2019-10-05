246a571c2d96be22e64b3a49e92a111cfb39f7cc
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2019-10-05 13:26:43 +0100

    Refactor.

---
+++ Test/TestLoadGeographicalAreas.cs
@@ -0,0 +1,69 @@
+﻿using Autofac;
+using Data;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using NHibernateIntegration;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestFixture]
+    public class TestLoadGeographicalAreas
+    {
+        protected IContainer _container;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            var builder = new ContainerBuilder();
+            builder
+                .RegisterModule<NHibernateIntegration.Module>();
+            builder
+                .RegisterType<CommonDomainObjects.Mapping.ConventionModelMapperFactory>()
+                .As<IModelMapperFactory>()
+                .SingleInstance();
+            builder
+                .RegisterType<SQLiteConfigurationFactory>()
+                .As<IConfigurationFactory>()
+                .SingleInstance();
+            builder
+                .RegisterModule(new SessionFactoryModule("Test"));
+            builder
+                .RegisterModule<Service.Module>();
+
+            _container = builder.Build();
+
+            File.Delete(SQLiteConfigurationFactory.DatabasePath);
+            var schemaExport = new SchemaExport(_container.Resolve<IConfigurationFactory>().Build("Test"));
+            schemaExport.Create(
+                scriptAction => { },
+                true);
+        }
+
+        [Test]
+        public async Task LoadGeographicalAreas()
+        {
+            var geographicalAreas = Loader.LoadIso3166();
+            Assert.That(geographicalAreas.Members.Count, Is.GreaterThan(0));
+
+            using(var scope = _container.BeginLifetimeScope())
+            {
+                var session = scope.Resolve<ISession>();
+                await session.SaveAsync(geographicalAreas);
+                geographicalAreas.Visit(
+                    member =>
+                    {
+                        session.Save(member.Member);
+                        session.Save(member);
+                    });
+                session.Flush();
+            }
+        }
+    }
+}
