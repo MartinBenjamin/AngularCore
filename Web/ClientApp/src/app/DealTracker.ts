5e232cce2c8dc22dc7ac8150f5d9c19d1c7b4680
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2019-10-17 19:43:24 +0100

    Refactor.

--- Web/ClientApp/src/app/DealTracker.ts
+++ Web/ClientApp/src/app/DealTracker.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { Tab } from './Gallery/TabbedView';
-import { KeyDealData } from './KeyDealData';
-import { OriginationTab } from './OriginationTab';
-import { MoreTabs } from './MoreTabs';
 
 @Component(
     {
@@ -11,16 +7,4 @@ import { MoreTabs } from './MoreTabs';
     })
 export class DealTracker
 {
-    public Tabs =
-    [
-        new Tab('Key Deal<br/>Data'     , KeyDealData   ),
-        new Tab('Transaction<br>Details', OriginationTab),
-        new Tab('Security'              , OriginationTab),
-        new Tab('Fees &<br/>Income'     , OriginationTab),
-        new Tab('Key<br/>Dates'         , OriginationTab),
-        new Tab('Key<br/>Counterparties', OriginationTab),
-        new Tab('Syndicate<br/>Info'    , OriginationTab),
-        new Tab('Key Risks &<br/>Events', OriginationTab),
-        new Tab('More'                  , MoreTabs      )
-    ];
 }
