19fda7a1076ade20202359fc0611870252774ad5
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2019-10-26 12:04:43 +0100

    Add Geophysical project.

---
+++ Geophysical/GeographicalAreaHierarchy.cs
@@ -0,0 +1,51 @@
+﻿using CommonDomainObjects;
+using System;
+using System.Collections.Generic;
+
+namespace Geophysical
+{
+    public class GeographicalAreaHierarchy: Hierarchy<Guid, GeographicalAreaHierarchy, GeographicalAreaHierarchyMember, GeographicalArea>
+    {
+        protected GeographicalAreaHierarchy() : base()
+        {
+        }
+
+        public GeographicalAreaHierarchy(
+            IDictionary<GeographicalArea, IList<GeographicalArea>> hierachy
+            ) : base(
+                Guid.NewGuid(),
+                hierachy)
+        {
+        }
+
+        protected override GeographicalAreaHierarchyMember NewHierarchyMember(
+            GeographicalArea                geographicalArea,
+            GeographicalAreaHierarchyMember parentHierarchyMember
+            )
+        {
+            return new GeographicalAreaHierarchyMember(
+                this,
+                geographicalArea,
+                parentHierarchyMember);
+        }
+    }
+
+    public class GeographicalAreaHierarchyMember: HierarchyMember<Guid, GeographicalAreaHierarchy, GeographicalAreaHierarchyMember, GeographicalArea>
+    {
+        protected GeographicalAreaHierarchyMember() : base()
+        {
+        }
+
+        internal GeographicalAreaHierarchyMember(
+            GeographicalAreaHierarchy       hierarchy,
+            GeographicalArea                geographicalArea,
+            GeographicalAreaHierarchyMember parent
+            ) : base(
+                Guid.NewGuid(),
+                hierarchy,
+                geographicalArea,
+                parent)
+        {
+        }
+    }
+}
