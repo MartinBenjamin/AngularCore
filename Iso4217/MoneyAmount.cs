512f8ba5a38ec1aca5dea67765c43555c092bf8c
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2019-10-29 22:51:46 +0000

    Add MoneyAmount.

---
+++ Iso4217/MoneyAmount.cs
@@ -0,0 +1,17 @@
+﻿namespace Iso4217
+{
+    public struct MoneyAmount
+    {
+        public decimal  Value    { get; private set; }
+        public Currency Currency { get; private set; }
+
+        public MoneyAmount(
+            decimal  value,
+            Currency currency
+            )
+        {
+            Value    = value;
+            Currency = currency;
+        }
+    }
+}
