2046f469a1eded0fcadd7c0c53caa3a050a0cece
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2019-11-01 15:23:21 +0000

    Add AdHocSchedule and Ratchets.

---
+++ FacilityAgreements/Ratchets.cs
@@ -0,0 +1,38 @@
+﻿using CommonDomainObjects;
+using System;
+
+namespace FacilityAgreements
+{
+    public class Ratchets: AdHocSchedule<Guid, Ratchets, Ratchet>
+    {
+        protected Ratchets() : base()
+        {
+        }
+
+        public Ratchets(
+            Guid id
+            ) : base(id)
+        {
+        }
+    }
+
+    public class Ratchet: AdHocScheduleEntry<Guid, Ratchets, Ratchet>
+    {
+        public decimal Rate { get; protected set; }
+ 
+        protected Ratchet() : base()
+        {
+        }
+
+        public Ratchet(
+            Guid     id,
+            DateTime date,
+            decimal  rate
+            ) : base(
+                id,
+                date)
+        {
+            Rate = rate;
+        }
+    }
+}
