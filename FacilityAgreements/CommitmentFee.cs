b16b3401e175c858d829aa3057d875ad3f9d0f21
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2019-11-03 15:29:22 +0000

    Refactor.

--- FacilityAgreements/CommitmentFee.cs
+++ FacilityAgreements/CommitmentFee.cs
@@ -8,6 +8,78 @@ namespace FacilityAgreements
         PercentageOfMarginRate
     }
 
+    public abstract class NumericExpression
+    {
+        public abstract decimal Evaluate();
+    }
+
+    public class ConstantNumericExpression: NumericExpression
+    {
+        public decimal Value { get; protected set; }
+
+        public ConstantNumericExpression(
+            decimal value
+            )
+        {
+            Value = value;
+        }
+
+        public override decimal Evaluate() => Value;
+    }
+
+    public class RatioExpression: NumericExpression
+    {
+        public virtual NumericExpression Numerator   { get; protected set; }
+        public virtual NumericExpression Denominator { get; protected set; }
+
+        public RatioExpression(
+            NumericExpression numerator,
+            NumericExpression denominator
+            )
+        {
+            Numerator   = numerator;
+            Denominator = denominator;
+        }
+
+        public override decimal Evaluate() => Numerator.Evaluate() / Denominator.Evaluate();
+    }
+
+    public abstract class PercentageExpression: RatioExpression
+    {
+        private static readonly NumericExpression _denominator = new ConstantNumericExpression(100m);
+
+        protected PercentageExpression(
+            NumericExpression value
+            ): base(
+                value,
+                _denominator)
+        {
+        }
+    }
+
+    public class ConstantPercentageExpression: PercentageExpression
+    {
+        public ConstantPercentageExpression(
+            decimal value
+            ): base(new ConstantNumericExpression(value))
+        {
+        }
+
+        public decimal Value
+        {
+            get => Numerator.Evaluate();
+            protected set => Numerator = new ConstantNumericExpression(value);
+        }
+    }
+
+    public class PercentageOfExpression: NumericExpression
+    {
+        public PercentageExpression Percentage { get; protected set; }
+        public NumericExpression    Of         { get; protected set; }
+
+        public override decimal Evaluate() => Percentage.Numerator.Evaluate() * Of.Evaluate() / Percentage.Denominator.Evaluate();
+    }
+
     public abstract class CommitmentFee: FacilityCommitment
     {
         public decimal?          Estimated              { get; protected set; }
