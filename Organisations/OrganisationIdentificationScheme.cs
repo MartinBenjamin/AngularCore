0229a40ab7ff5c0e822bdb94d549bf9fe086501f
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-01-15 22:36:14 +0000

    Refactor.

---
+++ Organisations/OrganisationIdentificationScheme.cs
@@ -0,0 +1,22 @@
+﻿using CommonDomainObjects;
+using System;
+
+namespace Organisations
+{
+    public class OrganisationIdentificationScheme: Named<Guid>
+    {
+
+        protected OrganisationIdentificationScheme() : base()
+        {
+        }
+
+        protected OrganisationIdentificationScheme(
+            Guid   id,
+            string name
+            ) : base(
+                id,
+                name)
+        {
+        }
+    }
+}
---
+++ Organisations/OrganisationIdentifier.cs
@@ -0,0 +1,58 @@
+﻿namespace Organisations
+{
+    public class OrganisationIdentifier
+    {
+        public OrganisationIdentificationScheme Scheme     { get; protected set; }
+        public string                           Value      { get; protected set; }
+        public Organisation                     Identifies { get; protected set; }
+
+        protected OrganisationIdentifier()
+        {
+        }
+
+        public OrganisationIdentifier(
+            OrganisationIdentificationScheme scheme,
+            string                           value,
+            Organisation                     identifies
+            )
+        {
+            Scheme     = scheme;
+            Value      = value;
+            Identifies = identifies;
+        }
+
+        public override bool Equals(
+            object obj
+            )
+        {
+            var rhs = obj as OrganisationIdentifier;
+            return
+                rhs != null &&
+                Scheme == rhs.Scheme &&
+                Value  == rhs.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public static bool operator ==(
+            OrganisationIdentifier lhs,
+            OrganisationIdentifier rhs
+            )
+        {
+            return Equals(
+                lhs,
+                rhs);
+        }
+
+        public static bool operator !=(
+            OrganisationIdentifier lhs,
+            OrganisationIdentifier rhs
+            )
+        {
+            return !(lhs == rhs);
+        }
+    }
+}
