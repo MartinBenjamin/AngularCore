f032f00f1e26caf09a2d5f45a9f321bd34f2d011
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-01-16 20:49:04 +0000

    Refactor.

---
+++ Organisations/Branch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Organisations
+{
+    public class Branch: OrganisationalSubUnit
+    {
+        
+        public Branch(
+            Guid         id,
+            string       name,
+            string       acronym,
+            Organisation organisation
+            ) : base(
+                id,
+                name,
+                acronym,
+                organisation)
+        {
+        }
+
+        public Branch(
+            string       name,
+            string       acronym,
+            Organisation organisation
+            ) : this(
+                Guid.NewGuid(),
+                name,
+                acronym,
+                organisation)
+        {
+        }
+    }
+}
