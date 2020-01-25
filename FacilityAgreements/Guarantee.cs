a57b092c7754efeceade3968012c4a92eb616cb6
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-01-25 15:11:43 +0000

    Add Guarantee.

---
+++ FacilityAgreements/Guarantee.cs
@@ -0,0 +1,30 @@
+﻿using Agreements;
+using System;
+using System.Collections.Generic;
+
+namespace FacilityAgreements
+{
+    // A commitment whereby something, such as a debt, cash flows on a debt instrument(e.g., interest payments), or performance of some obligation,
+    // is guaranteed if the person or organization with primary liability fails to perform
+    public class Guarantee: FacilityCommitment
+    {
+        // A party that guarantees, endorses, or provides indemnity for some obligation on behalf of some other party.
+        public AgreementParty Guarantor { get; protected set; }
+
+        protected Guarantee() : base()
+        {
+        }
+
+        public Guarantee(
+            Guid           id,
+            Facility       facility,
+            AgreementParty guarantor
+            ) : base(
+                id,
+                facility,
+                new List<AgreementParty> { guarantor })
+        {
+            Guarantor = guarantor;
+        }
+    }
+}
