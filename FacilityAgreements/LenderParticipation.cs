7c0e6e510c6e09d0148e9501c2870f1173ad9408
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-01-25 15:51:38 +0000

    Refactor.

--- FacilityAgreements/LenderParticipation.cs
+++ FacilityAgreements/LenderParticipation.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using Agreements;
+using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace FacilityAgreements
 {
     public class LenderParticipation: FacilityCommitment
     {
+        public virtual AgreementParty Lender                { get; protected set; }
+        public virtual decimal?       UnderwriteAmount      { get; protected set; }
+        public virtual decimal?       CreditSoughtLimit     { get; protected set; }
+        public virtual decimal?       AnticipatedHoldAmount { get; protected set; }
+        public virtual decimal?       ActualAllocation      { get; protected set; }
+
+        protected LenderParticipation() : base()
+        {
+        }
+
+        public LenderParticipation(
+            Guid           id,
+            Facility       facility,
+            AgreementParty lender,
+            decimal?       underwriteAmount,
+            decimal?       creditSoughtLimit,
+            decimal?       anticipatedHoldAmount,
+            decimal?       actualAllocation
+            ): base(
+                id,
+                facility,
+                new List<AgreementParty> { lender })
+        {
+            Lender                = lender;
+            UnderwriteAmount      = underwriteAmount;
+            CreditSoughtLimit     = creditSoughtLimit;
+            AnticipatedHoldAmount = anticipatedHoldAmount;
+            ActualAllocation      = actualAllocation;
+        }
     }
 }
