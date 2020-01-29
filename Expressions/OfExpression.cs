3c6ebe86e2bb2eb56c64f90bab537d170c834e77
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-01-29 21:36:13 +0000

    Refactor.

---
+++ Expressions/OfExpression.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Expressions
+{
+    public class OfExpression: Expression<decimal?>
+    {
+        public Expression<decimal?> Lhs { get; protected set; }
+        public Expression<decimal?> Rhs { get; protected set; }
+
+        protected OfExpression() : base()
+        {
+        }
+
+        public OfExpression(
+            Guid                 id,
+            Expression<decimal?> lhs,
+            Expression<decimal?> rhs
+            ) : base(id)
+        {
+            Lhs = lhs;
+            Rhs = rhs;
+        }
+
+        public override decimal? Evaluate()
+        {
+            var lhs = Lhs.Evaluate();
+            var rhs = Rhs.Evaluate();
+
+            return lhs.HasValue && rhs.HasValue ? lhs * rhs : null;
+        }
+    }
+}
