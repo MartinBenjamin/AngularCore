b9213e1f3ab62bc77ac4fe11e21da24405059c55
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-02-04 21:43:05 +0000

    Refactor.

--- CommonDomainObjects.Mapping/Identifier.cs
+++ CommonDomainObjects.Mapping/Identifier.cs
@@ -4,14 +4,22 @@ namespace CommonDomainObjects.Mapping
 {
     public class Identifier: ClassMapping<Agents.Identifier>
     {
+        private static string _uniqueKeyName = "IdentificationScheme_Identifier";
+
         public Identifier()
         {
             ComposedId(
                 composedIdmapper =>
                 {
-                    ManyToOne(Identifier => Identifier.Scheme    );
-                    ManyToOne(identifier => identifier.Identifies);
+                    composedIdmapper.ManyToOne(
+                        identifier => identifier.Scheme,
+                        manyToOneMapper => manyToOneMapper.UniqueKey(_uniqueKeyName));
+                    composedIdmapper.Property(identifier => identifier.Value);
                 });
+
+            ManyToOne(
+                identifier => identifier.Identifies,
+                manyToOneMapper => manyToOneMapper.UniqueKey(_uniqueKeyName));
         }
     }
 }
