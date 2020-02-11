1e0f1b0eb832d0868effef28b523079cb16e0f8a
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-02-11 22:10:08 +0000

    Refactor.

--- CommonDomainObjects/VersionedObject.cs
+++ CommonDomainObjects/VersionedObject.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace CommonDomainObjects
@@ -11,9 +12,9 @@ namespace CommonDomainObjects
 
         public class VersionedVersion: DomainObject<TId>
         {
-            public virtual     TVersioned Versioned { get; protected set; }
-            public virtual     int        Number    { get; protected set; }
-            new public virtual TVersion   Version   { get; protected set; }
+            public virtual TVersioned Versioned { get; protected set; }
+            public virtual int        Number    { get; protected set; }
+            public virtual TVersion   Version   { get; protected set; }
 
             protected VersionedVersion() : base()
             {
@@ -56,6 +57,36 @@ namespace CommonDomainObjects
         protected virtual int NextNumber()
         {
             return ++Number;
+        }
+    }
+
+    public class VersionedObject<TVersion>: VersionedObject<Guid, VersionedObject<TVersion>, VersionedObject<TVersion>.VersionedVersion, TVersion>
+    {
+        new public class VersionedVersion: VersionedObject<Guid, VersionedObject<TVersion>, VersionedVersion, TVersion>.VersionedVersion
+        {
+            protected VersionedVersion() : base()
+            {
+            }
+
+            public VersionedVersion(
+                VersionedObject<TVersion> versioned,
+                TVersion                  version
+                ) : base(
+                    Guid.NewGuid(),
+                    versioned,
+                    version)
+            {
+            }
+        }
+
+        protected VersionedObject() : base()
+        {
+        }
+
+        public VersionedObject(
+            Guid id
+            ) : base(id)
+        {
         }
     }
 }
