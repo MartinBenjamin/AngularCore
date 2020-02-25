91053fbf63daf06d71e38d7699f2d9514e5e510f
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-02-25 21:56:27 +0000

    Refactor.

--- Expressions/Expression.cs
+++ Expressions/Expression.cs
@@ -17,19 +17,4 @@ namespace Expressions
 
         public abstract TResult Evaluate();
     }
-
-    public abstract class Function<T, TResult>: DomainObject<Guid>
-    {
-        protected Function() : base()
-        {
-        }
-
-        protected Function(
-            Guid id
-            ) : base(id)
-        {
-        }
-
-        public abstract TResult Evaluate(T t);
-    }
 }
---
+++ Expressions/Function.cs
@@ -0,0 +1,20 @@
+﻿using CommonDomainObjects;
+using System;
+
+namespace Expressions
+{
+    public abstract class Function<T, TResult>: DomainObject<Guid>
+    {
+        protected Function() : base()
+        {
+        }
+
+        protected Function(
+            Guid id
+            ) : base(id)
+        {
+        }
+
+        public abstract TResult Evaluate(T t);
+    }
+}
