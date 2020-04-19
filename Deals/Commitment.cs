7428fa16cb5102475729d92af31e08bead02a60e
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-04-19 13:40:21 +0100

    Refactor.

--- Deals/Commitment.cs
+++ Deals/Commitment.cs
@@ -6,9 +6,11 @@ namespace Deals
 {
     public abstract class Commitment: DomainObject<Guid>
     {
-        public virtual Deal             Deal     { get; protected set; }
+        public virtual Deal              Deal     { get; protected set; }
         // Parties that are bound legally or by agreement to repay a debt, make a payment, do something, or refrain from doing something.
-        public virtual IList<DealParty> Obligors { get; protected set; }
+        public virtual IList<DealParty>  Obligors { get; protected set; }
+        public virtual Commitment        PartOf   { get; protected set; }
+        public virtual IList<Commitment> Parts    { get; protected set; }
 
         protected Commitment() : base()
         {
@@ -17,12 +19,16 @@ namespace Deals
         protected Commitment(
             Guid             id,
             Deal             deal,
-            IList<DealParty> obligors
+            IList<DealParty> obligors,
+            Commitment       partOf
             ) : base(id)
         {
             Deal     = deal;
             Obligors = obligors;
+            PartOf   = partOf;
+            Parts    = new List<Commitment>();
             Deal.Commitments.Add(this);
+            PartOf?.Parts.Add(this);
         }
     }
 }
