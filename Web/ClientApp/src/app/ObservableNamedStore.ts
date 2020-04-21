f647f0a689695ada944575964f19e86e8ce181da
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-04-21 16:55:15 +0100

    Refactor.

--- Web/ClientApp/src/app/ObservableNamedStore.ts
+++ Web/ClientApp/src/app/ObservableNamedStore.ts
@@ -1,10 +1,11 @@
-import { BehaviorSubject, Observable } from "rxjs";
+import { Observable, Subscriber } from "rxjs";
 import { Named } from "./CommonDomainObjects";
 import { INamedService, NamedFilters } from "./INamedService";
 
 export class ObservableNamedStore<TId, TNamed extends Named<TId>> extends Observable<TNamed[]>
 {
-    private _namedStore: BehaviorSubject<TNamed[]>;
+    private _named      : TNamed[];
+    private _subscribers: Subscriber<TNamed[]>[];
 
     constructor(
         namedService: INamedService<TId, TNamed, NamedFilters>
@@ -13,18 +14,32 @@ export class ObservableNamedStore<TId, TNamed extends Named<TId>> extends Observ
         super(
             subscriber =>
             {
-                if(!this._namedStore)
+                if(!this._subscribers)
                 {
-                    this._namedStore = new BehaviorSubject<TNamed[]>(null);
-                    namedService.Find(new NamedFilters()).subscribe(result => this._namedStore.next(result));
+                    this._subscribers = [];
+                    namedService.Find(new NamedFilters()).subscribe(
+                        result =>
+                        {
+                            this._named = result;
+                            this._subscribers.forEach(subscriber => subscriber.next(this._named));
+                        });
                 }
 
-                const subscription = this._namedStore.subscribe(
-                    value => subscriber.next(value),
-                    error => subscriber.error(error),
-                    ()    => subscriber.complete());
+                this._subscribers.push(subscriber);
 
-                subscriber.add(subscription);
+                subscriber.add(
+                    () =>
+                    {
+                        const index = this._subscribers.indexOf(subscriber);
+
+                        if(index != -1)
+                            this._subscribers.splice(
+                                index,
+                                1);
+                    });
+
+                if(this._named)
+                    subscriber.next(this._named);
             });
     }
 }
