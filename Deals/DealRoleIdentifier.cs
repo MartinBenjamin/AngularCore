4c4de567276ab01c4f26f53fa94f03a29cb43155
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-04-23 17:19:10 +0100

    Refactor.

--- Deals/DealRoleIdentifier.cs
+++ Deals/DealRoleIdentifier.cs
@@ -4,20 +4,20 @@ namespace Deals
 {
     public static class DealRoleIdentifier
     {
-        public static readonly Guid Borrower    = new Guid("664D48D3-5BB4-46A4-997A-F096B2030808");
-        public static readonly Guid Lender      = new Guid("515CB087-21CF-417E-BE1B-229585416015");
-        public static readonly Guid Guarantor   = new Guid("6E4EACFB-27CD-4C83-8CE1-B3BF4043DC37");
-        public static readonly Guid Advisor     = new Guid("32076CE7-7219-48C9-8BE1-9E6754B034A6");
-        public static readonly Guid Sponsor     = new Guid("00119E8C-E136-4AA8-A554-DB0FCC09850A");
+        public static readonly Guid Borrower    = new Guid("664d48d3-5bb4-46a4-997a-f096b2030808");
+        public static readonly Guid Lender      = new Guid("515cb087-21cf-417e-be1b-229585416015");
+        public static readonly Guid Guarantor   = new Guid("6e4eacfb-27cd-4c83-8ce1-b3bf4043dc37");
+        public static readonly Guid Advisor     = new Guid("32076ce7-7219-48c9-8be1-9e6754b034a6");
+        public static readonly Guid Sponsor     = new Guid("00119e8c-e136-4aa8-a554-db0fcc09850a");
 
 
     }
 
     public static class DealTeamRoleIdentifier
     {
-        public static readonly Guid Leader         = new Guid("1394234B-8153-410D-B775-07BADA9536C5");
-        public static readonly Guid Member         = new Guid("55DF9BD2-C0C1-4690-BE63-0DFF1F887E81");
-        public static readonly Guid WallStraddler  = new Guid("99DCE2CC-4722-4606-B4A6-61210A6648FE");
-        public static readonly Guid AccountOfficer = new Guid("430B921B-387D-42ED-A63B-410D923722ED");
+        public static readonly Guid Leader         = new Guid("1394234b-8153-410d-b775-07bada9536c5");
+        public static readonly Guid Member         = new Guid("55df9bd2-c0c1-4690-be63-0dff1f887e81");
+        public static readonly Guid WallStraddler  = new Guid("99dce2cc-4722-4606-b4a6-61210a6648fe");
+        public static readonly Guid AccountOfficer = new Guid("430b921b-387d-42ed-a63b-410d923722ed");
     }
 }
