1d20b540112dc24e3c9aad46f553edc82701a558
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-05-30 11:26:59 +0100

    Refactor.

---
+++ Ontology/DataAllValuesFrom.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace Ontology
+{
+    public class DataAllValuesFrom:
+        ClassExpression,
+        IDataAllValuesFrom
+    {
+        private IDataPropertyExpression _dataPropertyExpression;
+        private IDataRange              _dataRange;
+
+        public DataAllValuesFrom(
+            IDataPropertyExpression dataPropertyExpression,
+            IDataRange              dataRange
+            )
+        {
+            _dataPropertyExpression = dataPropertyExpression;
+            _dataRange              = dataRange;
+        }
+
+        IDataPropertyExpression IDataAllValuesFrom.DataPropertyExpression => _dataPropertyExpression;
+
+        IDataRange IDataAllValuesFrom.DataRange => _dataRange;
+
+        public override bool HasMember(
+            object individual
+            ) => _dataPropertyExpression.Values(individual).All(_dataRange.HasMember);
+    }
+}
