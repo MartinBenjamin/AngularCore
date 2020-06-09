2a77c373c0a27d87066436b7e00a3c1cda459547
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-06-09 17:23:25 +0100

    Refactor.

--- Test/TestOntology.cs
+++ Test/TestOntology.cs
@@ -77,8 +77,9 @@ namespace Test
             (
                 from classExpression in classification[deal]
                 from axiom in classExpression.SuperClasses
+                from annotation in axiom.Annotations
                 where
-                    axiom.Annotations.Any(annotation => annotation.Property == dealOntology.Mandatory) &&
+                    annotation.Property == dealOntology.Mandatory &&
                     axiom.SuperClassExpression is IPropertyRestriction propertyRestriction &&
                     propertyRestriction.PropertyExpression.Name == "Name"
                 select axiom
@@ -126,8 +127,9 @@ namespace Test
             (
                 from classExpression in classification[sponsor]
                 from axiom in classExpression.SuperClasses
+                from annotation in axiom.Annotations
                 where
-                    axiom.Annotations.Any(annotation => annotation.Property == dealOntology.Mandatory) &&
+                    annotation.Property == dealOntology.Mandatory &&
                     axiom.SuperClassExpression is IPropertyRestriction propertyRestriction &&
                     propertyRestriction.PropertyExpression.Name == "Equity"
                 select axiom
@@ -188,70 +190,6 @@ namespace Test
 
             Assert.That(subClassOf, Is.Not.Null);
             Assert.That(result, Is.EqualTo(subClassOf.SuperClassExpression.HasMember(deal)));
-        }
-
-        [TestCase(null , false)]
-        [TestCase(""   , false)]
-        [TestCase("a"  , true )]
-        [TestCase("ab" , true )]
-        [TestCase("abc", true )]
-        public void TestX(
-            string value,
-            bool   result
-            )
-        {
-            //var d = new Deal(
-            //    Guid.NewGuid(),
-            //    value,
-            //    "ProjectFinance",
-            //    null,
-            //    null);
-
-            //var copy = new Deal(
-            //    d.Id,
-            //    null,
-            //    null,
-            //    null,
-            //    null);
-            //var ontology = new DealOntology();
-
-            //var dealClass = ((IOntology)ontology).Classes["Deal"];
-
-            //Assert.That(dealClass.Keys, Is.Not.Empty);
-            //Assert.That(dealClass.Keys[0].AreEqual(d, d), Is.True);
-            //Assert.That(dealClass.AreEqual(d, d), Is.True);
-            //Assert.That(dealClass.AreEqual(d, copy), Is.True);
-            //Assert.That(dealClass.SuperClasses.Contains(ontology.NameMandatory));
-
-            //var classExpressions = ontology.Classify(d);
-            //Assert.That(classExpressions.ContainsKey(d));
-            //classExpressions[d].ForEach(TestContext.WriteLine);
-
-            //var ids = ontology.Id.Values(d).ToList();
-            //Assert.That(ids.Count, Is.EqualTo(1));
-            //Assert.That(ids[0], Is.EqualTo(d.Id));
-            //Assert.That(ids[0], Is.Not.EqualTo(Guid.Empty));
-            //ids = ontology.Id.Values(new object()).ToList();
-            //Assert.That(ids, Is.Empty);
-            ////return;
-            ////Assert.That(PF.ProjectFinance.ClassAxioms, Does.Contain(PF.SponsorCardinality));
-            ////Assert.That(PF.NameMandatory.Validate(d), Is.EqualTo(result));
-
-            //var failed = 
-            //(
-            //    from classExpression in classExpressions[d]
-            //    from axiom in classExpression.SuperClasses
-            //    where !axiom.SuperClassExpression.HasMember(d)
-            //    select axiom
-            //);
-
-            //if(!result)
-            //    Assert.That(failed, Does.Contain(ontology.NameMandatory));
-
-            //else
-            //    Assert.That(failed, Does.Not.Contain(ontology.NameMandatory));
-
-            //Assert.That(failed, Does.Contain(ontology.SponsorCardinality));
         }
     }
 }
