2c4abd09d11703105810b8974ae99e21b37f7a62
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-06-22 11:20:05 +0100

    Add DataType.

---
+++ Ontology/DataType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ontology
+{
+    public class DataType:
+        Entity,
+        IDataType
+    {
+        public DataType(
+            IOntology ontology,
+            string    name
+        ) : base(
+            ontology,
+            name)
+        {
+        }
+
+        bool IDataRange.HasMember(
+            object value
+            )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ontology/IDataType.cs
@@ -0,0 +1,8 @@
+﻿namespace Ontology
+{
+    public interface IDataType:
+        IEntity,
+        IDataRange
+    {
+    }
+}
