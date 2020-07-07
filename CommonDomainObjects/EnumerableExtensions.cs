a5ab147bbc4cf9cc15e576aa3ffdf4c82884d10b
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-07-07 10:29:56 +0100

    Refactor.

--- CommonDomainObjects/EnumerableExtensions.cs
+++ CommonDomainObjects/EnumerableExtensions.cs
@@ -66,20 +66,6 @@ namespace CommonDomainObjects
                 yield return nextbatch;
         }
 
-        public static bool PreservesStructure<U, V>(
-            this Func<U, V> map,
-            Func<U, U>      uMorphism,
-            Func<V, V>      vMorphism,
-            U               u
-            ) => Equals(vMorphism(map(u)), map(uMorphism(u)));
-
-        public static bool PreservesStructure<U, V>(
-            this Func<U, V>         map,
-            Func<U, IEnumerable<U>> uMorphism,
-            Func<V, IEnumerable<V>> vMorphism,
-            U                       u
-            ) => vMorphism(map(u)).ToHashSet().SetEquals(uMorphism(u).Select(map));
-
         public static IEnumerable<IList<T>> Permute<T>(
             this IEnumerable<T> enumerable
             )
---
+++ CommonDomainObjects/FuncExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommonDomainObjects
+{
+    public static class FuncExtensions
+    {
+        public static bool PreservesStructure<U, V>(
+            this Func<U, V> map,
+            Func<U, U>      uMorphism,
+            Func<V, V>      vMorphism,
+            U               u
+            ) => Equals(vMorphism(map(u)), map(uMorphism(u)));
+
+        public static bool PreservesStructure<U, V>(
+            this Func<U, V>         map,
+            Func<U, IEnumerable<U>> uMorphism,
+            Func<V, IEnumerable<V>> vMorphism,
+            U                       u
+            ) => vMorphism(map(u)).ToHashSet().SetEquals(uMorphism(u).Select(map));
+    }
+}
