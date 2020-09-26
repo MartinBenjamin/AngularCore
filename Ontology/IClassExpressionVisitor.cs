5b131d0a52e1be9e3cdb3824dc55199c2ab2850f
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-09-26 09:41:14 +0100

    Refactor.

--- Ontology/IClassExpressionVisitor.cs
+++ Ontology/IClassExpressionVisitor.cs
@@ -2,22 +2,39 @@
 {
     public interface IClassExpressionVisitor
     {
-        void Visit(IClass                  @class                );
-        void Visit(IDataAllValuesFrom      dataAllValuesFrom     );
-        void Visit(IDataMinCardinality     dataMinCardinality    );
-        void Visit(IDataMaxCardinality     dataMaxCardinality    );
-        void Visit(IDataExactCardinality   dataExactCardinality  );
-        void Visit(IDataHasValue           dataHasValue          );
-        void Visit(IDataSomeValuesFrom     dataSomeValuesFrom    );
-        void Visit(IObjectAllValuesFrom    objectAllValuesFrom   );
-        void Visit(IObjectMinCardinality   objectMinCardinality  );
-        void Visit(IObjectMaxCardinality   objectMaxCardinality  );
-        void Visit(IObjectExactCardinality objectExactCardinality);
-        void Visit(IObjectComplementOf     objectComplementOf    );
-        void Visit(IObjectHasValue         objectHasValue        );
-        void Visit(IObjectIntersectionOf   objectIntersectionOf  );
-        void Visit(IObjectOneOf            objectOneOf           );
-        void Visit(IObjectSomeValuesFrom   objectSomeValuesFrom  );
-        void Visit(IObjectUnionOf          objectUnionOf         );
+        void Enter(IClass                  @class                );
+        void Exit (IClass                  @class                );
+        void Enter(IDataAllValuesFrom      dataAllValuesFrom     );
+        void Exit (IDataAllValuesFrom      dataAllValuesFrom     );
+        void Enter(IDataMinCardinality     dataMinCardinality    );
+        void Exit (IDataMinCardinality     dataMinCardinality    );
+        void Enter(IDataMaxCardinality     dataMaxCardinality    );
+        void Exit (IDataMaxCardinality     dataMaxCardinality    );
+        void Enter(IDataExactCardinality   dataExactCardinality  );
+        void Exit (IDataExactCardinality   dataExactCardinality  );
+        void Enter(IDataHasValue           dataHasValue          );
+        void Exit (IDataHasValue           dataHasValue          );
+        void Enter(IDataSomeValuesFrom     dataSomeValuesFrom    );
+        void Exit (IDataSomeValuesFrom     dataSomeValuesFrom    );
+        void Enter(IObjectAllValuesFrom    objectAllValuesFrom   );
+        void Exit (IObjectAllValuesFrom    objectAllValuesFrom   );
+        void Enter(IObjectMinCardinality   objectMinCardinality  );
+        void Exit (IObjectMinCardinality   objectMinCardinality  );
+        void Enter(IObjectMaxCardinality   objectMaxCardinality  );
+        void Exit (IObjectMaxCardinality   objectMaxCardinality  );
+        void Enter(IObjectExactCardinality objectExactCardinality);
+        void Exit (IObjectExactCardinality objectExactCardinality);
+        void Enter(IObjectComplementOf     objectComplementOf    );
+        void Exit (IObjectComplementOf     objectComplementOf    );
+        void Enter(IObjectHasValue         objectHasValue        );
+        void Exit (IObjectHasValue         objectHasValue        );
+        void Enter(IObjectIntersectionOf   objectIntersectionOf  );
+        void Exit (IObjectIntersectionOf   objectIntersectionOf  );
+        void Enter(IObjectOneOf            objectOneOf           );
+        void Exit (IObjectOneOf            objectOneOf           );
+        void Enter(IObjectSomeValuesFrom   objectSomeValuesFrom  );
+        void Exit (IObjectSomeValuesFrom   objectSomeValuesFrom  );
+        void Enter(IObjectUnionOf          objectUnionOf         );
+        void Exit (IObjectUnionOf          objectUnionOf         );
     }
 }
