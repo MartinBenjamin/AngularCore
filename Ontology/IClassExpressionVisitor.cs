222ce73dc5836bb25a3b2a5c5034ffeb7fdec676
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-09-26 09:53:41 +0100

    Refactor.

--- Ontology/IClassExpressionVisitor.cs
+++ Ontology/IClassExpressionVisitor.cs
@@ -37,4 +37,43 @@
         void Enter(IObjectUnionOf          objectUnionOf         );
         void Exit (IObjectUnionOf          objectUnionOf         );
     }
+
+    public class ClassExpressionVisitor: IClassExpressionVisitor
+    {
+        public virtual void Enter(IClass                  @class                ) {}
+        public virtual void Exit (IClass                  @class                ) {}
+        public virtual void Enter(IDataAllValuesFrom      dataAllValuesFrom     ) {}
+        public virtual void Exit (IDataAllValuesFrom      dataAllValuesFrom     ) {}
+        public virtual void Enter(IDataMinCardinality     dataMinCardinality    ) {}
+        public virtual void Exit (IDataMinCardinality     dataMinCardinality    ) {}
+        public virtual void Enter(IDataMaxCardinality     dataMaxCardinality    ) {}
+        public virtual void Exit (IDataMaxCardinality     dataMaxCardinality    ) {}
+        public virtual void Enter(IDataExactCardinality   dataExactCardinality  ) {}
+        public virtual void Exit (IDataExactCardinality   dataExactCardinality  ) {}
+        public virtual void Enter(IDataHasValue           dataHasValue          ) {}
+        public virtual void Exit (IDataHasValue           dataHasValue          ) {}
+        public virtual void Enter(IDataSomeValuesFrom     dataSomeValuesFrom    ) {}
+        public virtual void Exit (IDataSomeValuesFrom     dataSomeValuesFrom    ) {}
+        public virtual void Enter(IObjectAllValuesFrom    objectAllValuesFrom   ) {}
+        public virtual void Exit (IObjectAllValuesFrom    objectAllValuesFrom   ) {}
+        public virtual void Enter(IObjectMinCardinality   objectMinCardinality  ) {}
+        public virtual void Exit (IObjectMinCardinality   objectMinCardinality  ) {}
+        public virtual void Enter(IObjectMaxCardinality   objectMaxCardinality  ) {}
+        public virtual void Exit (IObjectMaxCardinality   objectMaxCardinality  ) {}
+        public virtual void Enter(IObjectExactCardinality objectExactCardinality) {}
+        public virtual void Exit (IObjectExactCardinality objectExactCardinality) {}
+        public virtual void Enter(IObjectComplementOf     objectComplementOf    ) {}
+        public virtual void Exit (IObjectComplementOf     objectComplementOf    ) {}
+        public virtual void Enter(IObjectHasValue         objectHasValue        ) {}
+        public virtual void Exit (IObjectHasValue         objectHasValue        ) {}
+        public virtual void Enter(IObjectIntersectionOf   objectIntersectionOf  ) {}
+        public virtual void Exit (IObjectIntersectionOf   objectIntersectionOf  ) {}
+        public virtual void Enter(IObjectOneOf            objectOneOf           ) {}
+        public virtual void Exit (IObjectOneOf            objectOneOf           ) {}
+        public virtual void Enter(IObjectSomeValuesFrom   objectSomeValuesFrom  ) {}
+        public virtual void Exit (IObjectSomeValuesFrom   objectSomeValuesFrom  ) {}
+        public virtual void Enter(IObjectUnionOf          objectUnionOf         ) {}
+        public virtual void Exit(IObjectUnionOf objectUnionOf                   ) {}
+
+    }
 }
