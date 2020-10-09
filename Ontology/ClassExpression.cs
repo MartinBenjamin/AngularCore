e58815c088fa36a1bf81ac3c0ac55de2a2f7ff94
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-09 18:19:35 +0100

    Refactor.

--- Ontology/ClassExpression.cs
+++ Ontology/ClassExpression.cs
@@ -1,17 +1,10 @@
-﻿using System.Collections.Generic;
-
-namespace Ontology
+﻿namespace Ontology
 {
     public abstract class ClassExpression: IClassExpression
     {
         protected ClassExpression() : base()
         {
         }
-
-        public abstract bool HasMember(
-            IOntology                                      context,
-            IDictionary<object, HashSet<IClassExpression>> classifications,
-            object                                         individual);
 
         public abstract void Accept(IClassExpressionVisitor visitor);
 
