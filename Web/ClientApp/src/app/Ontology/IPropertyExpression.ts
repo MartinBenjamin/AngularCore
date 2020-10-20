9b55ebf6983ce3a41e2972a09dd7f20cab00b91d
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-20 10:18:52 +0100

    Refactor.

--- Web/ClientApp/src/app/Ontology/IPropertyExpression.ts
+++ Web/ClientApp/src/app/Ontology/IPropertyExpression.ts
@@ -1,11 +1,7 @@
 import { IEntity } from "./IEntity";
-import { IOntology } from "./IOntology";
 
 export interface IPropertyExpression extends IEntity
 {
-    //Values(
-    //    context   : IOntology,
-    //    individual: object): Iterable<object>;
 }
 
 export interface IObjectPropertyExpression extends IPropertyExpression
@@ -14,8 +10,4 @@ export interface IObjectPropertyExpression extends IPropertyExpression
 
 export interface IDataPropertyExpression extends IPropertyExpression
 {
-    //AreEqual(
-    //    context: IOntology,
-    //    lhs    : object,
-    //    rhs    : object): boolean;
 }
