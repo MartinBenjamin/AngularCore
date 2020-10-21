1191c26afabead880d90d48def87eb6ca6277829
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-21 15:09:54 +0100

    Refactor.

--- Web/ClientApp/src/app/Ontology/AdjacencyList.ts
+++ Web/ClientApp/src/app/Ontology/AdjacencyList.ts
@@ -23,57 +23,18 @@ export function LongestPath<TVertex, TAdjacent extends Iterable<TVertex>>(
 
 }
 
-//export function TopologicalSort<TVertex, TAdjacent extends Iterable<TVertex>>(
-//    graph: Map<TVertex, TAdjacent>
-//    ): Iterable<TVertex>
-//{
-    
-//    if(!longestPaths.has(vertex))
-//    {
-//        let longestPath = 0;
-//        for(let adjacentVertex of graph.get(vertex))
-//            longestPath = Math.max(
-//                longestPath,
-//                LongestPath(
-//                    graph,
-//                    longestPaths,
-//                    adjacentVertex));
-//        longestPaths.set(
-//            vertex,
-//            longestPath);
-//    }
-
-//    return longestPaths.get(vertex);
-
-//}
-//        public static int LongestPath<TVertex>(
-//        this Map < TVertex, IList < TVertex >> graph,
-//        TVertex                                   vertex
-//    ) => graph.LongestPath(
-//        new Dictionary<TVertex, int>(),
-//        vertex);
-
-//        public static int LongestPath<TVertex, TAdjacent>(
-//            this Map < TVertex, TAdjacent > graph,
-//            TVertex                              vertex
-//        ) where TAdjacent: IEnumerable < TVertex >
-//                => graph.LongestPath(
-//            new Dictionary<TVertex, int>(),
-//            vertex);
-
-//        public static IEnumerable < TVertex > TopologicalSort<TVertex>(
-//                this Map < TVertex, IList < TVertex >> graph
-//            ) => from vertex in graph.Keys
-//orderby graph.LongestPath(
-//    new Dictionary<TVertex, int>(),
-//    vertex)
-//select vertex;
-
-//        public static IEnumerable < TVertex > TopologicalSort<TVertex, TAdjacent>(
-//    this Map < TVertex, TAdjacent > graph
-//) where TAdjacent: IEnumerable < TVertex >
-//                => from vertex in graph.Keys
-//orderby graph.LongestPath(
-//    new Dictionary<TVertex, int>(),
-//    vertex)
-//select vertex;
+export function TopologicalSort<TVertex, TAdjacent extends Iterable<TVertex>>(
+    graph: Map<TVertex, TAdjacent>
+    ): Iterable<TVertex>
+{
+    let longestPaths = new Map<TVertex, number>();
+    return Array.from(graph.keys()).sort((
+        a,
+        b) => LongestPath(
+            graph,
+            longestPaths,
+            b) - LongestPath(
+                graph,
+                longestPaths,
+                a));
+}
