03141e75d4a2c4076ad6837f1ae2f373e287cd4b
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-23 15:37:41 +0100

    Refactor.

--- Web/ClientApp/src/app/Ontology/IOntology.ts
+++ Web/ClientApp/src/app/Ontology/IOntology.ts
@@ -3,66 +3,10 @@ import { IIsAxiom } from './IIsAxiom'
 
 export interface IOntology
 {
-    Iri    : string;
-    Imports: IOntology[];
-    Axioms : IAxiom[];
-    IsAxiom: IIsAxiom;
+    readonly Iri    : string;
+    readonly Imports: IOntology[];
+    readonly Axioms : IAxiom[];
+    readonly IsAxiom: IIsAxiom;
     GetOntologies(): Iterable<IOntology>;
     Get<TAxiom extends IAxiom>(typeGuard: (axiom: object) => axiom is TAxiom): Iterable<TAxiom>;
 }
-
-function* i(
-    ontology: IOntology)
-{
-    yield ontology;
-}
-
-class iter implements Iterator<IOntology>
-{
-    done = false;
-    next(): IteratorResult<IOntology>
-    {
-        if(!this.done)
-        {
-            this.done = true;
-            return {
-                value: new Ontology(),
-                done: false
-            };
-        }
-        else
-            return {
-                value: null,
-                done: true
-            };
-    }
-}
-
-export class Ontology implements IOntology
-{
-    Iri: string;
-    Imports: IOntology[];
-    Axioms: IAxiom[];
-    IsAxiom: null;
-    GetOntologies(): Iterable<IOntology>
-    {
-        //return {
-        //    [Symbol.iterator]()
-        //    {
-        //        return new iter();
-        //    }
-        let o = this;
-        return {
-            *[Symbol.iterator]()
-            {
-                yield o;
-            }
-          };
-    }
-    Get<TAxiom extends IAxiom>(): Iterable<TAxiom>
-    {
-        throw new Error("Method not implemented.");
-    }
-
-}
-
