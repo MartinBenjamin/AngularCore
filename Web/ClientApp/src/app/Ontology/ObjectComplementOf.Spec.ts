53c33af227f6a0994efe49af867908d1ce877206
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-24 10:54:16 +0100

    Refactor.

---
+++ Web/ClientApp/src/app/Ontology/ObjectComplementOf.Spec.ts
@@ -0,0 +1,59 @@
+import { } from 'jasmine';
+import { Class } from "./Class";
+import { ClassMembershipEvaluator } from './ClassMembershipEvaluator';
+import { IClassExpression } from './IClassExpression';
+import { IOntology } from "./IOntology";
+import { ClassAssertion, NamedIndividual } from './NamedIndividual';
+import { Ontology } from "./Ontology";
+import { ObjectComplementOf } from './ObjectComplementOf';
+
+describe(
+    'ObjectComplementOf',
+    () =>
+    {
+        describe(
+            'Given an Ontology o1:',
+            () =>
+            {
+                let o1: IOntology = new Ontology('o1');
+                describe(
+                    `Given o1 declares Class c1 with member i1 and Class c2 with member i2:`,
+                    () =>
+                    {
+                        let c1 = new Class(o1, 'c1');
+                        let c2 = new Class(o1, 'c2');
+                        let i1 = new NamedIndividual(o1, 'i1');
+                        let i2 = new NamedIndividual(o1, 'i2');
+                        new ClassAssertion(o1, c1, i1);
+                        new ClassAssertion(o1, c2, i2);
+                        let evaluator = new ClassMembershipEvaluator(o1, new Map<object, Set<IClassExpression>>());
+                        it(
+                            'c1.Evaluate(evaluator, i1)',
+                            () => expect(c1.Evaluate(evaluator, i1)).toBe(true));
+                        it(
+                            'new ObjectComplementOf(c1).Evaluate(evaluator, i1) === false',
+                            () => expect(new ObjectComplementOf(c1).Evaluate(evaluator, i1)).toBe(false));
+
+                        it(
+                            'c1.Evaluate(evaluator, i2) === false',
+                            () => expect(c1.Evaluate(evaluator, i2)).toBe(false));
+                        it(
+                            'new ObjectComplementOf(c1).Evaluate(evaluator, i2)',
+                            () => expect(new ObjectComplementOf(c1).Evaluate(evaluator, i2)).toBe(true));
+
+                        it(
+                            'c2.Evaluate(evaluator, i1) === false',
+                            () => expect(c2.Evaluate(evaluator, i1)).toBe(false));
+                        it(
+                            'new ObjectComplementOf(c2).Evaluate(evaluator, i1)',
+                            () => expect(new ObjectComplementOf(c2).Evaluate(evaluator, i1)).toBe(true));
+
+                        it(
+                            'c2.Evaluate(evaluator, i2)',
+                            () => expect(c2.Evaluate(evaluator, i2)).toBe(true));
+                        it(
+                            'new ObjectComplementOf(c2).Evaluate(evaluator, i2)',
+                            () => expect(new ObjectComplementOf(c2).Evaluate(evaluator, i2)).toBe(false));
+                    });
+            });
+    });
