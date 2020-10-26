1bbb8a1b271d6f792f39bede5cddfb154f9c4b7b
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-26 19:56:48 +0000

    Refactor.

--- Web/ClientApp/src/app/Ontology/ObjectExactCardinality.Spec.ts
+++ Web/ClientApp/src/app/Ontology/ObjectExactCardinality.Spec.ts
@@ -5,14 +5,14 @@ import { IClassExpression } from './IClassExpression';
 import { IOntology } from "./IOntology";
 import { IObjectPropertyExpression } from './IPropertyExpression';
 import { ClassAssertion, NamedIndividual } from './NamedIndividual';
-import { ObjectMaxCardinality } from './ObjectMaxCardinality';
+import { ObjectExactCardinality } from './ObjectExactCardinality';
 import { ObjectSomeValuesFrom } from './ObjectSomeValuesFrom';
 import { Ontology } from "./Ontology";
 import { ObjectPropertyExpression } from './Property';
 
 function assertBuilder(
     evaluator,
-    objectMaxCardinality,
+    objectExactCardinality,
     ope1,
     c1,
     i1,
@@ -25,14 +25,14 @@ function assertBuilder(
         assertion,
         () => expect(new Function(
             'evaluator',
-            'ObjectMaxCardinality',
+            'ObjectExactCardinality',
             'ope1',
             'c1',
             'i1',
             'i2',
             'return ' + assertion)(
                 evaluator,
-                objectMaxCardinality,
+                objectExactCardinality,
                 ope1,
                 c1,
                 i1,
@@ -40,7 +40,7 @@ function assertBuilder(
 }
 
 describe(
-    'ObjectMaxCardinality',
+    'ObjectExactCardinality',
     () =>
     {
         describe(
@@ -59,19 +59,15 @@ describe(
                         new ClassAssertion(o1, c1, i1);
                         new ClassAssertion(o1, c1, i2);
                         let evaluator = new ClassMembershipEvaluator(o1, new Map<object, Set<IClassExpression>>());
-                        let assert = assertBuilder(evaluator, ObjectMaxCardinality, ope1, c1, i1, i2);
+                        let assert = assertBuilder(evaluator, ObjectExactCardinality, ope1, c1, i1, i2);
                         new ObjectSomeValuesFrom(ope1, c1).Evaluate(evaluator, { ope1: [i1] })
-                        assert('new ObjectMaxCardinality(ope1, 0).Evaluate(evaluator, { ope1: [] })');
-                        assert('new ObjectMaxCardinality(ope1, 0).Evaluate(evaluator, { ope1: [ {} ] }) === false');
-                        assert('new ObjectMaxCardinality(ope1, 1).Evaluate(evaluator, { ope1: [] })');
-                        assert('new ObjectMaxCardinality(ope1, 1).Evaluate(evaluator, { ope1: [ {} ] })');
-                        assert('new ObjectMaxCardinality(ope1, 1).Evaluate(evaluator, { ope1: [ {}, {} ] }) === false');
-                        assert('new ObjectMaxCardinality(ope1, 0, c1).Evaluate(evaluator, { ope1: [ {} ] })');
-                        assert('new ObjectMaxCardinality(ope1, 0, c1).Evaluate(evaluator, { ope1: [ i1 ] }) === false');
-                        assert('new ObjectMaxCardinality(ope1, 1, c1).Evaluate(evaluator, { ope1: [ i1 ] })');
-                        assert('new ObjectMaxCardinality(ope1, 1, c1).Evaluate(evaluator, { ope1: [ i1, {} ] })');
-                        assert('new ObjectMaxCardinality(ope1, 1, c1).Evaluate(evaluator, { ope1: [ i1, i2 ] }) === false');
-                        assert('new ObjectMaxCardinality(ope1, 2, c1).Evaluate(evaluator, { ope1: [ i1, i2 ] })');
+                        assert('new ObjectExactCardinality(ope1, 0).Evaluate(evaluator, { ope1: [ {} ] }) === false');
+                        assert('new ObjectExactCardinality(ope1, 1).Evaluate(evaluator, { ope1: [ {} ] })');
+                        assert('new ObjectExactCardinality(ope1, 2).Evaluate(evaluator, { ope1: [ {} ] }) === false');
+                        assert('new ObjectExactCardinality(ope1, 0, c1).Evaluate(evaluator, { ope1: [ {}, i1 ] }) === false');
+                        assert('new ObjectExactCardinality(ope1, 1, c1).Evaluate(evaluator, { ope1: [ {}, i1 ] })');
+                        assert('new ObjectExactCardinality(ope1, 2, c1).Evaluate(evaluator, { ope1: [ {}, i1 ] }) === false');
+                        assert('new ObjectExactCardinality(ope1, 2, c1).Evaluate(evaluator, { ope1: [ {}, i1, i2 ] })');
                     });
             });
     });
