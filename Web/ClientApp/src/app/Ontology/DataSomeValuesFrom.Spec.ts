e902e43cb6a3cf41ba555af5fcfab88dd5afbd81
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-28 14:11:33 +0000

    Refactor.

---
+++ Web/ClientApp/src/app/Ontology/DataSomeValuesFrom.Spec.ts
@@ -0,0 +1,30 @@
+import { } from 'jasmine';
+import { assertBuilder } from './assertBuilder';
+import { ClassMembershipEvaluator } from './ClassMembershipEvaluator';
+import { DataOneOf } from './DataOneOf';
+import { DataSomeValuesFrom } from './DataSomeValuesFrom';
+import { IClassExpression } from './IClassExpression';
+import { IOntology } from "./IOntology";
+import { IDataPropertyExpression } from './IPropertyExpression';
+import { Ontology } from "./Ontology";
+import { DataPropertyExpression } from './Property';
+
+describe(
+    'DataSomeValuesFrom',
+    () =>
+    {
+        describe(
+            'Given an Ontology o1 which declares DataPropertyExpression dpe1:',
+            () =>
+            {
+                let o1: IOntology = new Ontology('o1');
+                let dpe1: IDataPropertyExpression = new DataPropertyExpression(o1, 'dpe1');
+                let evaluator = new ClassMembershipEvaluator(o1, new Map<object, Set<IClassExpression>>());
+                let assert = assertBuilder('evaluator', 'DataSomeValuesFrom', 'DataOneOf', 'dpe1')
+                    (evaluator, DataSomeValuesFrom, DataOneOf, dpe1);
+                assert('!new DataSomeValuesFrom(dpe1, new DataOneOf([ 1 ])).Evaluate(evaluator, { dpe1: [] })');
+                assert('new DataSomeValuesFrom(dpe1, new DataOneOf([ 1 ])).Evaluate(evaluator, { dpe1: [ 1 ] })');
+                assert('new DataSomeValuesFrom(dpe1, new DataOneOf([ 1 ])).Evaluate(evaluator, { dpe1: [ 1, 2 ] })');
+                assert('!new DataSomeValuesFrom(dpe1, new DataOneOf([ 1 ])).Evaluate(evaluator, { dpe1: [ 2 ] })');
+            });
+    });
