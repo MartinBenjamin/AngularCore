db3bf4ba118a98e2ad49e342d78aed45e564e943
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-28 13:03:19 +0000

    Refactor.

--- Web/ClientApp/src/app/Ontology/ObjectAllValuesFrom.Spec.ts
+++ Web/ClientApp/src/app/Ontology/ObjectAllValuesFrom.Spec.ts
@@ -22,26 +22,25 @@ describe(
                 let o1: IOntology = new Ontology('o1');
                 let ope1: IObjectPropertyExpression = new ObjectPropertyExpression(o1, 'ope1');
                 describe(
-                    `Given o1 declares Class c1 with members i1 and i3 and Class c2 with member i2:`,
+                    `Given o1 declares individuals i1, i2, i3 and Class c1 with members i1 and i3:`,
                     () =>
                     {
                         let c1 = new Class(o1, 'c1');
-                        let c2 = new Class(o1, 'c2');
                         let i1 = new NamedIndividual(o1, 'i1');
                         let i2 = new NamedIndividual(o1, 'i2');
                         let i3 = new NamedIndividual(o1, 'i2');
                         new ClassAssertion(o1, c1, i1);
-                        new ClassAssertion(o1, c2, i2);
                         new ClassAssertion(o1, c1, i3);
                         let evaluator = new ClassMembershipEvaluator(o1, new Map<object, Set<IClassExpression>>());
-                        let assert = assertBuilder('evaluator', 'ObjectAllValuesFrom', 'ope1', 'c1', 'c2', 'i1', 'i2', 'i3')
-                            (evaluator, ObjectAllValuesFrom, ope1, c1, c2, i1, i2, i3);
+                        let assert = assertBuilder('evaluator', 'ObjectAllValuesFrom', 'ope1', 'c1', 'i1', 'i2', 'i3')
+                            (evaluator, ObjectAllValuesFrom, ope1, c1, i1, i2, i3);
                         new ObjectSomeValuesFrom(ope1, c1).Evaluate(evaluator, { ope1: [i1] })
                         assert('new ObjectAllValuesFrom(ope1, c1).Evaluate(evaluator, { ope1: []})');
                         assert('new ObjectAllValuesFrom(ope1, c1).Evaluate(evaluator, { ope1: [ i1 ] })');
                         assert('new ObjectAllValuesFrom(ope1, c1).Evaluate(evaluator, { ope1: [ i3 ] })');
                         assert('new ObjectAllValuesFrom(ope1, c1).Evaluate(evaluator, { ope1: [ i1, i3 ] })');
                         assert('new ObjectAllValuesFrom(ope1, c1).Evaluate(evaluator, { ope1: [ i1, i2 ] }) == false');
+                        assert('new ObjectAllValuesFrom(ope1, c1).Evaluate(evaluator, { ope1: [ i2, i3 ] }) == false');
                         assert('new ObjectAllValuesFrom(ope1, c1).Evaluate(evaluator, { ope1: [ i2 ] }) === false');
                     });
             });
--- Web/ClientApp/src/app/Ontology/ObjectSomeValuesFrom.Spec.ts
+++ Web/ClientApp/src/app/Ontology/ObjectSomeValuesFrom.Spec.ts
@@ -21,18 +21,16 @@ describe(
                 let o1: IOntology = new Ontology('o1');
                 let ope1: IObjectPropertyExpression = new ObjectPropertyExpression(o1, 'ope1');
                 describe(
-                    `Given o1 declares Class c1 with member i1 and Class c2 with member i2:`,
+                    `Given o1 declares individuals i1, i2 and Class c1 with member i1:`,
                     () =>
                     {
                         let c1 = new Class(o1, 'c1');
-                        let c2 = new Class(o1, 'c2');
                         let i1 = new NamedIndividual(o1, 'i1');
                         let i2 = new NamedIndividual(o1, 'i2');
                         new ClassAssertion(o1, c1, i1);
-                        new ClassAssertion(o1, c2, i2);
                         let evaluator = new ClassMembershipEvaluator(o1, new Map<object, Set<IClassExpression>>());
-                        let assert = assertBuilder('evaluator', 'ObjectSomeValuesFrom', 'ope1', 'c1', 'c2', 'i1', 'i2')
-                            (evaluator, ObjectSomeValuesFrom, ope1, c1, c2, i1, i2);
+                        let assert = assertBuilder('evaluator', 'ObjectSomeValuesFrom', 'ope1', 'c1', 'i1', 'i2')
+                            (evaluator, ObjectSomeValuesFrom, ope1, c1, i1, i2);
                         new ObjectSomeValuesFrom(ope1, c1).Evaluate(evaluator, {ope1: [ i1 ]})
                         assert('new ObjectSomeValuesFrom(ope1, c1).Evaluate(evaluator, { ope1: [] }) === false');
                         assert('new ObjectSomeValuesFrom(ope1, c1).Evaluate(evaluator, { ope1: [ i1 ] })');
