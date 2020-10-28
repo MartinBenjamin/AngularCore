3cb8a14862e813036a61a9e4224300396d511c6b
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-28 16:27:04 +0000

    Refactor.

---
+++ Web/ClientApp/src/app/Ontology/DataMaxCardinality.Spec.ts
@@ -0,0 +1,37 @@
+import { } from 'jasmine';
+import { assertBuilder } from './assertBuilder';
+import { ClassMembershipEvaluator } from './ClassMembershipEvaluator';
+import { DataMaxCardinality } from './DataMaxCardinality';
+import { DataOneOf } from './DataOneOf';
+import { IClassExpression } from './IClassExpression';
+import { IOntology } from "./IOntology";
+import { IDataPropertyExpression } from './IPropertyExpression';
+import { Ontology } from "./Ontology";
+import { DataPropertyExpression } from './Property';
+
+describe(
+    'DataMaxCardinality',
+    () =>
+    {
+        describe(
+            'Given an Ontology o1 which declares DataPropertyExpression dpe1:',
+            () =>
+            {
+                let o1: IOntology = new Ontology('o1');
+                let dpe1: IDataPropertyExpression = new DataPropertyExpression(o1, 'dpe1');
+                let evaluator = new ClassMembershipEvaluator(o1, new Map<object, Set<IClassExpression>>());
+                let assert = assertBuilder('evaluator', 'DataMaxCardinality', 'DataOneOf', 'dpe1')
+                    (evaluator, DataMaxCardinality, DataOneOf, dpe1);
+                assert('new DataMaxCardinality(dpe1, 0).Evaluate(evaluator, { dpe1: [] })');
+                assert('new DataMaxCardinality(dpe1, 0).Evaluate(evaluator, { dpe1: [ 1 ] }) === false');
+                assert('new DataMaxCardinality(dpe1, 1).Evaluate(evaluator, { dpe1: [] })');
+                assert('new DataMaxCardinality(dpe1, 1).Evaluate(evaluator, { dpe1: [ 1 ] })');
+                assert('new DataMaxCardinality(dpe1, 1).Evaluate(evaluator, { dpe1: [ 1, 2 ] }) === false');
+                assert('new DataMaxCardinality(dpe1, 0, new DataOneOf([ 1 ])).Evaluate(evaluator, { dpe1: [ 0 ] })');
+                assert('new DataMaxCardinality(dpe1, 0, new DataOneOf([ 1 ])).Evaluate(evaluator, { dpe1: [ 1 ] }) === false');
+                assert('new DataMaxCardinality(dpe1, 1, new DataOneOf([ 1 ])).Evaluate(evaluator, { dpe1: [ 1 ] })');
+                assert('new DataMaxCardinality(dpe1, 1, new DataOneOf([ 1 ])).Evaluate(evaluator, { dpe1: [ 1, 2 ] })');
+                assert('new DataMaxCardinality(dpe1, 1, new DataOneOf([ 1, 2 ])).Evaluate(evaluator, { dpe1: [ 1, 2 ] }) === false');
+                assert('new DataMaxCardinality(dpe1, 2, new DataOneOf([ 1, 2 ])).Evaluate(evaluator, { dpe1: [ 1, 2 ] })');
+            });
+    });
