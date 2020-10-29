67a0e3b66103ebc9d2bde3555e0ee472555e2073
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-29 17:29:20 +0000

    Refactor.

---
+++ Web/ClientApp/src/app/Ontology/EquivalentClasses.Spec.ts
@@ -0,0 +1,17 @@
+import { } from 'jasmine';
+import { assertBuilder } from './assertBuilder';
+import { Class } from './Class';
+import { ClassAssertion, NamedIndividual } from './NamedIndividual';
+import { Ontology } from './Ontology';
+import { SubClassOf } from './SubClassOf';
+
+describe(
+    'EquivalentClasses',
+    () =>
+    {
+        describe(
+            'Given ontology o1 ...:',
+            () =>
+            {
+            });
+    });
--- Web/ClientApp/src/app/Ontology/SubClassOf.Spec.ts
+++ Web/ClientApp/src/app/Ontology/SubClassOf.Spec.ts
@@ -10,14 +10,16 @@ describe(
     () =>
     {
         describe(
-            'Given ontology o1 which declares c1 with member i1 and c1 as a subclass of c2:',
+            'Given ontology o1 which declares c1 with member i1, c1 as a subclass of c2 and c2 as a subclass of c3:',
             () =>
             {
                 let o1 = new Ontology('o1');
                 let c1 = new Class(o1, 'c1');
                 let c2 = new Class(o1, 'c2');
+                let c3 = new Class(o1, 'c3');
                 let i1 = new NamedIndividual(o1, 'i1');
                 new SubClassOf(o1, c1, c2);
+                new SubClassOf(o1, c2, c3);
                 new ClassAssertion(o1, c1, i1);
 
                 describe(
@@ -25,11 +27,12 @@ describe(
                     () =>
                     {
                         let result = o1.Classify(i1);
-                        let assert = assertBuilder('result', 'c1', 'c2', 'i1')(result, c1, c2, i1);
+                        let assert = assertBuilder('result', 'c1', 'c2', 'c3', 'i1')(result, c1, c2, c3, i1);
                         assert('result.has(i1)')
-                        assert('result.get(i1).size === 2');
+                        assert('result.get(i1).size === 3');
                         assert('result.get(i1).has(c1)');
                         assert('result.get(i1).has(c2)');
+                        assert('result.get(i1).has(c3)');
                     });
             });
     });
