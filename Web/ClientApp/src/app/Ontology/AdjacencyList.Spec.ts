cfd9db8dfccf965df30f4079e0936066287a990d
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-30 10:46:37 +0000

    Refactor.

--- Web/ClientApp/src/app/Ontology/AdjacencyList.Spec.ts
+++ Web/ClientApp/src/app/Ontology/AdjacencyList.Spec.ts
@@ -2,25 +2,35 @@ import { } from 'jasmine';
 import { LongestPaths } from './AdjacencyList';
 
 describe(
-    'Given A is adjacent to B and B is adjacent to C:',
+    'Adjacency List',
     () =>
     {
-        let a = {};
-        let b = {};
-        let c = {};
-        let adjacencyList = new Map<object, object[]>();
-        adjacencyList.set(a, [b]);
-        adjacencyList.set(b, [c]);
-        adjacencyList.set(c, []);
+        describe(
+            'Given A is adjacent to B and C and B is adjacent to D:',
+            () =>
+            {
+                let a = {};
+                let b = {};
+                let c = {};
+                let d = {};
+                let adjacencyList = new Map<object, object[]>();
+                adjacencyList.set(a, [b, c]);
+                adjacencyList.set(b, [d]);
+                adjacencyList.set(c, []);
+                adjacencyList.set(d, []);
 
-        let longestPaths = LongestPaths(adjacencyList);
-        it(
-            'The longest path of A is 2',
-            () => expect(longestPaths.get(a)).toBe(2));
-        it(
-            'The longest path of B is 1',
-            () => expect(longestPaths.get(b)).toBe(1));
-        it(
-            'The longest path of C is 0',
-            () => expect(longestPaths.get(c)).toBe(0));
+                let longestPaths = LongestPaths(adjacencyList);
+                it(
+                    'The longest path of A is 2',
+                    () => expect(longestPaths.get(a)).toBe(2));
+                it(
+                    'The longest path of B is 1',
+                    () => expect(longestPaths.get(b)).toBe(1));
+                it(
+                    'The longest path of C is 0',
+                    () => expect(longestPaths.get(c)).toBe(0));
+                it(
+                    'The longest path of D is 0',
+                    () => expect(longestPaths.get(d)).toBe(0));
+            });
     });
