63fa65ebc101af57e8f985e372341e74a8d81c94
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-10-30 16:50:38 +0000

    Refactor.

---
+++ Web/ClientApp/src/app/Ontologies/Organisations.ts
@@ -0,0 +1,15 @@
+import { IClass } from "../Ontology/IClass";
+import { Ontology } from "../Ontology/Ontology";
+import { commonDomainObjects } from "./CommonDomainObjects";
+
+export class Organisations extends Ontology
+{
+    readonly Organisation: IClass
+
+    public constructor()
+    {
+        super("Organisations", commonDomainObjects);
+        this.Organisation = this.DeclareClass("Organisation");
+        this.Organisation.SubClassOf(commonDomainObjects.Named);
+    }
+}
