40b609711d8e371d2b9e74a06c986de43e6b23e4
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-11-02 15:06:51 +0000

    Refactor.

--- Web/ClientApp/src/app/Ontology/ObjectOneOf.ts
+++ Web/ClientApp/src/app/Ontology/ObjectOneOf.ts
@@ -1,13 +1,17 @@
+import { ClassExpression } from "./ClassExpression";
 import { IClassExpressionVisitor } from "./IClassExpressionVisitor";
 import { IClassMembershipEvaluator } from "./IClassMembershipEvaluator";
 import { IObjectOneOf } from "./IObjectOneOf";
 
-export class ObjectOneOf implements IObjectOneOf
+export class ObjectOneOf
+    extends ClassExpression
+    implements IObjectOneOf
 {
     public constructor(
         public Individuals: object[]
         )
     {
+        super();
     }
 
     Accept(
