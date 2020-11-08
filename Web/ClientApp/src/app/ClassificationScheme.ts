d3851c344f49384074aa1eecb26d166e759d3696
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-11-08 17:04:34 +0000

    Refactor.

--- Web/ClientApp/src/app/ClassificationScheme.ts
+++ Web/ClientApp/src/app/ClassificationScheme.ts
@@ -1,18 +1,35 @@
 import { DomainObject, Guid, Named, Range } from "./CommonDomainObjects";
 
+export interface ClassificationSchemeBase<
+    TId,
+    TClassificationScheme extends ClassificationSchemeBase<TId, TClassificationScheme, TClassificationSchemeClassifier, TClassifier>,
+    TClassificationSchemeClassifier extends ClassificationSchemeClassifierBase<TId, TClassificationScheme, TClassificationSchemeClassifier, TClassifier>,
+    TClassifier> extends DomainObject<TId>
+{
+    Classifiers: TClassificationSchemeClassifier[];
+}
+
+export interface ClassificationSchemeClassifierBase<
+    TId,
+    TClassificationScheme extends ClassificationSchemeBase<TId, TClassificationScheme, TClassificationSchemeClassifier, TClassifier>,
+    TClassificationSchemeClassifier extends ClassificationSchemeClassifierBase<TId, TClassificationScheme, TClassificationSchemeClassifier, TClassifier>,
+    TClassifier> extends DomainObject<TId>
+{
+    Classifier: TClassifier;                  
+    Super     : TClassificationSchemeClassifier;         
+    Sub       : TClassificationSchemeClassifier[]; 
+    Interval  : Range<number>;    
+}
+
+
 export interface Classifier extends Named<Guid>
 {
 }
 
-export interface ClassificationScheme extends DomainObject<Guid>
+export interface ClassificationScheme extends ClassificationSchemeBase<Guid, ClassificationScheme, ClassificationSchemeClassifier, Classifier>
 {
-    Classifiers: ClassificationSchemeClassifier[];
 }
 
-export interface ClassificationSchemeClassifier extends DomainObject<Guid>
+export interface ClassificationSchemeClassifier extends ClassificationSchemeClassifierBase<Guid, ClassificationScheme, ClassificationSchemeClassifier, Classifier>
 {
-    Classifier: Classifier;
-    Super     : ClassificationSchemeClassifier;
-    Sub       : ClassificationSchemeClassifier[];
-    Interval  : Range<number>;
 }
