4125ff66ed33dbe9a2e48059d92701903ef9e5bc
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-11-12 17:36:09 +0000

    Refactor,

--- Web/ClientApp/src/app/Ontologies/Validate.ts
+++ Web/ClientApp/src/app/Ontologies/Validate.ts
@@ -51,8 +51,67 @@ export function Validate(
                                 propertyErrors.push(subClassOf);
                             }
                 }
-                          
     }
 
     return errors;
 }
+
+export function Validate2(
+    ontology       : IOntology,
+    classifications: Map<object, Set<IClass>>
+    ): Map<object, Map<string, ISubClassOf[]>>
+{
+    var evaluator = new ClassMembershipEvaluator(
+        ontology,
+        classifications);
+
+    let errors = new Map<object, Map<string, ISubClassOf[]>>();
+    for(let keyValuePair of classifications.entries())
+    {
+        let individual = keyValuePair[0];
+        for(let class$ of keyValuePair[1])
+            for(let subClassOf of ontology.Get(ontology.IsAxiom.ISubClassOf))
+                if(subClassOf.SubClassExpression === class$)
+                {
+                    let superClassExpression = subClassOf.SuperClassExpression;
+                    if(ontology.IsClassExpression.IPropertyRestriction(superClassExpression))
+                        for(let annotation of subClassOf.Annotations)
+                            if(annotation.Property === deals.RestrictedfromStage &&
+                                subClassOf.SuperClassExpression.Evaluate(
+                                    evaluator,
+                                    individual))
+                            {
+                                let propertyName = superClassExpression.PropertyExpression.LocalName;
+                                for(let annotationAnnotation of annotation.Annotations)
+                                    if(annotationAnnotation.Property === deals.SubPropertyName)
+                                    {
+                                        propertyName = annotationAnnotation.Value;
+                                        break;
+                                    }
+
+                                let individualErrors = errors.get(individual);
+                                if(!individualErrors)
+                                {
+                                    individualErrors = new Map<string, ISubClassOf[]>();
+                                    errors.set(
+                                        individual,
+                                        individualErrors);
+                                }
+
+                                let propertyErrors = individualErrors.get(propertyName);
+                                if(!propertyErrors)
+                                {
+                                    propertyErrors = [];
+                                    individualErrors.set(
+                                        propertyName,
+                                        propertyErrors);
+                                }
+
+                                propertyErrors.push(subClassOf);
+                            }
+                }
+    }
+
+    return errors;
+}
+
