9cd808456a870649285cb18f439f6c61e38defdb
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-11-12 17:18:04 +0000

    Refactor.

--- Web/ClientApp/src/app/Ontologies/Validate.ts
+++ Web/ClientApp/src/app/Ontologies/Validate.ts
@@ -1,52 +1,58 @@
-import { IOntology } from "../Ontology/IOntology";
+import { ClassMembershipEvaluator } from "../Ontology/ClassMembershipEvaluator";
 import { IClass } from "../Ontology/IClass";
+import { IOntology } from "../Ontology/IOntology";
 import { IPropertyExpression } from "../Ontology/IPropertyExpression";
 import { ISubClassOf } from "../Ontology/ISubClassOf";
-import { ClassMembershipEvaluator } from "../Ontology/ClassMembershipEvaluator";
 import { deals } from "./Deals";
 
 export function Validate(
     ontology       : IOntology,
     classifications: Map<object, Set<IClass>>
-    ): Map<object, Map<IPropertyExpression, ISubClassOf>>
+    ): Map<object, Map<IPropertyExpression, ISubClassOf[]>>
 {
     var evaluator = new ClassMembershipEvaluator(
         ontology,
         classifications);
-    return null;
 
-    //for(let keyValuePair of classifications.entries())
-    //{
-    //    let individual = keyValuePair[0];
-    //    for(let class$ of keyValuePair[1])
-    //        for(let subClassOf of ontology.Get(ontology.IsAxiom.ISubClassOf))
-    //            if(subClassOf.SubClassExpression === class$ &&
-    //               subClassOf.SuperClassExpression instanceof IPropertyRestriction)
-    //                for(let annotation of subClassOf.Annotations)
-    //                    if(annotation.Property === deals.RestrictedfromStage &&
-    //                        subClassOf.SuperClassExpression.Evaluate(
-    //                            evaluator,
-    //                            individual)
+    let errors = new Map<object, Map<IPropertyExpression, ISubClassOf[]>>();
+    for(let keyValuePair of classifications.entries())
+    {
+        let individual = keyValuePair[0];
+        for(let class$ of keyValuePair[1])
+            for(let subClassOf of ontology.Get(ontology.IsAxiom.ISubClassOf))
+                if(subClassOf.SubClassExpression === class$)
+                {
+                    let superClassExpression = subClassOf.SuperClassExpression;
+                    if(ontology.IsClassExpression.IPropertyRestriction(superClassExpression))
+                        for(let annotation of subClassOf.Annotations)
+                            if(annotation.Property === deals.RestrictedfromStage &&
+                                subClassOf.SuperClassExpression.Evaluate(
+                                    evaluator,
+                                    individual))
+                            {
+                                let individualErrors = errors.get(individual);
+                                if(!individualErrors)
+                                {
+                                    individualErrors = new Map<IPropertyExpression, ISubClassOf[]>();
+                                    errors.set(
+                                        individual,
+                                        individualErrors);
+                                }
+
+                                let propertyErrors = individualErrors.get(superClassExpression.PropertyExpression);
+                                if(!propertyErrors)
+                                {
+                                    propertyErrors = [];
+                                    individualErrors.set(
+                                        superClassExpression.PropertyExpression,
+                                        propertyErrors);
+                                }
+
+                                propertyErrors.push(subClassOf);
+                            }
+                }
                           
-    //}
-    //    for(let class$ )
-    //return (
-    //    from keyValuePair in classifications
-    //let individual = keyValuePair.Key
-    //from classExpression in keyValuePair.Value
-    //from subClassOf in ontology.Get<ISubClassOf>()
-    //from annotation in subClassOf.Annotations
-    //where
-    //subClassOf.SubClassExpression == classExpression &&
-    //    subClassOf.SuperClassExpression is IPropertyRestriction &&
-    //        annotation.Property.Iri == "Validation.Restriction" &&
-    //        !subClassOf.SuperClassExpression.Evaluate(
-    //            evaluator,
-    //            individual)
-    //group subClassOf by individual into errorsGroupedByIndividual
-    //select errorsGroupedByIndividual
-    //        ).ToDictionary(
-    //    group => group.Key,
-    //    group => group.ToLookup(
-    //        g => ((IPropertyRestriction)g.SuperClassExpression).PropertyExpression));
+    }
+
+    return errors;
 }
