028015915f190bdc25b1a7e4d36f4459ac1563f7
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-11-14 16:21:38 +0000

    Refactor.

--- Web/ClientApp/src/app/Origination/Origination.ts
+++ Web/ClientApp/src/app/Origination/Origination.ts
@@ -1,5 +1,4 @@
 import { Component, TemplateRef } from '@angular/core';
-import { DealType } from '../Deals';
 import { BehaviorSubject } from 'rxjs';
 
 @Component(
@@ -10,27 +9,6 @@ import { BehaviorSubject } from 'rxjs';
 export class Origination
 {
     private readonly _title = new BehaviorSubject<TemplateRef<any>>(null);
-
-    DealTypes: DealType[] =
-        [
-            {
-                Id: null,
-                Name: 'Advisory',
-                Advisory: true
-            },
-            {
-                Id: null,
-                Name: 'Aviation Finance'
-            },
-            {
-                Id: null,
-                Name: 'Leveraged Finance'
-            },
-            {
-                Id: null,
-                Name: 'Project Finance'
-            }
-        ];
 
     get Title(): BehaviorSubject<TemplateRef<any>>
     {
