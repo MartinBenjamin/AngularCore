7e9ef617169746ff291c9370be9df276b19542e3
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2020-11-22 17:15:52 +0000

    Refactor.

--- Web/ClientApp/src/app/Components/Errors2.ts
+++ Web/ClientApp/src/app/Components/Errors2.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Path } from '../Ontologies/Validate';
+import { Path, PathSegment } from '../Ontologies/Validate';
 
 @Component(
     {
@@ -13,19 +13,27 @@ export class Errors2
 {
     private _errors: any[];
 
+    private _map =
+        {
+            GeographicRegion: 'Country',
+            Currency        : 'BaseCurrency',
+            Sponsor         : (party: any) => `${party.Role.Name} (${party.Organisation.Name})`
+        }
+
     private flatten(
         object: any
         )
     {
         if(Array.isArray(object))
         {
-            let errorpaths: Path[] = object;
-            for(let [,errorPath] of errorpaths)
+            let paths: Path[] = object;
+            for(let path of paths)
             {
-                let [propertyName, error] = errorPath[errorPath.length - 1];
+                let [, pathSegments] = path;
+                let [, error] = pathSegments[pathSegments.length - 1];
                 this._errors.push(
                     {
-                        Error: `${propertyName}: Mandatory`,
+                        Error: `${this.MapPath(path)}: Mandatory`,
                         Property: error
                     })
             }
@@ -64,5 +72,26 @@ export class Errors2
         ): void
     {
         this._errors.forEach((error: any) => error.Property.Highlight = (error.Property == property ? error.Property.Highlight + 1 : 0));
+    }
+
+    private MapPath(
+        [object, pathSegments]: Path
+        )
+    {
+        return pathSegments.map(pathSegment => this.MapPathSegment(pathSegment)).join(' ');
+    }
+
+    private MapPathSegment(
+        [propertyName, object]: PathSegment
+        ): string
+    {
+        let mapType = typeof this._map[propertyName];
+        if(mapType === 'string')
+            return this._map[propertyName];
+
+        else if(mapType === 'function')
+            return this._map[propertyName](object);
+
+        return propertyName; 
     }
 }
