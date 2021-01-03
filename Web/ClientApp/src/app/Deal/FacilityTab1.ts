c0d6f0f7d9850bf3751a8636338c7cced3318533
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-01-03 14:08:24 +0000

    Refactor.

--- Web/ClientApp/src/app/Deal/FacilityTab1.ts
+++ Web/ClientApp/src/app/Deal/FacilityTab1.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FacilityProvider } from '../FacilityProvider';
+import { Subscription } from 'rxjs';
+import { Facility } from '../FacilityAgreements';
 
 @Component(
     {
         templateUrl: './FacilityTab1.html'
     })
-export class FacilityTab1
+export class FacilityTab1 implements OnDestroy
 {
+    private _subscriptions: Subscription[] = [];
+
+    Facility: Facility;
+
+    constructor(
+        facilityProvider: FacilityProvider
+        )
+    {
+        facilityProvider.subscribe(facility => this.Facility = facility);
+    }
+
+    ngOnDestroy(): void
+    {
+        this._subscriptions.forEach(subscription => subscription.unsubscribe());
+    }
 }
