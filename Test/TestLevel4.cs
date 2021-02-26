183df171f9694845f446801031696affe8a89955
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-02-26 09:39:05 +0000

    Bank working code.

--- Test/TestLevel4.cs
+++ Test/TestLevel4.cs
@@ -76,12 +76,10 @@ namespace Test
 
         public IList<bool[]> GenerateCombinations(
             int n,
-            int w,
-            int d
+            int w
             )
         {
             var combination = new bool[n];
-            bool[] initial  = null;
             Initialise(
                 combination,
                 w);
@@ -90,20 +88,11 @@ namespace Test
             var generating = true;
             while(generating)
             {
-                if(initial == null)
-                {
-                    WriteCombination(combination);
-                    combinations.Add((bool[])combination.Clone());
-                    initial = combinations[0];
-                }
-                else if(Distance(initial, combination) == d)
-                {
-                    WriteCombination(combination);
-                    combinations.Add((bool[])combination.Clone());
-                }
-
+                WriteCombination(combination);
+                combinations.Add((bool[])combination.Clone());
                 generating = Increment(combination);
             }
+
             return combinations;
         }
 
@@ -165,48 +154,17 @@ namespace Test
             return distance;
         }
 
-        public IList<int[]> GenerateCombinations(
-            int n,
-            int r
-            )
-        {
-            var combination = new int[r];
-            int[] initial = null;
-            Initialise(combination);
-            IList<int[]> combinations = new List<int[]>();
-            var generating = true;
-            while(generating)
-            {
-                if(initial == null)
-                {
-                    WriteCombination(combination);
-                    combinations.Add((int[])combination.Clone());
-                    initial = combinations[0];
-                }
-                else if(Distance(initial, combination, n) == r)
-                {
-                    WriteCombination(combination);
-                    combinations.Add((int[])combination.Clone());
-                }
-
-                generating = Increment(
-                   n,
-                   combination);
-            }
-            return combinations;
-        }
-
         public int[][] GenerateKeyCombinations(
             int           numBuns,
             int           numRequired,
             int           n,
-            int           d,
             IList<bool[]> keyCombinations,
             int[]         keyCombinationCombination,
             int           position
             )
         {
             int[][] result = null;
+            var keyIsPresent = new bool[n];
             if(position < keyCombinationCombination.Length)
             {
                 var startIndex = 0;
@@ -214,12 +172,67 @@ namespace Test
                     startIndex = keyCombinationCombination[position - 1] + 1;
                 for(var index = startIndex;index < keyCombinations.Count - keyCombinationCombination.Length + position + 1 && result == null;++index)
                 {
-                    var keyCombination = keyCombinations[index];
                     var valid = true;
-                    for(var index1 = 1;index1 < position && valid;++index1)
-                        valid = valid && Distance(
-                            keyCombinations[keyCombinationCombination[index1]],
-                            keyCombination) == d;
+                    if(position + 1 >= numRequired - 1)
+                    {
+                        // This key combination when combined with num_required - 2 previously selected key combinations
+                        // should not contain all the keys.
+
+                        var numRequiredMinusTwoCombination = new int[numRequired - 2];
+
+                        Initialise(numRequiredMinusTwoCombination);
+                        var generating = true;
+
+                        while(
+                            valid &&
+                            generating)
+                        {
+                            Array.Copy(keyCombinations[index], keyIsPresent, n);
+
+                            foreach(var keyCombinationIndex in numRequiredMinusTwoCombination)
+                            {
+                                var keyCombination = keyCombinations[keyCombinationCombination[keyCombinationIndex]];
+                                for(var key = 0;key < n;++key)
+                                    keyIsPresent[key] = keyIsPresent[key] || keyCombination[key];
+                            }
+
+                            valid = !Array.TrueForAll(keyIsPresent, p => p);
+                            if(valid)
+                                generating = Increment(
+                                    position,
+                                    numRequiredMinusTwoCombination);
+                        }
+
+                        if(valid && position + 1 >= numRequired)
+                        {
+                            // This key combination when combined with num_required - 1 previously selected key combinations
+                            // should contain all hte keys.
+                            var numRequiredMinusOneCombination = new int[numRequired - 1];
+                            Initialise(numRequiredMinusOneCombination);
+                            generating = true;
+
+                            while(
+                                valid &&
+                                generating)
+                            {
+                                Array.Copy(keyCombinations[index], keyIsPresent, n);
+
+                                foreach(var keyCombinationIndex in numRequiredMinusOneCombination)
+                                {
+                                    var keyCombination = keyCombinations[keyCombinationCombination[keyCombinationIndex]];
+                                    for(var key = 0;key < n;++key)
+                                        keyIsPresent[key] = keyIsPresent[key] || keyCombination[key];
+                                }
+
+                                valid = Array.TrueForAll(keyIsPresent, p => p);
+                                if(valid)
+                                    generating = Increment(
+                                        position,
+                                        numRequiredMinusOneCombination);
+                            }
+                        }
+
+                    }
 
                     if(valid)
                     {
@@ -228,7 +241,6 @@ namespace Test
                             numBuns,
                             numRequired,
                             n,
-                            d,
                             keyCombinations,
                             keyCombinationCombination,
                             position + 1);
@@ -238,86 +250,14 @@ namespace Test
                 return result;
             }
 
-            var numRequiredcombination         = new int[numRequired];
-            var numRequiredMinusOneCombination = new int[numRequired - 1];
-            var keyIsPresent                   = new bool[n];
-            var rowSums                        = new int[n];
-
-            Initialise(numRequiredcombination);
-            var pass = true;
-            var generating = true;
-
-            // Optimisation.
-            // To get all keys in a choice of numRequired key combinations the key combinations at indices numRequired - 1 to numBuns - 1
-            // must have key n - 1.
-            for(var index = numRequired - 1;index < numBuns && pass;index++)
+            result = new int[numBuns][];
+            for(var index = 0;index < numBuns;++index)
             {
-                var keyCombination = keyCombinations[keyCombinationCombination[index]];
-                pass = pass && keyCombination[n - 1];
-            }
-
-            // Every choice of numRequired must have all keys.
-            while(
-                pass &&
-                generating)
-            {
-                Array.Fill(
-                    keyIsPresent,
-                    false);
-
-                foreach(var index in numRequiredcombination)
-                {
-                    var keyCombination = keyCombinations[keyCombinationCombination[index]];
-                    for(var key = 0;key < n;++key)
-                        keyIsPresent[key] = keyIsPresent[key] || keyCombination[key];
-                }
-
-                pass = Array.TrueForAll(keyIsPresent, p => p);
-                if(pass)
-                    generating = Increment(
-                        numBuns,
-                        numRequiredcombination);
-            }
-
-            if(pass)
-            {
-                // Every choice of numRequired - 1 must not have all keys.
-                Initialise(numRequiredMinusOneCombination);
-                generating = true;
-                while(
-                    pass &&
-                    generating)
-                {
-                    Array.Fill(
-                        keyIsPresent,
-                        false);
-
-                    foreach(var index in numRequiredMinusOneCombination)
-                    {
-                        var keyCombination = keyCombinations[keyCombinationCombination[index]];
-                        for(var key = 0;key < n;++key)
-                            keyIsPresent[key] = keyIsPresent[key] || keyCombination[key];
-                    }
-
-                    pass = !Array.TrueForAll(keyIsPresent, p => p);
-                    if(pass)
-                        generating = Increment(
-                            numBuns,
-                            numRequiredMinusOneCombination);
-                }
-            }
-
-            if(pass)
-            {
-                result = new int[numBuns][];
-                for(var index = 0;index < numBuns;++index)
-                {
-                    var keyCombination = new List<int>();
-                    for(var key = 0;key < n;++key)
-                        if(keyCombinations[keyCombinationCombination[index]][key])
-                            keyCombination.Add(key);
-                    result[index] = keyCombination.ToArray();
-                }
+                var keyCombination = new List<int>();
+                for(var key = 0;key < n;++key)
+                    if(keyCombinations[keyCombinationCombination[index]][key])
+                        keyCombination.Add(key);
+                result[index] = keyCombination.ToArray();
             }
 
             return result;
@@ -327,15 +267,13 @@ namespace Test
             int numBuns,
             int numRequired,
             int n,
-            int w,
-            int d
+            int w
             )
         {
             int[][] result = null;
             IList<bool[]> keyCombinations = GenerateCombinations(
                 n,
-                w,
-                d);
+                w);
             TestContext.WriteLine(keyCombinations.Count);
 
             if(keyCombinations.Count < numBuns)
@@ -345,7 +283,6 @@ namespace Test
                 numBuns,
                 numRequired,
                 n,
-                d,
                 keyCombinations,
                 new int[numBuns],
                 0);
@@ -379,7 +316,6 @@ namespace Test
             // Need to determine:
             // n - number of bits (keys).
             // w - weight/number of bits set to one.
-            // d - Hamming Distance.
             
             // If there are numBuns codes and any numRequired are selected then each key must appear (at least) once in the selection.
             var repeats = numBuns - numRequired + 1;
@@ -389,34 +325,23 @@ namespace Test
             // Therefore n * repeats = num_buns * w. (Design Theory: vr = bk.)
             // Therefore n/w = num_buns/repeats = (multiplier * num_buns)/(multiplier * repeats).
 
-            // A combination of 2 codes eliminates d/2 zeros.
-            // Assume subsequent blocks eliminate one zero - incorrect???
-            // Therefore d/2 + (numRequired - 3) <  n - w
-            // and       d/2 + (numRequired - 2) >= n - w.
-            // Therefore d Upper Bound = 2*(n - w - numRequired + 3)
-            // and       d Lower Bound = 2*(n - w - numRequired + 2).
-            // Therefore d Upper Bound - d Lower Bound = 2.
-            // Since, for a set of Constant Weight (m-by-n) codes the Hamming Distance (d) must be even, d = d Lower Bound;
-
             var multiplier = 1;
             while(result == null)
             {
                 var n = multiplier * numBuns;
                 var w = multiplier * repeats;
-                var d = 2 * (n - w - numRequired + 2);
                 result = GenerateKeyCombinations(
                     numBuns,
                     numRequired,
                     n,
-                    w,
-                    d);
+                    w);
                 multiplier += 1;
             }
 
             return result;
         }
 
-        [TestCaseSource("TestCases1"), Timeout(2000)]
+        [TestCaseSource("TestCases"), Timeout(5000)]
         public void Test(
             int               numBuns,
             int               numRequired,
@@ -431,9 +356,6 @@ namespace Test
             foreach(var keyCombination in result)
                 WriteCombination(keyCombination);
 
-            if(expected.Count == 0)
-                return;
-
             Assert.That(result.Length, Is.EqualTo(expected.Count));
             for(var index1 = 0;index1 < result.Length;index1++)
             {
@@ -443,19 +365,20 @@ namespace Test
             }
         }
 
-        [Test]
-        public void CountCombinations()
+        [TestCaseSource("CoverageTestCases"), Timeout(5000)]
+        public void Coverage(
+            int               numBuns,
+            int               numRequired,
+            IList<IList<int>> expected
+            )
         {
-            for(var numBuns = 1;numBuns < 10;++numBuns)
-                for(var numRequired = 1;numRequired <= numBuns;numRequired++)
-                {
-                    int repeats = numBuns - numRequired + 1;
-                    var n = numBuns;
-                    var w = repeats;
-                    var d = 2 * (n - w - numRequired + 2);
+            var result = GenerateKeyCombinations(
+                numBuns,
+                numRequired);
 
-                    TestContext.WriteLine($"numBuns: {numBuns}, numRequired: {numRequired}, repeats: {repeats}, n: {n}, w: {w}, d: {d}");
-                }
+            Assert.That(result, Is.Not.Null);
+            foreach(var keyCombination in result)
+                WriteCombination(keyCombination);
         }
 
         public static IEnumerable<object[]> TestCases
@@ -530,20 +453,25 @@ namespace Test
                             new TestDataList<int>{ 1, 3, 5, 6, 8, 9 },
                             new TestDataList<int>{ 2, 4, 5, 7, 8, 9 }
                         }
-                    }//,
-                    //new object[]
-                    //{
-                    //    6,
-                    //    3,
-                    //    new TestDataList<IList<int>>
-                    //    {
-                    //    }
-                    //}
+                    },
+                    new object[]
+                    {
+                        5,
+                        4,
+                        new TestDataList<IList<int>>
+                        {
+                            new TestDataList<int>{ 0, 1, 2, 3 },
+                            new TestDataList<int>{ 0, 4, 5, 6 },
+                            new TestDataList<int>{ 1, 4, 7, 8 },
+                            new TestDataList<int>{ 2, 5, 7, 9 },
+                            new TestDataList<int>{ 3, 6, 8, 9 }
+                        }
+                    }
                 };
             }
         }
 
-        public static IEnumerable<object[]> TestCases1
+        public static IEnumerable<object[]> CoverageTestCases
         {
             get
             {
