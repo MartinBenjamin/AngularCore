a85abb609fe530d6461a0fb66a2b085163d6839f
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-04-30 11:39:18 +0100

    Refactor.

--- Web/ClientApp/src/app/Deal/ExternalFunding.ts
+++ Web/ClientApp/src/app/Deal/ExternalFunding.ts
@@ -1,13 +1,15 @@
 import { Component, Inject, OnDestroy, ViewChild } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { EmptyGuid } from '../CommonDomainObjects';
-import { DealRoleIdentifier } from '../Deals';
+import { DealProvider } from '../DealProvider';
+import { Deal, DealRoleIdentifier } from '../Deals';
 import { Facility } from '../FacilityAgreements';
 import { FacilityProvider } from '../FacilityProvider';
 import { LegalEntity } from '../LegalEntities';
 import { LegalEntityFinder } from '../LegalEntityFinder';
 import { Role } from '../Roles';
 import { RolesToken } from '../RoleServiceProvider';
+import { PartyInRole } from '../Parties';
 
 @Component(
     {
@@ -16,22 +18,26 @@ import { RolesToken } from '../RoleServiceProvider';
     })
 export class ExternalFunding implements OnDestroy
 {
+    private _externalFundingProviderRole: Role;
     private _subscriptions              : Subscription[] = [];
+    private _deal                       : Deal;
     private _facility                   : Facility;
     private _externalFunding            : import('../FacilityAgreements').ExternalFunding;
-    private _externalFundingProviderRole: Role;
+    private _providers                  : LegalEntity[];
 
     @ViewChild('legalEntityFinder', { static: true })
     private _legalEntityFinder: LegalEntityFinder;
 
     constructor(
-        facilityProvider: FacilityProvider,
         @Inject(RolesToken)
         roles: Observable<Role[]>,
+        dealProvider: DealProvider,
+        facilityProvider: FacilityProvider,
         )
     {
         this._subscriptions.push(
             roles.subscribe(roles => this._externalFundingProviderRole = roles.find(role => role.Id == DealRoleIdentifier.ExternalFundingProvider)),
+            dealProvider.subscribe(deal => this._deal = deal),
             facilityProvider.subscribe(
                 facility =>
                 {
@@ -41,6 +47,8 @@ export class ExternalFunding implements OnDestroy
 
                     else
                         this._externalFunding = <import('../FacilityAgreements').ExternalFunding>this._facility.Parts.find(part => (<any>part).$type === 'Web.Model.ExternalFunding, Web');
+
+                    this.ComputeProviders();
                 }));
     }
 
@@ -90,11 +98,51 @@ export class ExternalFunding implements OnDestroy
         return this._externalFunding;
     }
 
+    private ComputeProviders()
+    {
+        if(!this._externalFunding)
+        {
+            this._providers = null;
+            return;
+        }
+
+        this._providers = this._externalFunding.Obligors
+            .filter(obligor => obligor.Role.Id == DealRoleIdentifier.ExternalFundingProvider)
+            .map(obligor => <LegalEntity>obligor.Organisation);
+    }
+
+    get Providers(): LegalEntity[]
+    {
+        return this._providers;
+    }
+
     AddProvider(): void
     {
         this._legalEntityFinder.Find(
             legalEntity =>
             {
+                if(this._providers && this._providers.find(provider => provider.Id == legalEntity.Id))
+                {
+                    alert(`${legalEntity.Name} is already a ${this._externalFundingProviderRole.Name}.`);
+                    return;
+                }
+
+                let providerParty = this._deal.Parties.find(
+                    party => party.Organisation.Id === legalEntity.Id && party.Role.Id === this._externalFundingProviderRole.Id);
+
+                if(!providerParty)
+                    providerParty = <PartyInRole>
+                        {
+                            Id             : EmptyGuid,
+                            AutonomousAgent: legalEntity,
+                            Organisation   : legalEntity,
+                            Person         : null,
+                            Role           : this._externalFundingProviderRole,
+                            Period         : null
+                        };
+
+                this._externalFunding.Obligors.push(providerParty);
+                this.ComputeProviders();
             });
     }
 
@@ -102,6 +150,18 @@ export class ExternalFunding implements OnDestroy
         provider: LegalEntity
         ): void
     {
-
+        for(let index = 0; index < this._externalFunding.Obligors.length; ++index)
+        {
+            let obligor = this._externalFunding.Obligors[index];
+            if(obligor.Role.Id == DealRoleIdentifier.ExternalFundingProvider &&
+               obligor.Organisation == provider)
+            {
+                this._externalFunding.Obligors.splice(
+                    index,
+                    1);
+                this.ComputeProviders();
+                break;
+            }
+        }
     }
 }
