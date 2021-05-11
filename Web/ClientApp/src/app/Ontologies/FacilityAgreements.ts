8f8f21043ba7432c38a4380b489084c7534fb446
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-05-11 13:38:09 +0100

    Enhance Facility validation.

--- Web/ClientApp/src/app/Ontologies/FacilityAgreements.ts
+++ Web/ClientApp/src/app/Ontologies/FacilityAgreements.ts
@@ -5,6 +5,7 @@ import { Ontology } from "../Ontology/Ontology";
 import { agreements } from './Agreements';
 import { annotations } from './Annotations';
 import { commonDomainObjects } from "./CommonDomainObjects";
+import { Decimal } from '../Ontology/Xsd';
 
 export class FacilityAgreements extends Ontology
 {
@@ -15,14 +16,22 @@ export class FacilityAgreements extends Ontology
             agreements,
             annotations);
 
+        let nonEmptyString = new DataComplementOf(new DataOneOf([""]));
         let facility = this.DeclareClass("Facility");
         facility.Define(commonDomainObjects.$type.HasValue('Web.Model.Facility, Web'));
         facility.SubClassOf(agreements.Commitment);
 
-        let nonEmptyString = new DataComplementOf(new DataOneOf([""]));
-
         facility.SubClassOf(commonDomainObjects.Name.MinCardinality(1, nonEmptyString))
             .Annotate(annotations.RestrictedfromStage, DealStageIdentifier.Prospect);
+
+        let currency = facility.DeclareObjectProperty("Currency");
+        facility.SubClassOf(currency.ExactCardinality(1))
+            .Annotate(annotations.RestrictedfromStage, DealStageIdentifier.Prospect);
+
+        let totalCommitments = facility.DeclareDataProperty("TotalCommitments");
+        totalCommitments.Range(Decimal);
+        facility.SubClassOf(totalCommitments.ExactCardinality(1))
+            .Annotate(annotations.RestrictedfromStage, DealStageIdentifier.Prospect)
     }
 }
 
