a60f487cff635cd11d953ab37ff1084687d478a0
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-06-03 10:23:20 +0100

    Refactor.

--- Web/ClientApp/src/app/Ontology/IClassExpressionVisitor.ts
+++ Web/ClientApp/src/app/Ontology/IClassExpressionVisitor.ts
@@ -15,44 +15,44 @@ import { IObjectUnionOf } from "./IObjectUnionOf";
 
 export interface IClassExpressionVisitor
 {
-    Class                 (class$                : IClass                 );
-    ObjectIntersectionOf  (objectIntersectionOf  : IObjectIntersectionOf  );
-    ObjectUnionOf         (objectUnionOf         : IObjectUnionOf         );
-    ObjectComplementOf    (objectComplementOf    : IObjectComplementOf    );
-    ObjectOneOf           (objectOneOf           : IObjectOneOf           );
-    ObjectSomeValuesFrom  (objectSomeValuesFrom  : IObjectSomeValuesFrom  );
-    ObjectAllValuesFrom   (objectAllValuesFrom   : IObjectAllValuesFrom   );
-    ObjectHasValue        (objectHasValue        : IObjectHasValue        );
-    ObjectHasSelf         (objectHasSelf         : IObjectHasSelf         );
-    ObjectMinCardinality  (objectMinCardinality  : IObjectMinCardinality  );
-    ObjectMaxCardinality  (objectMaxCardinality  : IObjectMaxCardinality  );
-    ObjectExactCardinality(objectExactCardinality: IObjectExactCardinality);
-    DataSomeValuesFrom    (dataSomeValuesFrom    : IDataSomeValuesFrom    );
-    DataAllValuesFrom     (dataAllValuesFrom     : IDataAllValuesFrom     );
-    DataHasValue          (dataHasValue          : IDataHasValue          );
-    DataMinCardinality    (dataMinCardinality    : IDataMinCardinality    );
-    DataMaxCardinality    (dataMaxCardinality    : IDataMaxCardinality    );
-    DataExactCardinality  (dataExactCardinality  : IDataExactCardinality  );
+    Class                 (class$                : IClass                 ): void;
+    ObjectIntersectionOf  (objectIntersectionOf  : IObjectIntersectionOf  ): void;
+    ObjectUnionOf         (objectUnionOf         : IObjectUnionOf         ): void;
+    ObjectComplementOf    (objectComplementOf    : IObjectComplementOf    ): void;
+    ObjectOneOf           (objectOneOf           : IObjectOneOf           ): void;
+    ObjectSomeValuesFrom  (objectSomeValuesFrom  : IObjectSomeValuesFrom  ): void;
+    ObjectAllValuesFrom   (objectAllValuesFrom   : IObjectAllValuesFrom   ): void;
+    ObjectHasValue        (objectHasValue        : IObjectHasValue        ): void;
+    ObjectHasSelf         (objectHasSelf         : IObjectHasSelf         ): void;
+    ObjectMinCardinality  (objectMinCardinality  : IObjectMinCardinality  ): void;
+    ObjectMaxCardinality  (objectMaxCardinality  : IObjectMaxCardinality  ): void;
+    ObjectExactCardinality(objectExactCardinality: IObjectExactCardinality): void;
+    DataSomeValuesFrom    (dataSomeValuesFrom    : IDataSomeValuesFrom    ): void;
+    DataAllValuesFrom     (dataAllValuesFrom     : IDataAllValuesFrom     ): void;
+    DataHasValue          (dataHasValue          : IDataHasValue          ): void;
+    DataMinCardinality    (dataMinCardinality    : IDataMinCardinality    ): void;
+    DataMaxCardinality    (dataMaxCardinality    : IDataMaxCardinality    ): void;
+    DataExactCardinality  (dataExactCardinality  : IDataExactCardinality  ): void;
 }
 
 export class ClassExpressionVisitor implements IClassExpressionVisitor
 {
-    Class                 (class$                : IClass                 ) {}
-    ObjectIntersectionOf  (objectIntersectionOf  : IObjectIntersectionOf  ) {}
-    ObjectUnionOf         (objectUnionOf         : IObjectUnionOf         ) {}
-    ObjectComplementOf    (objectComplementOf    : IObjectComplementOf    ) {}
-    ObjectOneOf           (objectOneOf           : IObjectOneOf           ) {}
-    ObjectSomeValuesFrom  (objectSomeValuesFrom  : IObjectSomeValuesFrom  ) {}
-    ObjectAllValuesFrom   (objectAllValuesFrom   : IObjectAllValuesFrom   ) {}
-    ObjectHasValue        (objectHasValue        : IObjectHasValue        ) {}
-    ObjectHasSelf         (objectHasSelf         : IObjectHasSelf         ) {}
-    ObjectMinCardinality  (objectMinCardinality  : IObjectMinCardinality  ) {}
-    ObjectMaxCardinality  (objectMaxCardinality  : IObjectMaxCardinality  ) {}
-    ObjectExactCardinality(objectExactCardinality: IObjectExactCardinality) {}
-    DataSomeValuesFrom    (dataSomeValuesFrom    : IDataSomeValuesFrom    ) {}
-    DataAllValuesFrom     (dataAllValuesFrom     : IDataAllValuesFrom     ) {}
-    DataHasValue          (dataHasValue          : IDataHasValue          ) {}
-    DataMinCardinality    (dataMinCardinality    : IDataMinCardinality    ) {}
-    DataMaxCardinality    (dataMaxCardinality    : IDataMaxCardinality    ) {}
-    DataExactCardinality  (dataExactCardinality  : IDataExactCardinality  ) {}
+    Class                 (class$                : IClass                 ): void {}
+    ObjectIntersectionOf  (objectIntersectionOf  : IObjectIntersectionOf  ): void {}
+    ObjectUnionOf         (objectUnionOf         : IObjectUnionOf         ): void {}
+    ObjectComplementOf    (objectComplementOf    : IObjectComplementOf    ): void {}
+    ObjectOneOf           (objectOneOf           : IObjectOneOf           ): void {}
+    ObjectSomeValuesFrom  (objectSomeValuesFrom  : IObjectSomeValuesFrom  ): void {}
+    ObjectAllValuesFrom   (objectAllValuesFrom   : IObjectAllValuesFrom   ): void {}
+    ObjectHasValue        (objectHasValue        : IObjectHasValue        ): void {}
+    ObjectHasSelf         (objectHasSelf         : IObjectHasSelf         ): void {}
+    ObjectMinCardinality  (objectMinCardinality  : IObjectMinCardinality  ): void {}
+    ObjectMaxCardinality  (objectMaxCardinality  : IObjectMaxCardinality  ): void {}
+    ObjectExactCardinality(objectExactCardinality: IObjectExactCardinality): void {}
+    DataSomeValuesFrom    (dataSomeValuesFrom    : IDataSomeValuesFrom    ): void {}
+    DataAllValuesFrom     (dataAllValuesFrom     : IDataAllValuesFrom     ): void {}
+    DataHasValue          (dataHasValue          : IDataHasValue          ): void {}
+    DataMinCardinality    (dataMinCardinality    : IDataMinCardinality    ): void {}
+    DataMaxCardinality    (dataMaxCardinality    : IDataMaxCardinality    ): void {}
+    DataExactCardinality  (dataExactCardinality  : IDataExactCardinality  ): void {}
 }
