f1e059e2d3ec4827d56ddcd4b3fe20fd83f81eb1
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-06-04 15:56:33 +0100

    Refactor.

--- Web/ClientApp/src/app/Ontology/TransitiveClosure.ts
+++ Web/ClientApp/src/app/Ontology/TransitiveClosure.ts
@@ -1,62 +1,5 @@
 type AdjacencyMatrix<TVertex> = Map<TVertex, Map<TVertex, boolean>>;
 
-export function TransitiveClosure(
-    adjacencyMatrix: boolean[][]
-    ): boolean[][]
-{
-    const transitiveClosure: boolean[][] = [];
-    for(let rowIndex = 0; rowIndex < adjacencyMatrix.length; ++rowIndex)
-        transitiveClosure[rowIndex] = [...adjacencyMatrix[rowIndex]];
-
-    //for(let k = 0; k < adjacencyMatrix.length; ++k)
-    //    for(let i = 0; i < adjacencyMatrix.length; ++i)
-    //        for(let j = 0; j < adjacencyMatrix.length; ++j)
-    //            transitiveClosure[i][j] = transitiveClosure[i][j] || (transitiveClosure[i][k] && transitiveClosure[k][j]);
-
-    for(let i = 0; i < adjacencyMatrix.length; ++i)
-        for(let j = 0; j < adjacencyMatrix.length; ++j)
-            if(adjacencyMatrix[i][j])
-                TransitiveClosureDfs(
-                    adjacencyMatrix,
-                    transitiveClosure,
-                    i,
-                    j);
-
-    return transitiveClosure;
-}
-
-export function TransitiveClosure2<TVertex>(
-    input: AdjacencyMatrix<TVertex>
-    ): AdjacencyMatrix<TVertex>
-{
-    const keys = [...input.keys()];
-    let matrix: boolean[][] = [];
-    for(let rowIndex = 0; rowIndex < keys.length; ++rowIndex)
-    {
-        const row: boolean[] = [];
-        matrix[rowIndex] = row;
-        for(let columnIndex = 0; columnIndex < keys.length; ++columnIndex)
-            row[columnIndex] = input.get(keys[rowIndex]).get(keys[columnIndex]);
-    }
-
-    matrix = TransitiveClosure(matrix);
-
-    var result = new Map<TVertex, Map<TVertex, boolean>>();
-    for(let rowIndex = 0; rowIndex < keys.length; ++rowIndex)
-    {
-        const row = new Map<TVertex, boolean>();
-        result.set(
-            keys[rowIndex],
-            row);
-        for(let columnIndex = 0; columnIndex < keys.length; ++columnIndex)
-            row.set(
-                keys[columnIndex],
-                matrix[rowIndex][columnIndex]);
-    }
-
-    return result;
-}
-
 function TransitiveClosureDfs(
     adjacencyMatrix  : boolean[][],
     transitiveClosure: boolean[][],
@@ -72,4 +15,60 @@ function TransitiveClosureDfs(
                 transitiveClosure,
                 i,
                 k);
+}
+
+export function TransitiveClosure(
+    adjacencyMatrix: boolean[][]
+    ): boolean[][]
+{
+    const transitiveClosure: boolean[][] = [];
+    for(let rowIndex = 0; rowIndex < adjacencyMatrix.length; ++rowIndex)
+    {
+        transitiveClosure[rowIndex] = [];
+        for(let columnIndex = 0; columnIndex < adjacencyMatrix.length; ++columnIndex)
+            transitiveClosure[rowIndex][columnIndex] = false;
+    }
+
+    for(let i = 0; i < adjacencyMatrix.length; ++i)
+        for(let j = 0; j < adjacencyMatrix.length; ++j)
+            if(adjacencyMatrix[i][j])
+                TransitiveClosureDfs(
+                    adjacencyMatrix,
+                    transitiveClosure,
+                    i,
+                    j);
+
+    return transitiveClosure;
+}
+
+export function TransitiveClosure2<TVertex>(
+    adjacencyMatrix: AdjacencyMatrix<TVertex>
+    ): AdjacencyMatrix<TVertex>
+{
+    const keys = [...adjacencyMatrix.keys()];
+    const transformedAdjacencyMatrix: boolean[][] = [];
+    for(let rowIndex = 0; rowIndex < keys.length; ++rowIndex)
+    {
+        const row: boolean[] = [];
+        transformedAdjacencyMatrix[rowIndex] = row;
+        for(let columnIndex = 0; columnIndex < keys.length; ++columnIndex)
+            row[columnIndex] = adjacencyMatrix.get(keys[rowIndex]).get(keys[columnIndex]);
+    }
+
+    const transformedTransitiveClosure = TransitiveClosure(transformedAdjacencyMatrix);
+
+    const transitiveClosure = new Map<TVertex, Map<TVertex, boolean>>();
+    for(let rowIndex = 0; rowIndex < keys.length; ++rowIndex)
+    {
+        const row = new Map<TVertex, boolean>();
+        transitiveClosure.set(
+            keys[rowIndex],
+            row);
+        for(let columnIndex = 0; columnIndex < keys.length; ++columnIndex)
+            row.set(
+                keys[columnIndex],
+                transformedTransitiveClosure[rowIndex][columnIndex]);
+    }
+
+    return transitiveClosure;
 }
