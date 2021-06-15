a9315cdc7af5560ed1da04302566a07afb05f035
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-06-15 17:44:53 +0100

    Introduce ObservableGenerator.

--- Web/ClientApp/src/app/Ontology/ObjectMinCardinalityObservable.Spec.ts
+++ Web/ClientApp/src/app/Ontology/ObjectMinCardinalityObservable.Spec.ts
@@ -1,10 +1,9 @@
 import { } from 'jasmine';
 import { Class } from './Class';
-import { ClassifierGenerator } from './ClassifierGenerator';
-import { EquivalentClasses } from './EquivalentClasses';
 import { DataPropertyAssertion, NamedIndividual } from './NamedIndividual';
 import { ObjectMinCardinality } from './ObjectMinCardinality';
 import { ObjectOneOf } from './ObjectOneOf';
+import { ObservableGenerator } from './ObservableGenerator';
 import { Ontology } from "./Ontology";
 import { DataProperty, ObjectProperty } from './Property';
 
@@ -12,58 +11,54 @@ describe(
     'ObjectMinCardinality(n OPE) ({ x | #{ y | ( x , y ) ∈ (OPE)OP } ≥ n })',
     () =>
     {
-        const generator = new ClassifierGenerator();
-
         describe(
-            'Given an Ontology o1 with axioms Class(c1), ObjectProperty(op1) and EquivalentClasses(c1 ObjectMinCardinality(0 op1)):',
+            'Given an Ontology o1 with axioms Class(c1), ObjectProperty(op1):',
             () =>
             {
                 const o1 = new Ontology('o1');
-                const c1 = new Class(o1, 'c1');
                 const op1 = new ObjectProperty(o1, 'op1');
-                new EquivalentClasses(o1, [c1, new ObjectMinCardinality(op1, 0)]);
-                const classifier = generator.Generate(o1);
+                const ope = new ObjectMinCardinality(op1, 0);
+                const generator = new ObservableGenerator(o1);
 
                 it(
-                    '(c1)C = ΔI',
-                    () => expect(classifier[2].get(c1)).toBe(classifier[0]));
+                    '(ObjectMinCardinality(0 op1))C = ΔI',
+                    () => expect(generator.ClassExpression(ope)).toBe(generator.ObjectDomain));
 
                 describe(
                     'Given x ∈ ΔI:',
                     () =>
                     {
                         const x = 1;
-                        let   c1Members: Set<any> = null;
-                        const subscription = classifier[2].get(c1).subscribe(m => c1Members = m);
-                        classifier[0].next(new Set<any>([x]));
+                        let opeMembers: Set<any> = null;
+                        const subscription = generator.ClassExpression(ope).subscribe(m => opeMembers = m);
+                        generator.ObjectDomain.next(new Set<any>([x]));
                         it(
-                            'x ∈ (c1)C',
-                            () => expect(c1Members.has(x)).toBe(true));
+                            'x ∈ (ObjectMinCardinality(0 op1))C',
+                            () => expect(opeMembers.has(x)).toBe(true));
                         subscription.unsubscribe();
                     });
             });
 
         describe(
-            'Given an Ontology o1 with axioms Class(c1), ObjectProperty(op1) and EquivalentClasses(c1 ObjectMinCardinality(1 op1)):',
+            'Given an Ontology o1 with axioms Class(c1), ObjectProperty(op1):',
             () =>
             {
                 const o1 = new Ontology('o1');
-                const c1 = new Class(o1, 'c1');
                 const op1 = new ObjectProperty(o1, 'op1');
-                new EquivalentClasses(o1, [c1, new ObjectMinCardinality(op1, 1)]);
-                const classifier = generator.Generate(o1);
+                const ope = new ObjectMinCardinality(op1, 1);
+                const generator = new ObservableGenerator(o1);
 
                 describe(
                     'Given x ∈ ΔI:',
                     () =>
                     {
                         const x = 1;
-                        let c1Members: Set<any> = null;
-                        const subscription = classifier[2].get(c1).subscribe(m => c1Members = m);
-                        classifier[0].next(new Set<any>([x]));
+                        let opeMembers: Set<any> = null;
+                        const subscription = generator.ClassExpression(ope).subscribe(m => opeMembers = m);
+                        generator.ObjectDomain.next(new Set<any>([x]));
                         it(
-                            '¬(x ∈ (c1)C)',
-                            () => expect(c1Members.has(x)).toBe(false));
+                            '¬(x ∈ (ObjectMinCardinality(1 op1))C)',
+                            () => expect(opeMembers.has(x)).toBe(false));
                         subscription.unsubscribe();
                     });
 
@@ -73,13 +68,13 @@ describe(
                     {
                         const x = 1;
                         const y = 2;
-                        let c1Members: Set<any> = null;
-                        const subscription = classifier[2].get(c1).subscribe(m => c1Members = m);
-                        classifier[0].next(new Set<any>([x]));
-                        classifier[1].get('op1').next([[x, y]]);
+                        let opeMembers: Set<any> = null;
+                        const subscription = generator.ClassExpression(ope).subscribe(m => opeMembers = m);
+                        generator.ObjectDomain.next(new Set<any>([x]));
+                        generator.PropertyExpression(op1).next([[x, y]]);
                         it(
-                            'x ∈ (c1)C',
-                            () => expect(c1Members.has(x)).toBe(true));
+                            'x ∈ (ObjectMinCardinality(1 op1))C',
+                            () => expect(opeMembers.has(x)).toBe(true));
                         subscription.unsubscribe();
                     });
             });
@@ -91,20 +86,20 @@ describe(
                 const o1 = new Ontology('o1');
                 const c1 = new Class(o1, 'c1');
                 const op1 = new ObjectProperty(o1, 'op1');
-                new EquivalentClasses(o1, [c1, new ObjectMinCardinality(op1, 2)]);
-                const classifier = generator.Generate(o1);
+                const ope = new ObjectMinCardinality(op1, 2);
+                const generator = new ObservableGenerator(o1);
 
                 describe(
                     'Given x ∈ ΔI:',
                     () =>
                     {
                         const x = 1;
-                        let c1Members: Set<any> = null;
-                        const subscription = classifier[2].get(c1).subscribe(m => c1Members = m);
-                        classifier[0].next(new Set<any>([x]));
+                        let opeMembers: Set<any> = null;
+                        const subscription = generator.ClassExpression(ope).subscribe(m => opeMembers = m);
+                        generator.ObjectDomain.next(new Set<any>([x]));
                         it(
-                            '¬(x ∈ (c1)C)',
-                            () => expect(c1Members.has(x)).toBe(false));
+                            '¬(x ∈ (ObjectMinCardinality(2 op1))C)',
+                            () => expect(opeMembers.has(x)).toBe(false));
                         subscription.unsubscribe();
                     });
 
@@ -114,13 +109,13 @@ describe(
                     {
                         const x = 1;
                         const y = 2;
-                        let c1Members: Set<any> = null;
-                        const subscription = classifier[2].get(c1).subscribe(m => c1Members = m);
-                        classifier[0].next(new Set<any>([x]));
-                        classifier[1].get('op1').next([[x, y]]);
+                        let opeMembers: Set<any> = null;
+                        const subscription = generator.ClassExpression(ope).subscribe(m => opeMembers = m);
+                        generator.ObjectDomain.next(new Set<any>([x]));
+                        generator.PropertyExpression(op1).next([[x, y]]);
                         it(
-                            '¬(x ∈ (c1)C)',
-                            () => expect(c1Members.has(x)).toBe(false));
+                            '¬(x ∈ (ObjectMinCardinality(2 op1))C)',
+                            () => expect(opeMembers.has(x)).toBe(false));
                         subscription.unsubscribe();
                     });
 
@@ -131,13 +126,13 @@ describe(
                         const x = 1;
                         const y = 2;
                         const z = 3;
-                        let   c1Members: Set<any> = null;
-                        const subscription = classifier[2].get(c1).subscribe(m => c1Members = m);
-                        classifier[0].next(new Set<any>([x]));
-                        classifier[1].get('op1').next([[x, y], [x, z]]);
+                        let opeMembers: Set<any> = null;
+                        const subscription = generator.ClassExpression(ope).subscribe(m => opeMembers = m);
+                        generator.ObjectDomain.next(new Set<any>([x]));
+                        generator.PropertyExpression(op1).next([[x, y], [x, z]]);
                         it(
-                            'x ∈ (c1)C',
-                            () => expect(c1Members.has(x)).toBe(true));
+                            'x ∈ (ObjectMinCardinality(2 op1))C',
+                            () => expect(opeMembers.has(x)).toBe(true));
                         subscription.unsubscribe();
                     });
             });
@@ -147,21 +142,17 @@ describe(
     'ObjectMinCardinality( n OPE CE ) ({ x | #{ y | ( x , y ) ∈ (OPE)OP and y ∈ (CE)C } ≥ n })',
     () =>
     {
-        const generator = new ClassifierGenerator();
-
         describe(
-            'Given an Ontology o1 with axioms Class(c1), ObjectProperty(op1), NamedIndividual(i)\
- and EquivalentClasses(c1 ObjectMinCardinality(1 op1 ObjectOneOf([i]))):',
+            'Given an Ontology o1 with axioms Class(c1), ObjectProperty(op1), NamedIndividual(i):',
             () =>
             {
                 const o1 = new Ontology('o1');
                 const i = new NamedIndividual(o1, 'i');
-                const iInterpretation = 10;
-                new DataPropertyAssertion(o1, new DataProperty(o1, 'Id'), i, iInterpretation);
-                const c1 = new Class(o1, 'c1');
+                new DataPropertyAssertion(o1, new DataProperty(o1, 'Id'), i, 10);
                 const op1 = new ObjectProperty(o1, 'op1');
-                new EquivalentClasses(o1, [c1, new ObjectMinCardinality(op1, 1, new ObjectOneOf([i]))]);
-                const classifier = generator.Generate(o1);
+                const ope = new ObjectMinCardinality(op1, 1, new ObjectOneOf([i]));
+                const generator = new ObservableGenerator(o1);
+                const iInterpretation = generator.InterpretIndividual(i);
 
                 describe(
                     'Given (op1)OP = {(x, y)}:',
@@ -169,13 +160,13 @@ describe(
                     {
                         const x = 1;
                         const y = 2;
-                        let c1Members: Set<any> = null;
-                        const subscription = classifier[2].get(c1).subscribe(m => c1Members = m);
-                        classifier[0].next(new Set<any>([x]));
-                        classifier[1].get('op1').next([[x, y]]);
+                        let opeMembers: Set<any> = null;
+                        const subscription = generator.ClassExpression(ope).subscribe(m => opeMembers = m);
+                        generator.ObjectDomain.next(new Set<any>([x]));
+                        generator.PropertyExpression(op1).next([[x, y]]);
                         it(
-                            '¬(x ∈ (c1)C)',
-                            () => expect(c1Members.has(x)).toBe(false));
+                            '¬(x ∈ (ObjectMinCardinality(1 op1 ObjectOneOf(i)))C)',
+                            () => expect(opeMembers.has(x)).toBe(false));
                         subscription.unsubscribe();
                     });
 
@@ -184,13 +175,13 @@ describe(
                     () =>
                     {
                         const x = 1;
-                        let c1Members: Set<any> = null;
-                        const subscription = classifier[2].get(c1).subscribe(m => c1Members = m);
-                        classifier[0].next(new Set<any>([x]));
-                        classifier[1].get('op1').next([[x, iInterpretation]]);
+                        let opeMembers: Set<any> = null;
+                        const subscription = generator.ClassExpression(ope).subscribe(m => opeMembers = m);
+                        generator.ObjectDomain.next(new Set<any>([x]));
+                        generator.PropertyExpression(op1).next([[x, iInterpretation]]);
                         it(
-                            'x ∈ (c1)C',
-                            () => expect(c1Members.has(x)).toBe(true));
+                            'x ∈ ((ObjectMinCardinality(1 op1 ObjectOneOf(i)))C',
+                            () => expect(opeMembers.has(x)).toBe(true));
                         subscription.unsubscribe();
                     });
 
@@ -200,13 +191,13 @@ describe(
                     {
                         const x = 1;
                         const y = 2;
-                        let c1Members: Set<any> = null;
-                        const subscription = classifier[2].get(c1).subscribe(m => c1Members = m);
-                        classifier[0].next(new Set<any>([x]));
-                        classifier[1].get('op1').next([[x, iInterpretation], [x, y]]);
+                        let opeMembers: Set<any> = null;
+                        const subscription = generator.ClassExpression(ope).subscribe(m => opeMembers = m);
+                        generator.ObjectDomain.next(new Set<any>([x]));
+                        generator.PropertyExpression(op1).next([[x, iInterpretation], [x, y]]);
                         it(
-                            'x ∈ (c1)C',
-                            () => expect(c1Members.has(x)).toBe(true));
+                            'x ∈ ((ObjectMinCardinality(1 op1 ObjectOneOf(i)))C',
+                            () => expect(opeMembers.has(x)).toBe(true));
                         subscription.unsubscribe();
                     });
             });
