132ca41821aa67512077e201a0dc8b53c9642dd1
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-06-19 10:10:52 +0100

    Test optimisation for FunctionalDataProperty.

--- Web/ClientApp/src/app/Ontology/DataExactCardinalityObservable.Spec.ts
+++ Web/ClientApp/src/app/Ontology/DataExactCardinalityObservable.Spec.ts
@@ -5,6 +5,7 @@ import { DataOneOf } from './DataOneOf';
 import { IStore, ObservableGenerator, Store } from './ObservableGenerator';
 import { Ontology } from "./Ontology";
 import { DataProperty } from './Property';
+import { FunctionalDataProperty } from './FunctionalDataProperty';
 
 describe(
     'DataExactCardinality( n DPE ) ({ x | #{ y | ( x , y ) ∈ (DPE)DP } = n })',
@@ -76,6 +77,63 @@ describe(
                                 () => expect(members.has(x.Id)).toBe(ce.Cardinality === 2));
                             subscription.unsubscribe();
                         }
+                    });
+            });
+
+            
+        describe(
+            'Given an Ontology o1 with axioms DataProperty(dp1) and FunctionalDataProperty(dp1):',
+            () =>
+            {
+                const o1 = new Ontology('o1');
+                const dp1 = new DataProperty(o1, 'dp1');
+                new FunctionalDataProperty(o1, dp1);
+                const ce = new DataExactCardinality(dp1, 1);
+                const store: IStore = new Store();
+                const generator = new ObservableGenerator(
+                    o1,
+                    store);
+
+                describe(
+                    'Given x ∈ ΔI:',
+                    () =>
+                    {
+                        const x = store.NewEntity<any>();
+                        let members: Set<any> = null;
+                        const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
+                        it(
+                           `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
+                            () => expect(members.has(x.Id)).toBe(false));
+                        subscription.unsubscribe();
+                    });
+
+                describe(
+                    'Given (dp1)DP = {(x, 1)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity<any>();
+                        store.Add(x, dp1.LocalName, 1);
+                        let members: Set<any> = null;
+                        const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(members.has(x.Id)).toBe(true));
+                        subscription.unsubscribe();
+                    });
+
+                describe(
+                    'Given (dp1)DP = {(x, 1), (x, 2)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity<any>();
+                        store.Add(x, dp1.LocalName, 1);
+                        store.Add(x, dp1.LocalName, 2);
+                        let members: Set<any> = null;
+                        const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(members.has(x.Id)).toBe(true));
+                        subscription.unsubscribe();
                     });
             });
     });
