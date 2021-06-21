dccc89475578f76411768a6c1f6d5222a452a96c
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-06-21 12:37:14 +0100

    Refactor.

--- Web/ClientApp/src/app/Ontology/DataMaxCardinalityObservable.Spec.ts
+++ Web/ClientApp/src/app/Ontology/DataMaxCardinalityObservable.Spec.ts
@@ -1,13 +1,13 @@
 import { } from 'jasmine';
 import { ClassExpressionWriter } from './ClassExpressionWriter';
-import { DataMinCardinality } from './DataMinCardinality';
+import { DataMaxCardinality } from './DataMaxCardinality';
 import { DataOneOf } from './DataOneOf';
 import { IStore, ObservableGenerator, Store } from './ObservableGenerator';
 import { Ontology } from "./Ontology";
 import { DataProperty } from './Property';
 
 describe(
-    'DataMinCardinality( n DPE ) ({ x | #{ y | ( x , y ) ∈ (DPE)DP} ≥ n })',
+    'DataMaxCardinality( n DPE ) ({ x | #{ y | ( x , y ) ∈ (DPE)DP } ≤ n })',
     () =>
     {
         const classExpressionWriter = new ClassExpressionWriter();
@@ -18,15 +18,11 @@ describe(
             {
                 const o1 = new Ontology('o1');
                 const dp1 = new DataProperty(o1, 'dp1');
-                const ces = [0, 1, 2].map(cardinality => new DataMinCardinality(dp1, cardinality));
+                const ces = [0, 1, 2].map(cardinality => new DataMaxCardinality(dp1, cardinality));
                 const store: IStore = new Store();
                 const generator = new ObservableGenerator(
                     o1,
                     store);
-
-                it(
-                    `(${classExpressionWriter.Write(ces[0])})C = ΔI`,
-                    () => expect(generator.ClassExpression(ces[0])).toBe(store.ObjectDomain));
 
                 describe(
                     'Given x ∈ ΔI:',
@@ -40,25 +36,25 @@ describe(
                             it(
                                 ce.Cardinality === 0 ?
                                     `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
-                                () => expect(members.has(x)).toBe(ce.Cardinality === 0));
+                                () => expect(members.has(x)).toBe(ce.Cardinality >= 0));
                             subscription.unsubscribe();
                         }
                     });
 
                 describe(
-                    'Given (dp1)DP = {(x, 1)}:',
+                    'Given (dp1)DP = {(x, 2)}:',
                     () =>
                     {
                         const x = store.NewEntity<any>();
-                        store.Add(x, dp1.LocalName, 1);
+                        store.Add(x, dp1.LocalName, 2);
                         for(const ce of ces)
                         {
                             let members: Set<any> = null;
                             const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
                             it(
-                                ce.Cardinality <= 1 ?
+                                ce.Cardinality === 1 ?
                                     `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
-                                () => expect(members.has(x)).toBe(ce.Cardinality <= 1));
+                                () => expect(members.has(x)).toBe(ce.Cardinality >= 1));
                             subscription.unsubscribe();
                         }
                     });
@@ -70,14 +66,14 @@ describe(
                         const x = store.NewEntity<any>();
                         store.Add(x, dp1.LocalName, 1);
                         store.Add(x, dp1.LocalName, 2);
-                        let members: Set<any> = null;
                         for(const ce of ces)
                         {
+                            let members: Set<any> = null;
                             const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
                             it(
-                                ce.Cardinality <= 2 ?
+                                ce.Cardinality === 2 ?
                                     `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
-                                () => expect(members.has(x)).toBe(ce.Cardinality <= 2));
+                                () => expect(members.has(x)).toBe(ce.Cardinality >= 2));
                             subscription.unsubscribe();
                         }
                     });
@@ -85,22 +81,65 @@ describe(
     });
 
 describe(
-    'DataMinCardinality( n DPE DR ) ({ x | #{ y | ( x , y ) ∈ (DPE)DP and y ∈ (DR)DT } ≥ n })',
+    'DataMaxCardinality( n DPE DR ) ({ x | #{ y | ( x , y ) ∈ (DPE)DP and y ∈ (DR)DT } ≤ n })',
     () =>
     {
         const classExpressionWriter = new ClassExpressionWriter();
 
         describe(
-            'Given an Ontology o1 axiom DataProperty(dp1):',
+            'Given an Ontology o1 with axioms Class(c1) and DataProperty(dp1):',
             () =>
             {
                 const o1 = new Ontology('o1');
                 const dp1 = new DataProperty(o1, 'dp1');
-                const ce = new DataMinCardinality(dp1, 1, new DataOneOf([1]));
+                const ce = new DataMaxCardinality(dp1, 1, new DataOneOf([1, 2]));
                 const store: IStore = new Store();
                 const generator = new ObservableGenerator(
                     o1,
                     store);
+
+                describe(
+                    'Given (dp1)DP = {(x, 3)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity<any>();
+                        store.Add(x, dp1.LocalName, 3);
+                        let members: Set<any> = null;
+                        const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(members.has(x)).toBe(true));
+                        subscription.unsubscribe();
+                    });
+
+                describe(
+                    'Given (dp1)DP = {(x, 1)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity<any>();
+                        store.Add(x, dp1.LocalName, 1);
+                        let members: Set<any> = null;
+                        const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(members.has(x)).toBe(true));
+                        subscription.unsubscribe();
+                    });
+
+                describe(
+                    'Given (dp1)DP = {(x, 1), (x, 3)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity<any>();
+                        store.Add(x, dp1.LocalName, 1);
+                        store.Add(x, dp1.LocalName, 3);
+                        let members: Set<any> = null;
+                        const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(members.has(x)).toBe(true));
+                        subscription.unsubscribe();
+                    });
 
                 describe(
                     'Given (dp1)DP = {(x, 2)}:',
@@ -111,17 +150,18 @@ describe(
                         let members: Set<any> = null;
                         const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
                         it(
-                            `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
-                            () => expect(members.has(x)).toBe(false));
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(members.has(x)).toBe(true));
                         subscription.unsubscribe();
                     });
 
                 describe(
-                    'Given (dp1)DP = {(x, 1)}:',
+                    'Given (dp1)DP = {(x, 2), (x, 3)}:',
                     () =>
                     {
                         const x = store.NewEntity<any>();
-                        store.Add(x, dp1.LocalName, 1);
+                        store.Add(x, dp1.LocalName, 2);
+                        store.Add(x, dp1.LocalName, 3);
                         let members: Set<any> = null;
                         const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
                         it(
@@ -135,14 +175,13 @@ describe(
                     () =>
                     {
                         const x = store.NewEntity<any>();
-                        const y = 2;
                         store.Add(x, dp1.LocalName, 1);
                         store.Add(x, dp1.LocalName, 2);
                         let members: Set<any> = null;
                         const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
                         it(
-                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
-                            () => expect(members.has(x)).toBe(true));
+                            '¬(x ∈ (${classExpressionWriter.Write(ce)})C)',
+                            () => expect(members.has(x)).toBe(false));
                         subscription.unsubscribe();
                     });
             });
