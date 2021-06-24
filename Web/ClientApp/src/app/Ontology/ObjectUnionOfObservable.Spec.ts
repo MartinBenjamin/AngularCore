3324fdea093d97b501ece3c3167b4c7a3d51aecb
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-06-24 11:09:34 +0100

    Refactor.

--- Web/ClientApp/src/app/Ontology/ObjectUnionOfObservable.Spec.ts
+++ Web/ClientApp/src/app/Ontology/ObjectUnionOfObservable.Spec.ts
@@ -6,12 +6,15 @@ import { ObjectUnionOf } from './ObjectUnionOf';
 import { IStore, ObservableGenerator, Store } from './ObservableGenerator';
 import { Ontology } from "./Ontology";
 import { DataProperty } from './Property';
+import { IClassExpression } from './IClassExpression';
+import { Subscription } from 'rxjs';
 
 describe(
     'ObjectUnionOf( CE1 ... CEn ) ((CE1)C ∪ ... ∪ (CEn)C)',
     () =>
     {
         const classExpressionWriter = new ClassExpressionWriter();
+
 
         describe(
             'Given an Ontology o1 with axioms NamedIndividual(i1) and NamedIndividual(i2):',
@@ -23,7 +26,9 @@ describe(
                 const i2 = new NamedIndividual(o1, 'i2');
                 new DataPropertyAssertion(o1, id, i1, 1);
                 new DataPropertyAssertion(o1, id, i2, 2);
-                const ce = new ObjectUnionOf([new ObjectOneOf([i1]), new ObjectOneOf([i2])]);
+                const ce1 = new ObjectOneOf([i1]);
+                const ce2 = new ObjectOneOf([i2]);
+                const ce3 = new ObjectUnionOf([ce1, ce2]);
                 const store: IStore = new Store();
                 const generator = new ObservableGenerator(
                     o1,
@@ -31,14 +36,40 @@ describe(
                 const i1Interpretation = generator.InterpretIndividual(i1);
                 const i2Interpretation = generator.InterpretIndividual(i2);
 
-                let members: Set<any> = null;
-                const subscription = generator.ClassExpression(ce).subscribe(m => members = m);
+                function elements(
+                    ce: IClassExpression
+                    ): Set<any>
+                {
+                    let subscription: Subscription;
+                    try
+                    {
+                        let members: Set<any> = null;
+                        subscription = generator.ClassExpression(ce).subscribe(m => members = m);
+                        return members;
+                    }
+                    finally
+                    {
+                        subscription.unsubscribe();
+                    }
+                }
+
                 it(
-                    `(i1)I ∈ (${classExpressionWriter.Write(ce)})C`,
-                    () => expect(members.has(i1Interpretation)).toBe(true));
+                    `(i1)I ∈ (${classExpressionWriter.Write(ce1)})C`,
+                    () => expect(elements(ce1).has(i1Interpretation)).toBe(true));
                 it(
-                    `(i2)I ∈ (${classExpressionWriter.Write(ce)})C`,
-                    () => expect(members.has(i2Interpretation)).toBe(true));
-                subscription.unsubscribe();
+                    `¬((i1)I ∈ (${classExpressionWriter.Write(ce2)})C)`,
+                    () => expect(elements(ce2).has(i1Interpretation)).toBe(false));
+                it(
+                    `(i1)I ∈ (${classExpressionWriter.Write(ce3)})C`,
+                    () => expect(elements(ce3).has(i1Interpretation)).toBe(true));
+                it(
+                    `¬((i2)I ∈ (${classExpressionWriter.Write(ce1)})C)`,
+                    () => expect(elements(ce1).has(i2Interpretation)).toBe(false));
+                it(
+                    `(i2)I ∈ (${classExpressionWriter.Write(ce2)})C`,
+                    () => expect(elements(ce2).has(i2Interpretation)).toBe(true));
+                it(
+                    `(i2)I ∈ (${classExpressionWriter.Write(ce3)})C`,
+                    () => expect(elements(ce3).has(i2Interpretation)).toBe(true));
             });
     });
