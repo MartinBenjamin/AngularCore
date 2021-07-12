3cc9c94de4d8b92d558be75f31c9e94fa1a70fee
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-07-12 11:16:07 +0100

    Add tests for ObjectHasSelf( OPE ).

---
+++ Web/ClientApp/src/app/Ontology/ObjectHasSelfObservable.Spec.ts
@@ -0,0 +1,68 @@
+import { } from 'jasmine';
+import { Subscription } from 'rxjs';
+import { ClassExpressionWriter } from './ClassExpressionWriter';
+import { IClassExpression } from './IClassExpression';
+import { DataPropertyAssertion, NamedIndividual } from './NamedIndividual';
+import { ObjectHasValue } from './ObjectHasValue';
+import { IStore, ObservableGenerator, Store } from './ObservableGenerator';
+import { Ontology } from "./Ontology";
+import { DataProperty, ObjectProperty } from './Property';
+import { ObjectHasSelf } from './ObjectHasSelf';
+
+describe(
+    'ObjectHasSelf( OPE ) ({ x | ( x , x ) ∈ (OPE)OP })',
+    () =>
+    {
+        const classExpressionWriter = new ClassExpressionWriter();
+
+        describe(
+            'Given an Ontology o1:',
+            () =>
+            {
+                const o1 = new Ontology('o1');
+                const op1 = new ObjectProperty(o1, 'op1');
+                const ce = new ObjectHasSelf(op1);
+                const store: IStore = new Store();
+                const generator = new ObservableGenerator(
+                    o1,
+                    store);
+
+                function elements(
+                    ce: IClassExpression
+                    ): Set<any>
+                {
+                    let subscription: Subscription;
+                    try
+                    {
+                        let elements: Set<any> = null;
+                        subscription = generator.ClassExpression(ce).subscribe(m => elements = m);
+                        return elements;
+                    }
+                    finally
+                    {
+                        subscription.unsubscribe();
+                    }
+                }
+
+                describe(
+                    'Given (op1)OP = {}',
+                    () =>
+                    {
+                        const x = store.NewEntity<any>();
+                        it(
+                            `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
+                            () => expect(elements(ce).has(x)).toBe(false));
+                    });
+
+                describe(
+                    'Given (op1)OP = {(x, x)}',
+                    () =>
+                    {
+                        const x = store.NewEntity<any>();
+                        store.Add(x, op1.LocalName, x)
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(elements(ce).has(x)).toBe(true));
+                    });
+            });
+    });
