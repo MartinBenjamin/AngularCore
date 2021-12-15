02e2a9b0805f36b9717c427cede1f5d78b58443c
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2021-12-15 17:42:51 +0000

    Refactor.

--- Data/FacilityFeeTypeLoader.cs
+++ Data/FacilityFeeTypeLoader.cs
@@ -1,6 +1,8 @@
-﻿using FacilityAgreements;
+﻿using CommonDomainObjects;
+using FacilityAgreements;
 using NHibernate;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Data
@@ -28,17 +30,14 @@ namespace Data
 
         async Task IEtl.ExecuteAsync()
         {
-            var facilityFeeTypes = await _csvExtractor.ExtractAsync(
-                _fileName,
-                record => new FacilityFeeType(
-                    new Guid(record[0]),
-                    record[1]));
-
             using(var session = _sessionFactory.OpenSession())
             using(var transaction = session.BeginTransaction())
             {
-                foreach(var facilityFeeType in facilityFeeTypes)
-                    await session.SaveAsync(facilityFeeType);
+                await (
+                    from record in await _csvExtractor.ExtractAsync(_fileName)
+                    select new FacilityFeeType(
+                        new Guid(record[0]),
+                        record[1])).ForEachAsync(facilityFeeType => session.SaveAsync(facilityFeeType));
                 await transaction.CommitAsync();
             }
         }
