0d654ed90daef2e53b4129f67f108c02751ff7cf
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2022-07-30 19:25:53 +0100

    Add SortedList and SortedSet.

---
+++ Web/ClientApp/src/app/Ontology/SortedSet.ts
@@ -0,0 +1,306 @@
+export class SortedList<T>
+{
+    protected _array  : T[] = [];
+    protected _compare: (lhs: T, rhs: T) => number =
+        (lhs, rhs) => lhs === rhs ? 0 : lhs < rhs ? -1 : 1;
+
+    constructor(
+        compare: (lhs: T, rhs: T) => number,
+        values?: Iterable<T>,
+        )
+    {
+        if(compare)
+            this._compare = compare;
+
+        if(values)
+            for(const value of values)
+                this.add(value);
+    }
+
+    public add(
+        value: T
+        ): this
+    {
+        this._array.splice(
+            this.last(value) + 1,
+            0,
+            value);
+
+        return this;
+    }
+
+    first(
+        t: T
+        ): number
+    {
+        if(!this._array.length)
+            return -1;
+
+        return this.findFirst(
+            t,
+            0,
+            this._array.length - 1);
+    }
+
+    lastBefore(
+        t: T
+        ): number
+    {
+        if(!this._array.length)
+            return -1;
+
+        return this.findLastBefore(
+            t,
+            0,
+            this._array.length - 1);
+    }
+
+    last(
+        t: T
+        ): number
+    {
+        if(!this._array.length)
+            return -1;
+
+        return this.findLast(
+            t,
+            0,
+            this._array.length - 1);
+    }
+
+    firstAfter(
+        t: T
+        ): number
+    {
+        if(!this._array.length)
+            return -1;
+
+        return this.findFirstAfter(
+            t,
+            0,
+            this._array.length - 1);
+    }
+
+    private findFirst(
+        t    : T,          
+        start: number,
+        end  : number           
+        ): number
+    {
+        if(start === end)
+            return this._compare(
+                this._array[start],
+                t) === 0 ? start : -1;
+
+        const mid = (start + end) / 2;
+        const midValue = this._array[mid];
+        const comparison = this._compare(midValue, t);
+
+        if(comparison < 0)
+            // midValue < t.
+            return this.findFirst(
+                t,
+                mid + 1,
+                end);
+
+        else
+            // midValue >= t.
+            return this.findFirst(
+                t,
+                start,
+                mid);
+    }
+
+    private findLastBefore(
+        t    : T,
+        start: number,
+        end  : number
+        ): number
+    {
+        if(start === end)
+            return this._compare(
+                this._array[start],
+                t) < 0 ? start : -1;
+
+        const mid = (start + end + 1) / 2;
+        const midValue = this._array[mid];
+        const comparison = this._compare(midValue, t);
+
+        if(comparison < 0)
+            // midValue < t.
+            return this.findLastBefore(
+                t,
+                mid,
+                end);
+
+        else
+            // midValue >= t;
+            return this.findLastBefore(
+                t,
+                start,
+                mid - 1);
+    }
+
+    private findLast(
+        t    : T,          
+        start: number,
+        end  : number           
+        ): number
+    {
+        if(start == end)
+            return this._compare(
+                this._array[start],
+                t) === 0 ? start : -1;
+
+        const mid = (start + end + 1) / 2;
+        const midValue = this._array[mid];
+        const comparison = this._compare(midValue, t);
+
+        if(comparison <= 0)
+            // midValue <= t.
+            return this.findLast(
+                t,
+                mid,
+                end);
+
+        else
+            // midValue > t.
+            return this.findLast(
+                t,
+                start,
+                mid - 1);
+    }
+
+    private findFirstAfter(
+        t    : T,          
+        start: number,
+        end  : number           
+        ): number
+    {
+        if(start == end)
+            return this._compare(
+                this._array[start],
+                t) > 0 ? start : -1;
+
+        const mid = (start + end) / 2;
+        const midValue = this._array[mid];
+        const comparison = this._compare(midValue, t);
+
+        if(comparison <= 0)
+            // midValue <= t.
+            return this.findFirstAfter(
+                t,
+                mid + 1,
+                end);
+
+        else
+            // midValue > t.
+            return this.findFirstAfter(
+                t,
+                start,
+                mid);
+    }
+}
+
+export class SortedSet<T> extends SortedList<T> implements Set<T>
+{
+    constructor(
+        compare: (lhs: T, rhs: T) => number,
+        values?: Iterable<T>
+        )
+    {
+        super(
+            compare,
+            values)
+    }
+
+    add(
+        value: T
+        ): this
+    {
+        const index = this.lastBefore(value) + 1;
+        if(index === this._array.length || this._compare(this._array[index], value) !== 0)
+            this._array.splice(
+                index,
+                0,
+                value);
+
+        return this;
+    }
+
+    clear(): void
+    {
+        this._array = [];
+    }
+
+    delete(
+        value: T
+        ): boolean
+    {
+        const index = this.first(value);
+        if(index !== -1)
+        {
+            this._array.splice(
+                index,
+                1);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    forEach(
+        callbackfn: (value: T, value2: T, set: Set<T>) => void,
+        thisArg?: any
+        ): void
+    {
+        for(const value of this.values())
+            thisArg ?
+                callbackfn.call(
+                    thisArg,
+                    value,
+                    value,
+                    this) :
+                callbackfn(
+                    value,
+                    value,
+                    this);
+    }
+
+    has(
+        value: T
+        ): boolean
+    {
+        return this.first(value) !== -1;
+    }
+
+    get size(): number
+    {
+        return this._array.length;
+    }
+
+    [Symbol.iterator](): IterableIterator<T>
+    {
+        return this.values();
+    }
+
+    *entries(): IterableIterator<[T, T]>
+    {
+        for(const [, value] of this._array.entries())
+            yield [value, value];
+    }
+
+    keys(): IterableIterator<T>
+    {
+        return this.values();
+    }
+
+    *values(): IterableIterator<T>
+    {
+        for(const [, value] of this._array.entries())
+            yield value;
+    }
+
+    [Symbol.toStringTag]: string;
+}
