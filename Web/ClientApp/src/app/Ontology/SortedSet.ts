35466a4f32037022e3e1d0e87aff0b147aac7d06
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2022-07-31 12:41:18 +0100

    Refactor.

--- Web/ClientApp/src/app/Ontology/SortedSet.ts
+++ Web/ClientApp/src/app/Ontology/SortedSet.ts
@@ -21,16 +21,10 @@ export class SortedList<T>
         value: T
         ): this
     {
-        const index = this.firstAfter(value);
-
-        if(index === -1)
-            this._array.push(value);
-
-        else
-            this._array.splice(
-                this.firstAfter(value),
-                0,
-                value);
+        this._array.splice(
+            this.lastBefore(value) + 1,
+            0,
+            value);
 
         return this;
     }
