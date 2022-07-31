fdda96b07659af56a6d2ab25c9d3a0128f750716
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2022-07-31 12:33:58 +0100

    Refactor.

--- Web/ClientApp/src/app/Ontology/SortedList.Spec.ts
+++ Web/ClientApp/src/app/Ontology/SortedList.Spec.ts
@@ -20,9 +20,6 @@ describe(
                 for(let value = 0; value <= 6; ++value)
                     assert(`list.first(${value}) === ${values.indexOf(value)}`);
 
-                for(let value = 0; value <= 6; ++value)
-                    assert(`list.last(${value}) === ${values.lastIndexOf(value)}`);
-
                 assert('list.lastBefore(0) === -1');
                 assert('list.lastBefore(1) === -1');
                 assert('list.lastBefore(2) ===  1');
@@ -31,17 +28,15 @@ describe(
                 assert('list.lastBefore(5) ===  4');
                 assert('list.lastBefore(6) ===  8');
 
-                list.first(3);
-                //assert('values.every(value => set.has(value))');
-                //assert('set.has([])');
-                //assert('set.has([0])');
-                //assert('set.has([0, 1])');
-                //assert('set.has([0, 2])');
-                //assert('!set.has([0, 3])');
+                for(let value = 0; value <= 6; ++value)
+                    assert(`list.last(${value}) === ${values.lastIndexOf(value)}`);
 
-                //assert('[...set.values()].length === 4');
-                //assert('[...set.values()].every(value0 => values.some(value1 => value0.length === value1.length && value0.every((element, index) => element === value1[index])))');
-                //assert('[...set.keys()].length === 4');
-                //assert('[...set.keys()].every(key => values.some(value => key.length === value.length && key.every((element, index) => element === value[index])))');
+                assert('list.firstAfter(0) ===  0');
+                assert('list.firstAfter(1) ===  2');
+                assert('list.firstAfter(2) ===  2');
+                assert('list.firstAfter(3) ===  5');
+                assert('list.firstAfter(4) ===  5');
+                assert('list.firstAfter(5) === -1');
+                assert('list.firstAfter(6) === -1');
             });
     });
