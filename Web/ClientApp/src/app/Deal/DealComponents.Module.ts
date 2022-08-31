9cb1f80a0454e902f7e3191ca589d242ac87be11
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2022-08-31 12:27:43 +0100

    Refactor.

--- Web/ClientApp/src/app/Deal/DealComponents.Module.ts
+++ Web/ClientApp/src/app/Deal/DealComponents.Module.ts
@@ -7,39 +7,13 @@ import { ErrorModule } from '../Components/ErrorModule';
 import { NumberModule } from '../Components/NumberModule';
 import { TabbedViewModule } from '../Components/TabbedView';
 import { YesNoPipeModule } from '../Components/YesNoPipe';
-import { GeographicRegionSelector } from '../GeographicRegionSelector';
 import { GeographicRegionSelectorModule } from '../GeographicRegionSelector.Module';
-import { LegalEntityFinder, LegalEntityFinderButtons } from '../LegalEntityFinder';
 import { LegalEntityFinderModule } from '../LegalEntityFinder.Module';
-import { Borrowers } from './Borrowers';
-import { ExternalFunding } from './ExternalFunding';
-import { Facility } from './Facility';
-import { FacilityErrors } from './FacilityErrors';
-import { FacilityFeeEditor } from './FacilityFeeEditor';
-import { FacilityFeeErrors } from './FacilityFeeErrors';
-import { FacilityFees } from './FacilityFees';
-import { FacilityTab } from './FacilityTab';
-import { FacilityTab1 } from './FacilityTab1';
-import { FacilityTab3 } from './FacilityTab3';
-import { Restricted } from './Restricted';
-import { Sponsors } from './Sponsors';
 
 @NgModule(
     {
         declarations:
             [
-                Borrowers,
-                ExternalFunding,
-                Facility,
-                FacilityErrors,
-                FacilityFeeEditor,
-                FacilityFeeErrors,
-                FacilityFees,
-                FacilityTab,
-                FacilityTab1,
-                FacilityTab3,
-                Restricted,
-                Sponsors
             ],
         imports:
             [
@@ -56,16 +30,6 @@ import { Sponsors } from './Sponsors';
             ],
         exports:
             [
-                Borrowers,
-                Facility,
-                FacilityErrors,
-                FacilityFeeEditor,
-                FacilityFeeErrors,
-                GeographicRegionSelector,
-                LegalEntityFinder,
-                LegalEntityFinderButtons,
-                Restricted,
-                Sponsors
             ]
     })
 export class DealComponentsModule
--- Web/ClientApp/src/app/Deal/FacilityTab.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component(
-    {
-        templateUrl: './FacilityTab.html'
-    })
-export class FacilityTab
-{
-}
