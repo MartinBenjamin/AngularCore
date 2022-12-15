b853559b45e944ca98139c8133f7e95499f18934
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2022-12-15 16:28:30 +0000

    Add tests.

---
+++ Web/ClientApp/src/app/Ontology/DataMaxCardinalitySignal.Spec.ts
@@ -0,0 +1,196 @@
+import { } from 'jasmine';
+import { Signal } from '../Signal';
+import { ClassExpressionSignalInterpreter } from './ClassExpressionSignalInterpreter';
+import { ClassExpressionWriter } from './ClassExpressionWriter';
+import { DataMaxCardinality } from './DataMaxCardinality';
+import { DataOneOf } from './DataOneOf';
+import { IClassExpression } from './IClassExpression';
+import { EavStore, IEavStore } from './ObservableGenerator';
+import { Ontology } from "./Ontology";
+import { DataProperty } from './Property';
+
+describe(
+    'DataMaxCardinality( n DPE ) ({ x | #{ y | ( x , y ) ∈ (DPE)DP } ≤ n })',
+    () =>
+    {
+        const classExpressionWriter = new ClassExpressionWriter();
+
+        describe(
+            'Given an Ontology o1 with axiom DataProperty(dp1):',
+            () =>
+            {
+                const o1 = new Ontology('o1');
+                const dp1 = new DataProperty(o1, 'dp1');
+                const ces = [0, 1, 2].map(cardinality => new DataMaxCardinality(dp1, cardinality));
+                const store: IEavStore = new EavStore();
+                const interpreter = new ClassExpressionSignalInterpreter(
+                    o1,
+                    store);
+
+                function elements(
+                    ce: IClassExpression
+                    ): Set<any>
+                {
+                    let signal: Signal<Set<any>>;
+                    let elements: Set<any> = null;
+                    try
+                    {
+                        signal = store.SignalScheduler.AddSignal(
+                            m => elements = m,
+                            [interpreter.ClassExpression(ce)]);
+                        return elements;
+                    }
+                    finally
+                    {
+                        store.SignalScheduler.RemoveSignal(signal);
+                    }
+                }
+
+                describe(
+                    'Given x ∈ ΔI:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        for(const ce of ces)
+                            it(
+                                ce.Cardinality === 0 ?
+                                    `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
+                                () => expect(elements(ce).has(x)).toBe(ce.Cardinality >= 0));
+                    });
+
+                describe(
+                    'Given (dp1)DP = {(x, 2)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        store.Assert(x, dp1.LocalName, 2);
+                        for(const ce of ces)
+                            it(
+                                ce.Cardinality === 1 ?
+                                    `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
+                                () => expect(elements(ce).has(x)).toBe(ce.Cardinality >= 1));
+                    });
+
+                describe(
+                    'Given (dp1)DP = {(x, 1), (x, 2)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        store.Assert(x, dp1.LocalName, 1);
+                        store.Assert(x, dp1.LocalName, 2);
+                        for(const ce of ces)
+                            it(
+                                ce.Cardinality === 2 ?
+                                    `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
+                                () => expect(elements(ce).has(x)).toBe(ce.Cardinality >= 2));
+                    });
+            });
+    });
+
+describe(
+    'DataMaxCardinality( n DPE DR ) ({ x | #{ y | ( x , y ) ∈ (DPE)DP and y ∈ (DR)DT } ≤ n })',
+    () =>
+    {
+        const classExpressionWriter = new ClassExpressionWriter();
+
+        describe(
+            'Given an Ontology o1 with axioms Class(c1) and DataProperty(dp1):',
+            () =>
+            {
+                const o1 = new Ontology('o1');
+                const dp1 = new DataProperty(o1, 'dp1');
+                const ce = new DataMaxCardinality(dp1, 1, new DataOneOf([1, 2]));
+                const store: IEavStore = new EavStore();
+                const interpreter = new ClassExpressionSignalInterpreter(
+                    o1,
+                    store);
+
+                function elements(
+                    ce: IClassExpression
+                    ): Set<any>
+                {
+                    let signal: Signal<Set<any>>;
+                    let elements: Set<any> = null;
+                    try
+                    {
+                        signal = store.SignalScheduler.AddSignal(
+                            m => elements = m,
+                            [interpreter.ClassExpression(ce)]);
+                        return elements;
+                    }
+                    finally
+                    {
+                        store.SignalScheduler.RemoveSignal(signal);
+                    }
+                }
+
+                describe(
+                    'Given (dp1)DP = {(x, 3)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        store.Assert(x, dp1.LocalName, 3);
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(elements(ce).has(x)).toBe(true));
+                    });
+
+                describe(
+                    'Given (dp1)DP = {(x, 1)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        store.Assert(x, dp1.LocalName, 1);
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(elements(ce).has(x)).toBe(true));
+                    });
+
+                describe(
+                    'Given (dp1)DP = {(x, 1), (x, 3)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        store.Assert(x, dp1.LocalName, 1);
+                        store.Assert(x, dp1.LocalName, 3);
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(elements(ce).has(x)).toBe(true));
+                    });
+
+                describe(
+                    'Given (dp1)DP = {(x, 2)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        store.Assert(x, dp1.LocalName, 2);
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(elements(ce).has(x)).toBe(true));
+                    });
+
+                describe(
+                    'Given (dp1)DP = {(x, 2), (x, 3)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        store.Assert(x, dp1.LocalName, 2);
+                        store.Assert(x, dp1.LocalName, 3);
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(elements(ce).has(x)).toBe(true));
+                    });
+
+                describe(
+                    'Given (dp1)DP = {(x, 1), (x, 2)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        store.Assert(x, dp1.LocalName, 1);
+                        store.Assert(x, dp1.LocalName, 2);
+                        it(
+                            `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
+                            () => expect(elements(ce).has(x)).toBe(false));
+                    });
+            });
+    });
