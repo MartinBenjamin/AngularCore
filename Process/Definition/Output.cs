5863bd68ab46ebeae67ca4ea959db6449b8b8eb4
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2024-01-23 11:51:54 +0000

    Add Output.

---
+++ Process/Definition/Output.cs
@@ -0,0 +1,32 @@
+﻿using Process.Expression;
+using System;
+
+namespace Process.Definition
+{
+    public class Output: IO
+    {
+        public Type                Type   { get; protected set; }
+        public IExpression<object> Source { get; protected set; }
+
+        public Output(
+            IExpression<string> channel,
+            Type                type,
+            IExpression<object> source
+            )
+            : base(channel)
+        {
+            Type   = type;
+            Source = source;
+        }
+
+        public override bool Accept(
+            IVisitor visitor
+            ) => visitor.Enter(this);
+
+        public override global::Process.Process New(
+            global::Process.Process parent
+            ) => new global::Process.Output(
+                this,
+                parent);
+    }
+}
---
+++ Process/Output.cs
@@ -0,0 +1,28 @@
+﻿namespace Process
+{
+    public class Output: IO
+    {
+        protected Output()
+            : base()
+        {
+        }
+
+        public Output(
+            Definition.Output definition,
+            Process           parent
+            )
+            : base(
+                definition,
+                parent)
+        {
+        }
+
+        public virtual object ExecuteOutput(
+            IExecutionService executionService
+            )
+        {
+            base.ExecuteIO(executionService);
+            return ((Definition.Output)Definition).Source.Evaluate(this);
+        }
+    }
+}
