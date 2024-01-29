591d5fe1db3f6c1528e8b49a986819959a84ff05
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2024-01-29 12:33:52 +0000

    Refactor.

--- Process/ExecutionService.cs
+++ Process/ExecutionService.cs
@@ -1,7 +1,4 @@
-﻿using Process.Definition;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 
 namespace Process
 {
@@ -9,9 +6,6 @@ namespace Process
     {
         private Queue<IExecutable> _queue = new Queue<IExecutable>();
         private int                _entered;
-
-        private IDictionary<Channel, ISet<IO>                   > _awaitSync = new Dictionary<Channel, ISet<IO>>();
-        private IDictionary<Channel, (ISet<Input>, ISet<Output>)> _awaitIO   = new Dictionary<Channel, (ISet<Input>, ISet<Output>)>();
 
         void IExecutionService.Save(
             IExecutable executable
@@ -44,99 +38,6 @@ namespace Process
             {
                 _entered -= 1;
             }
-        }
-
-        private void IO()
-        {
-            foreach(var ios in _awaitSync.Values)
-                if(ios.Count >= 2)
-                    foreach(var io in ios.ToArray())
-                        io.ExecuteIO(this);
-
-            foreach(var (inputs, outputs) in _awaitIO.Values)
-                if(inputs.Count > 0 && outputs.Count > 0)
-                {
-                    var outputArray = outputs.ToArray();
-                    var inputArray = inputs.ToArray();
-
-                    for(var index = 0; index < Math.Min(outputArray.Length, inputArray.Length);++index)
-                        inputArray[index].Executelnput(
-                            this,
-                            outputArray[index].ExecuteOutput(this));
-                }
-        }
-
-        void IExecutionService.Register(IO io)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IExecutionService.Register(
-            Input input
-            )
-        {
-            (ISet<Input> Inputs, ISet<Output> Outputs) inputsOutputs;
-            var previousSyncCount = 0;
-            if(_awaitIO.TryGetValue(
-                input.Channel,
-                out inputsOutputs))
-            {
-                previousSyncCount = Math.Min(inputsOutputs.Inputs.Count, inputsOutputs.Outputs.Count);
-                inputsOutputs.Inputs.Add(input);
-            }
-
-            else
-                _awaitIO[input.Channel] = (new HashSet<Input>() { input }, new HashSet<Output>());
-
-            if(previousSyncCount == 0 && Math.Min(inputsOutputs.Inputs.Count, inputsOutputs.Outputs.Count) > 0)
-            { };
-        }
-
-        void IExecutionService.Register(
-            Output output
-            )
-        {
-            //(ISet<Input> Inputs, ISet<Output> Outputs) inputsOutputs;
-            //var previousSyncCount = 0;
-            //var channel = ((Definition.IO)output.Definition).Channel.Evaluate(output);
-            //if(_awaitIO.TryGetValue(
-            //    channel,
-            //    out inputsOutputs))
-            //{
-            //    previousSyncCount = Math.Min(inputsOutputs.Inputs.Count, inputsOutputs.Outputs.Count);
-            //    inputsOutputs.Outputs.Add(output);
-            //}
-
-            //else
-            //    _awaitIO[channel] = (new HashSet<Input>(), new HashSet<Output>() { output });
-
-            //if(previousSyncCount == 0 && Math.Min(inputsOutputs.Inputs.Count, inputsOutputs.Outputs.Count) > 0)
-            //{ };
-        }
-
-        void IExecutionService.Deregister(IO io)
-        {
-            throw new NotImplementedException();
-        }
-
-        void IExecutionService.Deregister(
-            Input input
-            )
-        {
-            //if(_awaitIO.TryGetValue(
-            //    ((Definition.IO)input.Definition).Channel.Evaluate(input),
-            //    out (ISet<Input> Inputs, ISet<Output>) inputsOutputs))
-            //    inputsOutputs.Inputs.Remove(input);
-        }
-
-        void IExecutionService.Deregister(
-            Output output
-            )
-        {
-            //if(_awaitIO.TryGetValue(
-            //    ((Definition.IO)output.Definition).Channel.Evaluate(output),
-            //    out (ISet<Input>, ISet<Output> Outputs) inputsOutputs))
-            //    inputsOutputs.Outputs.Remove(output);
         }
     }
 }
---
+++ Process/Synchronisation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Process
+{
+    public class Synchronisation: IExecutable
+    {
+        private ISet<Input > _inputs  = new HashSet<Input >();
+        private ISet<Output> _outputs = new HashSet<Output>();
+
+        public int SyncCount
+        {
+            get => Math.Min(_inputs.Count, _outputs.Count);
+        }
+
+        void Register(
+            IExecutionService executionService,
+            Input             input
+            )
+        {
+            var previousSyncCount = SyncCount;
+            _inputs.Add(input);
+
+            if(previousSyncCount == 0 && SyncCount > 0)
+                executionService.Execute(this);
+        }
+
+        void Register(
+            IExecutionService executionService,
+            Output            output
+            )
+        {
+            var previousSyncCount = SyncCount;
+            _outputs.Add(output);
+
+            if(previousSyncCount == 0 && SyncCount > 0)
+                executionService.Execute(this);
+        }
+
+        void Deregister(
+            Input input
+            ) => _inputs.Remove(input);
+
+        void Deregister(
+            Output output
+            ) => _outputs.Remove(output);
+
+        void IExecutable.Execute(
+            IExecutionService executionService
+            )
+        {
+            var outputs = _outputs.ToArray();
+            var inputs  = _inputs.ToArray();
+            var count   = SyncCount;
+
+            for(var index = 0;index < count;++index)
+                inputs[index].Executelnput(
+                    executionService,
+                    outputs[index].ExecuteOutput(executionService));
+        }
+    }
+}
