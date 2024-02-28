5bdb9753b40bc0587094dbbaa709f2a8484ed2e9
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2024-02-28 12:08:19 +0000

    Refactor.

--- Web/ClientApp/src/app/EavStore/EavStore.Signal.Spec.ts
+++ Web/ClientApp/src/app/EavStore/EavStore.Signal.Spec.ts
@@ -607,16 +607,15 @@ signal = store.Signal(['?result'], [['rule', '?result']], [['rule', 3], []], [['
 store.SignalScheduler.AddSignal(result => trace.push(result), [signal])`,
                     () =>
                     {
-                        const trace: any[][][] = [];
+                        const trace: Set<any[]>[] = [];
                         const assert = assertBuilder('Store', 'store', 'e', 'trace')
                             (Store, store, e, trace);
                         const signal = store.Signal(['?result'], [['rule', '?result']], [['rule', 3], []], [['rule', 3], []]);
-                        const traceSignal = store.SignalScheduler.AddSignal(result => trace.push(result), [signal]);
+                        const traceSignal = store.SignalScheduler.AddSignal(result => trace.push(new ArraySet(result)), [signal]);
                         store.SignalScheduler.RemoveSignal(traceSignal);
                         assert('trace.length === 1');
-                        assert('trace[0].length === 1');
-                        assert('trace[0][0].length === 1');
-                        assert('trace[0][0][0] === 3');
+                        assert('trace[0].size === 1');
+                        assert('trace[0].has([3])');
                     });
 
                 describe(
