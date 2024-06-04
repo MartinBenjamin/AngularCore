34c16ff2946b107550f35419c9dc9574000945f4
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2024-06-04 12:22:20 +0100

    Refactor.

--- Web/ClientApp/src/app/Ontology/ClassAssertionDatalog.Spec.ts
+++ Web/ClientApp/src/app/Ontology/ClassAssertionDatalog.Spec.ts
@@ -21,21 +21,21 @@ describe(
         const classExpressionWriter = new ClassExpressionWriter();
 
         describe(
-            'Given an Ontology o with axioms Class(c), NamedIndividual(i) and ClassAssertion(c, i):',
+            'Given an Ontology o1 with axioms Class(c1), NamedIndividual(i1) and ClassAssertion(c1, i1):',
             () =>
             {
-                const o: IOntology = new Ontology('o');
-                const c = new Class(o, 'c');
-                const i = new NamedIndividual(o, 'i');
-                new ClassAssertion(o, c, i);
+                const o1: IOntology = new Ontology('o1');
+                const c1 = new Class(o1, 'c1');
+                const i1 = new NamedIndividual(o1, 'i1');
+                new ClassAssertion(o1, c1, i1);
                 const store: IEavStore = new EavStore();
                 const rules: Rule[] = [];
                 const interpreter = new AxiomInterpreter(
-                    o,
+                    o1,
                     store,
                     rules);
-                const iInterpretation = interpreter.InterpretIndividual(i);
-                for(const axiom of o.Axioms)
+                const i1Interpretation = interpreter.InterpretIndividual(i1);
+                for(const axiom of o1.Axioms)
                     axiom.Accept(interpreter);
 
                 console.log(JSON.stringify(rules));
@@ -43,20 +43,20 @@ describe(
                     store,
                     tupleCompare);
 
-                const signal = store.Signal(['?x'], [[c.Iri, '?x']], ...rules);
+                const signal = store.Signal(['?x'], [[c1.Iri, '?x']], ...rules);
                 const value = new SortedSet(tupleCompare, store.SignalScheduler.Sample(signal));
 
                 it(
-                    `(i)I ∈ (${classExpressionWriter.Write(c)})C`,
-                    () => expect(value.has([iInterpretation])).toBe(true));
+                    `(i1)I ∈ (${classExpressionWriter.Write(c1)})C`,
+                    () => expect(value.has([i1Interpretation])).toBe(true));
 
                 const signals = datalogInterpreter.Rules(rules);
-                const signal1 = signals.get(c.Iri);
+                const signal1 = signals.get(c1.Iri);
 
                 const value1 = new SortedSet(tupleCompare, store.SignalScheduler.Sample(signal1));
 
                 it(
-                    `(i)I ∈ (${classExpressionWriter.Write(c)})C`,
-                    () => expect(value1.has([c.Iri, iInterpretation])).toBe(true));
+                    `(i1)I ∈ (${classExpressionWriter.Write(c1)})C`,
+                    () => expect(value1.has([c1.Iri, i1Interpretation])).toBe(true));
             });
     });
