cd753888bc56c55a5ad8e46a23dea7f8a7d4e328
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2025-03-10 18:31:25 +0000

    Refactor.

--- Web/ClientApp/src/app/Ontology/DataPropertyAssertionDatalog.Spec.ts
+++ Web/ClientApp/src/app/Ontology/DataPropertyAssertionDatalog.Spec.ts
@@ -5,18 +5,19 @@ import { EavStore, tupleCompare } from '../EavStore/EavStore';
 import { IEavStore } from '../EavStore/IEavStore';
 import { DataPropertyAssertion } from './Assertion';
 import { AxiomInterpreter } from './AxiomInterpreterDatalog';
-import { AxiomWriter } from './AxiomWriter';
 import { IOntology } from './IOntology';
 import { NamedIndividual } from './NamedIndividual';
 import { Ontology } from "./Ontology";
+import { OntologyWriter } from './OntologyWriter';
 import { DataProperty } from './Property';
+import { PropertyExpressionWriter } from './PropertyExpressionWriter';
 
 describe(
     'DataPropertyAssertion( DPE a lt ) (( (a)I , (lt)LT ) ∈ (DPE)DP)',
     () =>
     {
-        const axiomWriter = new AxiomWriter();
-        const propertyExpressionWriter = axiomWriter.PropertyExpressionWriter;
+        const ontologyWriter = OntologyWriter();
+        const propertyExpressionWriter = new PropertyExpressionWriter();
         const o1: IOntology = new Ontology('o1');
         const i1 = new NamedIndividual(o1, 'i1');
         const i2 = new NamedIndividual(o1, 'i2');
@@ -25,7 +26,7 @@ describe(
         new DataPropertyAssertion(o1, dp1, i2, 2);
 
         describe(
-            `Given an Ontology o1 with axioms ${o1.Axioms.map(axiom => axiom.Select(axiomWriter)).join(', ')}:`,
+            `Given ${ontologyWriter(o1)}:`,
             () =>
             {
                 const store: IEavStore = new EavStore();
@@ -41,22 +42,20 @@ describe(
 
                 //console.log(JSON.stringify(rules));
 
-                {
-                    const dp1Signal = store.Signal(['?x', '?y'], [[dp1.Iri, '?x', '?y']], ...rules);
-                    const dp1Interpretation = new SortedSet(tupleCompare, store.SignalScheduler.Sample(dp1Signal));
-                    store.SignalScheduler.RemoveSignal(dp1Signal);
-                    it(
-                        `( (i1)I , (1)LT ) ∈ (${dp1.Select(propertyExpressionWriter)})DP`,
-                        () => expect(dp1Interpretation.has([i1Interpretation, 1])).toBe(true));
-                    it(
-                        `¬(( (i1)I , (2)LT ) ∈ (${dp1.Select(propertyExpressionWriter)})DP)`,
-                        () => expect(dp1Interpretation.has([i1Interpretation, 2])).toBe(false));
-                    it(
-                        `¬(( (i2)I , (1)LT ) ∈ (${dp1.Select(propertyExpressionWriter)})DP)`,
-                        () => expect(dp1Interpretation.has([i2Interpretation, 1])).toBe(false));
-                    it(
-                        `( (i2)I , (2)LT ) ∈ (${dp1.Select(propertyExpressionWriter)})DP`,
-                        () => expect(dp1Interpretation.has([i2Interpretation, 2])).toBe(true));
-                }
+                const dp1Signal = store.Signal(['?x', '?y'], [[dp1.Iri, '?x', '?y']], ...rules);
+                const dp1Interpretation = new SortedSet(tupleCompare, store.SignalScheduler.Sample(dp1Signal));
+                store.SignalScheduler.RemoveSignal(dp1Signal);
+                it(
+                    `( (i1)I , (1)LT ) ∈ (${dp1.Select(propertyExpressionWriter)})DP`,
+                    () => expect(dp1Interpretation.has([i1Interpretation, 1])).toBe(true));
+                it(
+                    `¬(( (i1)I , (2)LT ) ∈ (${dp1.Select(propertyExpressionWriter)})DP)`,
+                    () => expect(dp1Interpretation.has([i1Interpretation, 2])).toBe(false));
+                it(
+                    `¬(( (i2)I , (1)LT ) ∈ (${dp1.Select(propertyExpressionWriter)})DP)`,
+                    () => expect(dp1Interpretation.has([i2Interpretation, 1])).toBe(false));
+                it(
+                    `( (i2)I , (2)LT ) ∈ (${dp1.Select(propertyExpressionWriter)})DP`,
+                    () => expect(dp1Interpretation.has([i2Interpretation, 2])).toBe(true));
             });
     });
--- Web/ClientApp/src/app/Ontology/ObjectPropertyAssertionDatalog.Spec.ts
+++ Web/ClientApp/src/app/Ontology/ObjectPropertyAssertionDatalog.Spec.ts
@@ -5,18 +5,19 @@ import { EavStore, tupleCompare } from '../EavStore/EavStore';
 import { IEavStore } from '../EavStore/IEavStore';
 import { ObjectPropertyAssertion } from './Assertion';
 import { AxiomInterpreter } from './AxiomInterpreterDatalog';
-import { AxiomWriter } from './AxiomWriter';
 import { IOntology } from './IOntology';
 import { NamedIndividual } from './NamedIndividual';
 import { Ontology } from "./Ontology";
+import { OntologyWriter } from './OntologyWriter';
 import { ObjectProperty } from './Property';
+import { PropertyExpressionWriter } from './PropertyExpressionWriter';
 
 describe(
     'ObjectPropertyAssertion( OPE a1 a2 ) (( (a1)I , (a2)I ) ∈ (OPE)OP)',
     () =>
     {
-        const axiomWriter = new AxiomWriter();
-        const propertyExpressionWriter = axiomWriter.PropertyExpressionWriter;
+        const ontologyWriter = OntologyWriter();
+        const propertyExpressionWriter = new PropertyExpressionWriter();
         const o1: IOntology = new Ontology('o1');
         const i1 = new NamedIndividual(o1, 'i1');
         const i2 = new NamedIndividual(o1, 'i2');
@@ -24,7 +25,7 @@ describe(
         new ObjectPropertyAssertion(o1, op1, i1, i2);
 
         describe(
-            `Given an Ontology o1 with axioms ${o1.Axioms.map(axiom => axiom.Select(axiomWriter)).join(', ')}:`,
+            `Given ${ontologyWriter(o1)}:`,
             () =>
             {
                 const store: IEavStore = new EavStore();
@@ -40,16 +41,14 @@ describe(
 
                 //console.log(JSON.stringify(rules));
 
-                {
-                    const op1Signal = store.Signal(['?x', '?y'], [[op1.Iri, '?x', '?y']], ...rules);
-                    const op1Interpretation = new SortedSet(tupleCompare, store.SignalScheduler.Sample(op1Signal));
-                    store.SignalScheduler.RemoveSignal(op1Signal);
-                    it(
-                        `( (i1)I , (i2)I ) ∈ (${op1.Select(propertyExpressionWriter)})OP`,
-                        () => expect(op1Interpretation.has([i1Interpretation, i2Interpretation])).toBe(true));
-                    it(
-                        `¬(( (i2)I , (i1)I ) ∈ (${op1.Select(propertyExpressionWriter)})OP)`,
-                        () => expect(op1Interpretation.has([i2Interpretation, i1Interpretation])).toBe(false));
-                }
+                const op1Signal = store.Signal(['?x', '?y'], [[op1.Iri, '?x', '?y']], ...rules);
+                const op1Interpretation = new SortedSet(tupleCompare, store.SignalScheduler.Sample(op1Signal));
+                store.SignalScheduler.RemoveSignal(op1Signal);
+                it(
+                    `( (i1)I , (i2)I ) ∈ (${op1.Select(propertyExpressionWriter)})OP`,
+                    () => expect(op1Interpretation.has([i1Interpretation, i2Interpretation])).toBe(true));
+                it(
+                    `¬(( (i2)I , (i1)I ) ∈ (${op1.Select(propertyExpressionWriter)})OP)`,
+                    () => expect(op1Interpretation.has([i2Interpretation, i1Interpretation])).toBe(false));
             });
     });
