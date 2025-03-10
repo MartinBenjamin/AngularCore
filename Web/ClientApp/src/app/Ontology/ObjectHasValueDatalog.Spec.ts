3e8be6d8c067ac1dff415669492bca887646132f
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2025-03-10 13:43:23 +0000

    Refactor.

--- Web/ClientApp/src/app/Ontology/ObjectHasValueDatalog.Spec.ts
+++ Web/ClientApp/src/app/Ontology/ObjectHasValueDatalog.Spec.ts
@@ -3,11 +3,13 @@ import { SortedSet } from '../Collections/SortedSet';
 import { Rule } from '../EavStore/Datalog';
 import { EavStore, tupleCompare } from '../EavStore/EavStore';
 import { IEavStore } from '../EavStore/IEavStore';
-import { ObjectPropertyAssertion } from './Assertion';
+import { Tuple } from '../EavStore/Tuple';
+import { Signal } from '../Signal/Signal';
 import { AxiomInterpreter } from './AxiomInterpreterDatalog';
 import { Class } from './Class';
 import { ClassExpressionWriter } from './ClassExpressionWriter';
 import { EquivalentClasses } from './EquivalentClasses';
+import { IClass } from './IClass';
 import { NamedIndividual } from './NamedIndividual';
 import { ObjectHasValue } from './ObjectHasValue';
 import { Ontology } from "./Ontology";
@@ -23,11 +25,9 @@ describe(
         const o1 = new Ontology('o1');
         const op1 = new ObjectProperty(o1, 'op1');
         const i1 = new NamedIndividual(o1, 'i1');
-        const i2 = new NamedIndividual(o1, 'i2');
         const c1 = new Class(o1, 'c1');
-        const ce = new ObjectHasValue(op1, i2);
+        const ce = new ObjectHasValue(op1, i1);
         new EquivalentClasses(o1, [c1, ce]);
-        new ObjectPropertyAssertion(o1, op1, i1, i2);
 
         describe(
             `Given ${ontologyWriter(o1)}:`,
@@ -40,21 +40,47 @@ describe(
                     store,
                     rules);
                 const i1Interpretation = interpreter.InterpretIndividual(i1);
-                const i2Interpretation = interpreter.InterpretIndividual(i2);
                 for(const axiom of o1.Axioms)
                     axiom.Accept(interpreter);
 
                 //console.log(JSON.stringify(rules));
 
-                const c1Signal = store.Signal(['?x'], [[c1.Iri, '?x']], ...rules);
-                const c1Interpretation = new SortedSet(tupleCompare, store.SignalScheduler.Sample(c1Signal));
-                store.SignalScheduler.RemoveSignal(c1Signal);
+                function sample(
+                    c: IClass
+                    ): Set<Tuple>
+                {
+                    let signal: Signal;
 
-                it(
-                    `(i1)I ∈ (${classExpressionWriter.Write(ce)})C`,
-                    () => expect(c1Interpretation.has([i1Interpretation])).toBe(true));
-                it(
-                    `¬((i2)I ∈ (${classExpressionWriter.Write(ce)})C)`,
-                    () => expect(c1Interpretation.has([i2Interpretation])).toBe(false));
+                    try
+                    {
+                        signal = store.Signal(['?x'], [[c.Iri, '?x']], ...rules);
+                        return new SortedSet(tupleCompare, store.SignalScheduler.Sample(signal));
+                    }
+                    finally
+                    {
+                        store.SignalScheduler.RemoveSignal(signal);
+                    }
+                }
+
+                describe(
+                    'Given (op1)OP = {}',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        it(
+                            `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
+                            () => expect(sample(c1).has([x])).toBe(false));
+                    });
+
+                describe(
+                    'Given (op1)OP = {(x, i1)}',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        store.Assert(x, op1.LocalName, i1Interpretation)
+                        it(
+                            `x ∈ (${classExpressionWriter.Write(ce)})C`,
+                            () => expect(sample(c1).has([x])).toBe(true));
+                    });
             });
     });
