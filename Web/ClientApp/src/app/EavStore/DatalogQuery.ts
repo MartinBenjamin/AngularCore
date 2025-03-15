f7205ea8198144d3cc1f5fc2b2aeecbd4c94f57e
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2025-03-15 16:11:43 +0000

    Bancode.

---
+++ Web/ClientApp/src/app/EavStore/DatalogQuery.ts
@@ -0,0 +1,230 @@
+import { ArrayKeyedMap } from "../Collections/ArrayKeyedMap";
+import { Group } from "../Collections/Group";
+import { SortedMap } from "../Collections/SortedMap";
+import { LongestPaths } from "../Graph/AdjacencyList";
+import { Condense } from "../Graph/StronglyConnectedComponents";
+import { Aggregation } from "./Aggregation";
+import { Atom, IsConstant, IsEdb, IsIdb, IsVariable, RecursiveConjunction, Rule } from "./Datalog";
+import { tupleCompare } from "./EavStore";
+import { IEavStore } from "./IEavStore";
+import { Tuple } from "./Tuple";
+
+type SCC<T> = ReadonlyArray<T> & {LongestPath?: number;} & {Recursive?: boolean;};
+
+function Query<T extends Tuple>(
+    store: IEavStore,
+    head: [...T],
+    body: Atom[],
+    ...rules: Rule[]): {[K in keyof T]: any;}[]
+{
+    rules = [[['', ...head], body], ...rules];
+    const rulesGroupedByPredicateSymbol = Group(
+        rules,
+        rule => rule[0][0],
+        rule => rule);
+
+    const rulePredecessors = new Map(
+        rules.map<[string, string[]]>(
+            rule => [
+                rule[0][0],
+                [].concat(...rulesGroupedByPredicateSymbol.get(rule[0][0]).map(rule => rule[1].filter(IsIdb).map(idb => idb[0])))]));
+
+    // Condense the rule graph.
+    let condensed: ReadonlyMap<SCC<string>, ReadonlyArray<SCC<string>>> = Condense(rulePredecessors);
+
+    // Determine longest path for each strongly connected component in the condensed graph.
+    const longestPaths = LongestPaths(condensed);
+
+    for(const [stronglyConnectComponent, longestPath] of longestPaths)
+        stronglyConnectComponent.LongestPath = longestPath;
+
+    for(const scc of condensed.keys())
+        scc.Recursive = scc.length > 1 || rulePredecessors.get(scc[0]).includes(scc[0]);
+
+    const sorted = [...condensed.keys()].sort((a, b) => a.LongestPath - b.LongestPath);
+
+    const values = new ArrayKeyedMap(rules.map(rule => rule[1].filter(IsEdb).map(edb => <[any[], Iterable<Tuple>]>[edb, store.Query(edb)])).flat());
+
+    for(const stronglyConnectedComponent of sorted)
+        if(stronglyConnectedComponent.Recursive)
+            StronglyConnectedComponent(
+                rulesGroupedByPredicateSymbol,
+                stronglyConnectedComponent,
+                values);
+
+    return <any[]>values.get(['', ...head]);
+}
+
+function StronglyConnectedComponent(
+    rulesGroupedByPredicateSymbol: ReadonlyMap<string, Rule[]>,
+    stronglyConnectedComponent: SCC<string>,
+    values: Map<any[], Iterable<Tuple>>
+    ): void
+{
+    if(stronglyConnectedComponent.Recursive)
+        Recursion(
+            rulesGroupedByPredicateSymbol,
+            stronglyConnectedComponent,
+            values);
+    else
+    {
+        const predicateSymbol = stronglyConnectedComponent[0];
+        const rules = rulesGroupedByPredicateSymbol.get(predicateSymbol);
+        if(rules.length === 1)
+        {
+            const rule = rules[0];
+
+        }
+        else // Disjunction of conjunctions.
+        {
+
+        }
+    }
+}
+
+function Recursion(
+    rulesGroupedByPredicateSymbol: ReadonlyMap<string, Rule[]>,
+    stronglyConnectedComponent: SCC<string>,
+    values: Map<any[], Iterable<Tuple>>
+    ): void
+{
+}
+
+function RecursiveDisjunction(
+    rules: Rule[],
+    values: Map<any[], Iterable<Tuple>>
+    ): void
+{
+}
+
+function Conjunction(
+    rule: Rule,
+    values: Map<any[], Iterable<Tuple>>
+    ): void
+{
+    const [head, body] = rule;
+    let substitutions: object[];
+    [substitutions,] = RecursiveConjunction(
+        rule[1],
+        values);
+
+    if(body.some(term => term instanceof Aggregation))
+    {
+        const grouped = substitutions.reduce<Map<Tuple, object[]>>(
+            (grouped, substitution) =>
+            {
+                const key = head.map(term => (IsVariable(term) && term in substitution) ? substitution[term] : term);
+                let group = grouped.get(key);
+                if(group)
+                    group.push(substitution);
+
+                else
+                    grouped.set(
+                        key,
+                        [substitution])
+
+                return grouped;
+
+            },
+            new SortedMap<Tuple, object[]>(tupleCompare));
+
+        values.set(
+            head,
+            [...grouped.keys()].map(key => key.map(element => element instanceof Aggregation ? element.Aggregate(grouped.get(key)) : element)))
+        return;
+
+    }
+    else
+        values.set(
+            head,
+            substitutions.map(substitution => head.map(term => (IsVariable(term) && term in substitution) ? substitution[term] : term)));
+}
+
+let RecursiveConjunction: (body: Atom[], ...inputs: Iterable<Tuple>[])=> [object[], number];
+
+RecursiveConjunction = (body: Atom[], values: Map<any[], Iterable<Tuple>>): [object[], number] =>
+{
+    let inputIndex = 0;
+    const substitutions = body.reduce(
+        (substitutions, atom) =>
+        {
+            if(typeof atom === 'function')
+                return [...atom(substitutions)];
+
+            if(atom instanceof Not)
+            {
+                let input: Iterable<object>;
+                [input, inputIndex] = RecursiveConjunction(
+                    atom.Atoms,
+                    values);
+
+                let count = substitutions.length;
+                while(count--)
+                {
+                    const outer = substitutions.shift();
+                    let match = false;
+                    for(const inner of input)
+                    {
+                        match = true;
+                        for(const variable in inner)
+                            if(!(outer[variable] === undefined || outer[variable] === inner[variable]))
+                            {
+                                match = false;
+                                break;
+                            }
+
+                        if(match)
+                            break;
+                    }
+
+                    if(!match)
+                        substitutions.push(outer);
+                }
+            }
+            else
+            {
+                let count = substitutions.length;
+                const input = values.get(atom);
+                while(count--)
+                {
+                    const substitution = substitutions.shift();
+                    for(const tuple of input)
+                    {
+                        let merged = {...substitution};
+                        for(let index = 0; index < atom.length && merged; ++index)
+                        {
+                            const term = atom[index];
+                            if(IsConstant(term))
+                            {
+                                if(term !== tuple[index])
+                                    merged = null;
+                            }
+                            else if(IsVariable(term))
+                            {
+                                if(typeof merged[term] === 'undefined')
+                                    merged[term] = tuple[index];
+
+                                else if(merged[term] !== tuple[index])
+                                    // Fact does not match query pattern.
+                                    merged = null;
+                            }
+                        }
+
+                        if(merged)
+                            substitutions.push(merged);
+                    }
+                }
+            }
+
+            return substitutions;
+        },
+        [{}]);
+    return [substitutions, inputIndex];
+}
+
+function Disjunction(
+    rules: Rule[],
+    values: Map<any[], Iterable<Tuple>>
+    ): void
+{
+}
