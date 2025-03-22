6ade580faa0254ca1c6ffd33a9bfb886c534440d
Martin Benjamin <martin.benjamin@uk.mufg.jp>
2025-03-22 18:24:58 +0000

    Bank code.

--- Web/ClientApp/src/app/Ontology/ObjectExactCardinalityDatalog.Spec.ts
+++ Web/ClientApp/src/app/Ontology/ObjectExactCardinalityDatalog.Spec.ts
@@ -22,94 +22,91 @@ describe(
     {
         const ontologyWriter = OntologyWriter();
         const classExpressionWriter = new ClassExpressionWriter();
+        const o1 = new Ontology('o1');
+        const op1 = new ObjectProperty(o1, 'op1');
 
-        {
-            const o1 = new Ontology('o1');
-            const op1 = new ObjectProperty(o1, 'op1');
+        describe(
+            `Given ${ontologyWriter(o1)}:`,
+            () =>
+            {
+                const store: IEavStore = new EavStore();
+                const rules: Rule[] = [];
+                const interpreter = new AxiomInterpreter(
+                    o1,
+                    store,
+                    rules);
+                for(const axiom of o1.Axioms)
+                    axiom.Accept(interpreter);
 
-            describe(
-                `Given ${ontologyWriter(o1)}:`,
-                () =>
+                const ces = [0, 1, 2].map(cardinality => new ObjectExactCardinality(op1, cardinality));
+                const cePredicateSymbols = new Map(ces.map(ce => [ce, ce.Select(interpreter.ClassExpressionInterpreter)]));
+                //console.log(JSON.stringify(rules));
+
+                function Query(
+                    cePredicateSymbol: string
+                    ): Set<Tuple>
                 {
-                    const store: IEavStore = new EavStore();
-                    const rules: Rule[] = [];
-                    const interpreter = new AxiomInterpreter(
-                        o1,
-                        store,
-                        rules);
-                    for(const axiom of o1.Axioms)
-                        axiom.Accept(interpreter);
+                    return new SortedSet(tupleCompare, store.Query(['?x'], [[cePredicateSymbol, '?x']], ...rules));
+                }
 
-                    const ces = [0, 1, 2].map(cardinality => new ObjectExactCardinality(op1, cardinality));
-                    const cePredicateSymbols = new Map(ces.map(ce => [ce, ce.Select(interpreter.ClassExpressionInterpreter)]));
-                    //console.log(JSON.stringify(rules));
+                function Query1(
+                    cePredicateSymbol: string
+                    ): Set<Tuple>
+                {
+                    let signal: Signal;
 
-                    function Query(
-                        cePredicateSymbol: string
-                        ): Set<Tuple>
+                    try
                     {
-                        return new SortedSet(tupleCompare, store.Query(['?x'], [[cePredicateSymbol, '?x']], ...rules));
+                        signal = store.Signal(['?x'], [[cePredicateSymbol, '?x']], ...rules);
+                        return new SortedSet(tupleCompare, store.SignalScheduler.Sample(signal));
                     }
-
-                    function Query1(
-                        cePredicateSymbol: string
-                        ): Set<Tuple>
+                    finally
                     {
-                        let signal: Signal;
-
-                        try
-                        {
-                            signal = store.Signal(['?x'], [[cePredicateSymbol, '?x']], ...rules);
-                            return new SortedSet(tupleCompare, store.SignalScheduler.Sample(signal));
-                        }
-                        finally
-                        {
-                            store.SignalScheduler.RemoveSignal(signal);
-                        }
+                        store.SignalScheduler.RemoveSignal(signal);
                     }
-                    describe(
-                        'Given x ∈ ΔI:',
-                        () =>
-                        {
-                            const x = store.NewEntity();
-                            for(const ce of ces)
-                                it(
-                                    ce.Cardinality === 0 ?
-                                        `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
-                                    () => expect(Query(cePredicateSymbols.get(ce)).has([x])).toBe(ce.Cardinality === 0));
-                        });
+                }
+                describe(
+                    'Given x ∈ ΔI:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        for(const ce of ces)
+                            it(
+                                ce.Cardinality === 0 ?
+                                    `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
+                                () => expect(Query(cePredicateSymbols.get(ce)).has([x])).toBe(ce.Cardinality === 0));
+                    });
 
-                    describe(
-                        'Given (op1)OP = {(x, y)}:',
-                        () =>
-                        {
-                            const x = store.NewEntity();
-                            const y = 2;
-                            store.Assert(x, op1.LocalName, y);
-                            for(const ce of ces)
-                                it(
-                                    ce.Cardinality === 1 ?
-                                        `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
-                                    () => expect(Query(cePredicateSymbols.get(ce)).has([x])).toBe(ce.Cardinality === 1));
-                        });
+                describe(
+                    'Given (op1)OP = {(x, y)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        const y = 2;
+                        store.Assert(x, op1.LocalName, y);
+                        for(const ce of ces)
+                            it(
+                                ce.Cardinality === 1 ?
+                                    `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
+                                () => expect(Query(cePredicateSymbols.get(ce)).has([x])).toBe(ce.Cardinality === 1));
+                    });
 
-                    describe(
-                        'Given (op1)OP = {(x, y), (x, z)}:',
-                        () =>
-                        {
-                            const x = store.NewEntity();
-                            const y = 2;
-                            const z = 3;
-                            store.Assert(x, op1.LocalName, y);
-                            store.Assert(x, op1.LocalName, z);
-                            for(const ce of ces)
-                                it(
-                                    ce.Cardinality === 2 ?
-                                        `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
-                                    () => expect(Query(cePredicateSymbols.get(ce)).has([x])).toBe(ce.Cardinality === 2));
-                        });
-                });
-        }
+                describe(
+                    'Given (op1)OP = {(x, y), (x, z)}:',
+                    () =>
+                    {
+                        const x = store.NewEntity();
+                        const y = 2;
+                        const z = 3;
+                        store.Assert(x, op1.LocalName, y);
+                        store.Assert(x, op1.LocalName, z);
+                        for(const ce of ces)
+                            it(
+                                ce.Cardinality === 2 ?
+                                    `x ∈ (${classExpressionWriter.Write(ce)})C` : `¬(x ∈ (${classExpressionWriter.Write(ce)})C)`,
+                                () => expect(Query(cePredicateSymbols.get(ce)).has([x])).toBe(ce.Cardinality === 2));
+                    });
+            });
     });
 
 describe(
