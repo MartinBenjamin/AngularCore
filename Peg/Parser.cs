45cf2ce389bf2dcfb6fe86891d3b8a946e3c52fa
Martin Benjamin <martin.jonathan.benjamin@gmail.com>
2026-02-02 14:36:29 +0000

    Refactor.

--- Peg/Parser.cs
+++ Peg/Parser.cs
@@ -7,39 +7,38 @@ namespace Peg
     public class Parser
     {
         // Hierarchical syntax
-        public Definition Grammar                = new Definition(nameof(Grammar               ));
-        public Definition Definition             = new Definition(nameof(Definition            ));
-        public Definition Expression             = new Definition(nameof(Expression            ));
-        public Definition Sequence               = new Definition(nameof(Sequence              ));
-        public Definition Prefix                 = new Definition(nameof(Prefix                ));
-        public Definition Suffix                 = new Definition(nameof(Suffix                ));
-        public Definition Primary                = new Definition(nameof(Primary               ));
+        public Definition Grammar      = new Definition(nameof(Grammar     ));
+        public Definition Definition   = new Definition(nameof(Definition  ));
+        public Definition Expression   = new Definition(nameof(Expression  ));
+        public Definition Sequence     = new Definition(nameof(Sequence    ));
+        public Definition Prefix       = new Definition(nameof(Prefix      ));
+        public Definition Suffix       = new Definition(nameof(Suffix      ));
+        public Definition Primary      = new Definition(nameof(Primary     ));
 
         // Lexical syntax
-        public Definition Identifier             = new Definition(nameof(Identifier            ));
-        public Definition IdentStart             = new Definition(nameof(IdentStart            ));
-        public Definition IdentCont              = new Definition(nameof(IdentCont             ));
-        public Definition Literal                = new Definition(nameof(Literal               ));
-        public Definition CharacterSet           = new Definition(nameof(CharacterSet          ));
-        public Definition CharacterSetComplement = new Definition(nameof(CharacterSetComplement));
-        public Definition Range                  = new Definition(nameof(Range                 ));
-        public Definition Char                   = new Definition(nameof(Char                  ));
-        public Definition LEFTARROW              = new Definition(nameof(LEFTARROW             ));
+        public Definition Identifier   = new Definition(nameof(Identifier  ));
+        public Definition IdentStart   = new Definition(nameof(IdentStart  ));
+        public Definition IdentCont    = new Definition(nameof(IdentCont   ));
+        public Definition Literal      = new Definition(nameof(Literal     ));
+        public Definition CharacterSet = new Definition(nameof(CharacterSet));
+        public Definition Range        = new Definition(nameof(Range       ));
+        public Definition Char         = new Definition(nameof(Char        ));
+        public Definition LEFTARROW    = new Definition(nameof(LEFTARROW   ));
 
-        public Definition SLASH                  = new Definition(nameof(SLASH                 ));
-        public Definition AND                    = new Definition(nameof(AND                   ));
-        public Definition NOT                    = new Definition(nameof(NOT                   ));
-        public Definition QUESTION               = new Definition(nameof(QUESTION              ));
-        public Definition STAR                   = new Definition(nameof(STAR                  ));
-        public Definition PLUS                   = new Definition(nameof(PLUS                  ));
-        public Definition OPEN                   = new Definition(nameof(OPEN                  ));
-        public Definition CLOSE                  = new Definition(nameof(CLOSE                 ));
-        public Definition DOT                    = new Definition(nameof(DOT                   ));
-        public Definition Spacing                = new Definition(nameof(Spacing               ));
-        public Definition Comment                = new Definition(nameof(Comment               ));
-        public Definition Space                  = new Definition(nameof(Space                 ));
-        public Definition EndOfLine              = new Definition(nameof(EndOfLine             ));
-        public Definition EndOfFile              = new Definition(nameof(EndOfFile             ));
+        public Definition SLASH        = new Definition(nameof(SLASH       ));
+        public Definition AND          = new Definition(nameof(AND         ));
+        public Definition NOT          = new Definition(nameof(NOT         ));
+        public Definition QUESTION     = new Definition(nameof(QUESTION    ));
+        public Definition STAR         = new Definition(nameof(STAR        ));
+        public Definition PLUS         = new Definition(nameof(PLUS        ));
+        public Definition OPEN         = new Definition(nameof(OPEN        ));
+        public Definition CLOSE        = new Definition(nameof(CLOSE       ));
+        public Definition DOT          = new Definition(nameof(DOT         ));
+        public Definition Spacing      = new Definition(nameof(Spacing     ));
+        public Definition Comment      = new Definition(nameof(Comment     ));
+        public Definition Space        = new Definition(nameof(Space       ));
+        public Definition EndOfLine    = new Definition(nameof(EndOfLine   ));
+        public Definition EndOfFile    = new Definition(nameof(EndOfFile   ));
 
         public Parser()
         {
@@ -54,7 +53,6 @@ namespace Peg
                 new Sequence(OPEN, Expression, CLOSE),
                 Literal,
                 CharacterSet,
-                CharacterSetComplement,
                 DOT);
             Identifier  .Expression = new Sequence(IdentStart, new ZeroOrMore(IdentCont), Spacing);
             IdentStart  .Expression = new CharacterSet(
@@ -67,41 +65,23 @@ namespace Peg
             Literal.Expression = new Choice(
                 new Sequence(
                     new CharacterSet("\""),
-                    new ZeroOrMore(
-                        new Choice(
-                            new Sequence(new Literal("\\"), new CharacterSet("\\\"")),
-                            new CharacterSetComplement("\\\""))),
+                    new ZeroOrMore(new Sequence(new Not(new CharacterSet("\"")), Char)),
                     new CharacterSet("\""),
                     Spacing),
                 new Sequence(
                     new CharacterSet("'"),
-                    new ZeroOrMore(
-                        new Choice(
-                            new Sequence(new Literal("\\"), new CharacterSet("\\'")),
-                            new CharacterSetComplement("\\'"))),
+                    new ZeroOrMore(new Sequence(new Not(new CharacterSet("'")), Char)),
                     new CharacterSet("'"),
                     Spacing));
             CharacterSet.Expression = new Sequence(
                 new Literal("["),
-                new ZeroOrMore(
-                    new Choice(
-                        Range,
-                        Char)),
+                new ZeroOrMore(new Sequence(new Not(new Literal("]")), Range)),
                 new Literal("]"),
                 Spacing);
-            CharacterSetComplement
-                        .Expression = new Sequence(
-                new Literal("[^"),
-                new ZeroOrMore(
-                    new Choice(
-                        Range,
-                        Char)),
-                new Literal("]"),
-                Spacing);
-            Range.Expression = new Sequence(Char, new Literal("-"), Char);
+            Range       .Expression = new Choice(new Sequence(Char, new Literal("-"), Char), Char);
             Char        .Expression = new Choice(
-                new Sequence(new Literal("\\"), new CharacterSet("\\-]")),
-                new CharacterSetComplement("\\-]"));
+                new Sequence(new Literal("\\"), new CharacterSet("nrt'\"[]\\")),
+                new Sequence(new Not(new Literal("\\")), new Dot()));
             LEFTARROW   .Expression = new Sequence(new Literal("<-"), Spacing);
             SLASH       .Expression = new Sequence(new Literal( "/"), Spacing);
             AND         .Expression = new Sequence(new Literal( "&"), Spacing);
@@ -141,7 +121,6 @@ namespace Peg
                 IdentCont             ,
                 Literal               ,
                 CharacterSet          ,
-                CharacterSetComplement,
                 Range                 ,
                 Char                  ,
                 LEFTARROW             ,
